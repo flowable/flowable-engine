44b6a014735c595c99972bf13c2f5940e34df631
Joram Barrez <joram.barrez@gmail.com>
2016-10-14 14:46:47 +0200

    Copying new docs to v6 + change build script

--- userguide/src/en/generate-html.sh
+++ userguide/src/en/generate-html.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-asciidoctor -a stylesheet=colony.css index.adoc
+asciidoctor -a stylesheet=flowable.css -o output/index.html index-html.adoc
+asciidoctor -a stylesheet=flowable.css -o output/migration.html migration.adoc
 
-asciidoctor -a stylesheer=colony.css migration.adoc
+rm -rf output/images
+mkdir output/images
+cp -r images output
