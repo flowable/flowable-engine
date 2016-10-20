aacddae4a64b7de8f449bf37109a94ff386ff891
Tijs Rademakers <tijs.rademakers@gmail.com>
2016-10-20 21:35:44 +0200

    Move modeler and idm projects to modules folder and implement idm refactoring in Modeler

--- modules/flowable-ui-modeler/flowable-ui-modeler-conf/src/main/java/org/activiti/app/conf/ActivitiEngineConfiguration.java
+++ modules/flowable-ui-modeler/flowable-ui-modeler-conf/src/main/java/org/activiti/app/conf/ActivitiEngineConfiguration.java
@@ -15,58 +15,40 @@ package org.activiti.app.conf;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
 import javax.sql.DataSource;
 
 import org.activiti.dmn.engine.DmnEngineConfiguration;
 import org.activiti.dmn.engine.configurator.DmnEngineConfigurator;
-import org.activiti.engine.FormService;
-import org.activiti.engine.HistoryService;
-import org.activiti.engine.IdentityService;
-import org.activiti.engine.ManagementService;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.RepositoryService;
-import org.activiti.engine.RuntimeService;
-import org.activiti.engine.TaskService;
-import org.activiti.engine.impl.asyncexecutor.AsyncExecutor;
-import org.activiti.engine.impl.asyncexecutor.DefaultAsyncJobExecutor;
 import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.activiti.engine.parse.BpmnParseHandler;
-import org.activiti.engine.runtime.Clock;
-import org.activiti.form.api.FormRepositoryService;
 import org.activiti.form.engine.FormEngineConfiguration;
 import org.activiti.form.engine.configurator.FormEngineConfigurator;
 import org.activiti.idm.api.IdmIdentityService;
 import org.activiti.spring.ProcessEngineFactoryBean;
 import org.activiti.spring.SpringProcessEngineConfiguration;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.env.Environment;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@ComponentScan(basePackages= {
-		"org.activiti.app.runtime.activiti",
-		"org.activiti.app.extension.conf", // For custom configuration classes
-		"org.activiti.app.extension.bean" // For custom beans (delegates etc.)
-})
 public class ActivitiEngineConfiguration {
 
     private final Logger logger = LoggerFactory.getLogger(ActivitiEngineConfiguration.class);
     
-    @Inject
+    @Autowired
     private DataSource dataSource;
     
-    @Inject
+    @Autowired
     private PlatformTransactionManager transactionManager;
     
-    @Inject
+    @Autowired
     private Environment environment;
     
     @Bean(name="processEngine")
@@ -92,24 +74,8 @@ public class ActivitiEngineConfiguration {
     	processEngineConfiguration.setDataSource(dataSource);
     	processEngineConfiguration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
     	processEngineConfiguration.setTransactionManager(transactionManager);
-    	processEngineConfiguration.setAsyncExecutorActivate(true);
-    	processEngineConfiguration.setAsyncExecutor(asyncExecutor());
+    	processEngineConfiguration.setAsyncExecutorActivate(false);
 
-    	String emailHost = environment.getProperty("email.host");
-    	if (StringUtils.isNotEmpty(emailHost)) {
-        	processEngineConfiguration.setMailServerHost(emailHost);
-        	processEngineConfiguration.setMailServerPort(environment.getRequiredProperty("email.port", Integer.class));
-        	
-        	Boolean useCredentials = environment.getProperty("email.useCredentials", Boolean.class);
-            if (Boolean.TRUE.equals(useCredentials)) {
-                processEngineConfiguration.setMailServerUsername(environment.getProperty("email.username"));
-                processEngineConfiguration.setMailServerPassword(environment.getProperty("email.password"));
-            }
-    	}
-    	
-    	// Limit process definition cache
-    	processEngineConfiguration.setProcessDefinitionCacheLimit(environment.getProperty("activiti.process-definitions.cache.max", Integer.class, 128));
-    	
     	// Enable safe XML. See http://activiti.org/userguide/index.html#advanced.safe.bpmn.xml
     	processEngineConfiguration.setEnableSafeBpmnXml(true);
     	
@@ -134,66 +100,12 @@ public class ActivitiEngineConfiguration {
     }
     
     @Bean
-    public AsyncExecutor asyncExecutor() {
-      DefaultAsyncJobExecutor asyncExecutor = new DefaultAsyncJobExecutor();
-      asyncExecutor.setDefaultAsyncJobAcquireWaitTimeInMillis(5000);
-      asyncExecutor.setDefaultTimerJobAcquireWaitTimeInMillis(5000);
-      return asyncExecutor;
-    }
-    
-    @Bean(name="clock")
-    @DependsOn("processEngine")
-    public Clock getClock() {
-    	return processEngineConfiguration().getClock();
-    }
-    
-    @Bean
     public RepositoryService repositoryService() {
     	return processEngine().getRepositoryService();
     }
     
     @Bean
-    public RuntimeService runtimeService() {
-    	return processEngine().getRuntimeService();
-    }
-    
-    @Bean
-    public TaskService taskService() {
-    	return processEngine().getTaskService();
-    }
-    
-    @Bean
-    public HistoryService historyService() {
-    	return processEngine().getHistoryService();
-    }
-    
-    @Bean
-    public FormService formService() {
-    	return processEngine().getFormService();
-    }
-    
-    @Bean
-    public IdentityService identityService() {
-    	return processEngine().getIdentityService();
-    }
-    
-    @Bean
     public IdmIdentityService idmIdentityService() {
       return processEngine().getIdmIdentityService();
-    }
-    
-    @Bean
-    public ManagementService managementService() {
-    	return processEngine().getManagementService();
-    }
-    
-    @Bean
-    public FormRepositoryService formEngineRepositoryService() {
-      return processEngine().getFormEngineRepositoryService();
-    }
-    
-    @Bean
-    public org.activiti.form.api.FormService formEngineFormService() {
-      return processEngine().getFormEngineFormService();
     }
 }
