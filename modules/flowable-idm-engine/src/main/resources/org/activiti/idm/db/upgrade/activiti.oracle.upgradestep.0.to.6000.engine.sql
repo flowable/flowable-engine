b03e59edbd166e7da5ccee243b02a19e81d2a48c
Tijs Rademakers <tijs.rademakers@gmail.com>
2016-11-30 15:33:23 +0100

    Remove next.dbid from idm property table

--- modules/flowable-idm-engine/src/main/resources/org/activiti/idm/db/upgrade/activiti.oracle.upgradestep.0.to.6000.engine.sql
+++ modules/flowable-idm-engine/src/main/resources/org/activiti/idm/db/upgrade/activiti.oracle.upgradestep.0.to.6000.engine.sql
@@ -8,9 +8,6 @@ create table ACT_ID_PROPERTY (
 insert into ACT_ID_PROPERTY
 values ('schema.version', '6.0.0.0', 1);
 
-insert into ACT_ID_PROPERTY
-values ('next.dbid', '9999', 1);
-
 create table ACT_ID_BYTEARRAY (
     ID_ NVARCHAR2(64),
     REV_ INTEGER,
