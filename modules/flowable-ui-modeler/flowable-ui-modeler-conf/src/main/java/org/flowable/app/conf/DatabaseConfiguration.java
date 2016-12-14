ef0afa5d26b7c4a225d3f0690e1dbdda8cfee787
Tijs Rademakers <tijs.rademakers@gmail.com>
2016-12-14 09:22:47 +0100

    Fix Flowable modeler blob issue

--- modules/flowable-ui-modeler/flowable-ui-modeler-conf/src/main/java/org/flowable/app/conf/DatabaseConfiguration.java
+++ modules/flowable-ui-modeler/flowable-ui-modeler-conf/src/main/java/org/flowable/app/conf/DatabaseConfiguration.java
@@ -13,6 +13,9 @@
 package org.flowable.app.conf;
 
 import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -20,6 +23,7 @@ import javax.sql.DataSource;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.flowable.app.service.exception.InternalServerErrorException;
+import org.flowable.engine.common.api.FlowableException;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
@@ -57,6 +61,48 @@ public class DatabaseConfiguration {
   
   @Autowired
   protected ResourceLoader resourceLoader;
+  
+  protected static Properties databaseTypeMappings = getDefaultDatabaseTypeMappings();
+
+  public static final String DATABASE_TYPE_H2 = "h2";
+  public static final String DATABASE_TYPE_HSQL = "hsql";
+  public static final String DATABASE_TYPE_MYSQL = "mysql";
+  public static final String DATABASE_TYPE_ORACLE = "oracle";
+  public static final String DATABASE_TYPE_POSTGRES = "postgres";
+  public static final String DATABASE_TYPE_MSSQL = "mssql";
+  public static final String DATABASE_TYPE_DB2 = "db2";
+
+  public static Properties getDefaultDatabaseTypeMappings() {
+    Properties databaseTypeMappings = new Properties();
+    databaseTypeMappings.setProperty("H2", DATABASE_TYPE_H2);
+    databaseTypeMappings.setProperty("HSQL Database Engine", DATABASE_TYPE_HSQL);
+    databaseTypeMappings.setProperty("MySQL", DATABASE_TYPE_MYSQL);
+    databaseTypeMappings.setProperty("Oracle", DATABASE_TYPE_ORACLE);
+    databaseTypeMappings.setProperty("PostgreSQL", DATABASE_TYPE_POSTGRES);
+    databaseTypeMappings.setProperty("Microsoft SQL Server", DATABASE_TYPE_MSSQL);
+    databaseTypeMappings.setProperty(DATABASE_TYPE_DB2, DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/NT", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/NT64", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2 UDP", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/LINUX", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/LINUX390", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/LINUXX8664", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/LINUXZ64", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/LINUXPPC64", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/400 SQL", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/6000", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2 UDB iSeries", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/AIX64", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/HPUX", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/HP64", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/SUN", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/SUN64", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/PTX", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2/2", DATABASE_TYPE_DB2);
+    databaseTypeMappings.setProperty("DB2 UDB AS400", DATABASE_TYPE_DB2);
+    return databaseTypeMappings;
+  }
 
   @Bean
   public DataSource dataSource() {
@@ -163,18 +209,28 @@ public class DatabaseConfiguration {
   @Bean
   public SqlSessionFactory sqlSessionFactory() {
     SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-    sqlSessionFactoryBean.setDataSource(dataSource());
+    DataSource dataSource = dataSource();
+    sqlSessionFactoryBean.setDataSource(dataSource);
+    String databaseType = initDatabaseType(dataSource);
+    if (databaseType == null) {
+      throw new FlowableException("couldn't deduct database type");
+    }
     
     try {
       Properties properties = new Properties();
       properties.put("prefix", env.getProperty("datasource.prefix", ""));
+      properties.put("blobType", "BLOB");
+      properties.put("boolValue", "TRUE");
+      
+      properties.load(this.getClass().getClassLoader().getResourceAsStream("org/flowable/db/properties/" + databaseType + ".properties"));
+      
       sqlSessionFactoryBean.setConfigurationProperties(properties);
       sqlSessionFactoryBean
         .setMapperLocations(ResourcePatternUtils.getResourcePatternResolver(resourceLoader).getResources("classpath:/META-INF/modeler-mybatis-mappings/*.xml"));
       sqlSessionFactoryBean.afterPropertiesSet();
       return sqlSessionFactoryBean.getObject();
     } catch (Exception e) {
-      throw new RuntimeException("Could not create sqlSessionFactory", e);
+      throw new FlowableException("Could not create sqlSessionFactory", e);
     }
 
   }
@@ -201,6 +257,35 @@ public class DatabaseConfiguration {
     } catch (Exception e) {
       throw new InternalServerErrorException("Error creating liquibase database", e);
     }
+  }
+  
+  protected String initDatabaseType(DataSource dataSource) {
+    String databaseType = null;
+    Connection connection = null;
+    try {
+      connection = dataSource.getConnection();
+      DatabaseMetaData databaseMetaData = connection.getMetaData();
+      String databaseProductName = databaseMetaData.getDatabaseProductName();
+      log.info("database product name: '{}'", databaseProductName);
+      databaseType = databaseTypeMappings.getProperty(databaseProductName);
+      if (databaseType == null) {
+        throw new FlowableException("couldn't deduct database type from database product name '" + databaseProductName + "'");
+      }
+      log.info("using database type: {}", databaseType);
+
+    } catch (SQLException e) {
+      log.error("Exception while initializing Database connection", e);
+    } finally {
+      try {
+        if (connection != null) {
+          connection.close();
+        }
+      } catch (SQLException e) {
+        log.error("Exception while closing the Database connection", e);
+      }
+    }
+    
+    return databaseType;
   }
   
 }
