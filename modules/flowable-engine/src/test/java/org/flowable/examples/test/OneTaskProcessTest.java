52a7e575b186e4b30cb98bba810b1aaf39dd4e3c
Tijs Rademakers <tijs.rademakers@gmail.com>
2017-04-21 10:38:57 +0200

    Added license headers

--- modules/flowable-engine/src/test/java/org/flowable/examples/test/OneTaskProcessTest.java
+++ modules/flowable-engine/src/test/java/org/flowable/examples/test/OneTaskProcessTest.java
@@ -1,7 +1,29 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.flowable.examples.test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.flowable.bpmn.model.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.flowable.bpmn.model.BpmnModel;
+import org.flowable.bpmn.model.EndEvent;
+import org.flowable.bpmn.model.FlowNode;
+import org.flowable.bpmn.model.ScriptTask;
+import org.flowable.bpmn.model.SequenceFlow;
+import org.flowable.bpmn.model.StartEvent;
 import org.flowable.engine.event.EventLogEntry;
 import org.flowable.engine.impl.event.logger.EventLogger;
 import org.flowable.engine.impl.test.PluggableFlowableTestCase;
@@ -9,21 +31,12 @@ import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.engine.test.Deployment;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 /**
  * This class provides examples how to test one task process
  */
 public class OneTaskProcessTest extends PluggableFlowableTestCase {
 
     protected EventLogger databaseEventLogger;
-
-    protected ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void setUp() throws Exception {
