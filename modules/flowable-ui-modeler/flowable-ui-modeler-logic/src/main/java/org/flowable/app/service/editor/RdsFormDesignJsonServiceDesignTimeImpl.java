5a7c42d0f3e2fb09212f962d32899a079ee05976
simon.yang <simon.yang@relational.com.au>
2017-05-03 11:08:25 +0930

    add findAllFormDesignKeyJsonPairs to RdsFormDesignJsonServiceDesignTimeImpl

--- modules/flowable-ui-modeler/flowable-ui-modeler-logic/src/main/java/org/flowable/app/service/editor/RdsFormDesignJsonServiceDesignTimeImpl.java
+++ modules/flowable-ui-modeler/flowable-ui-modeler-logic/src/main/java/org/flowable/app/service/editor/RdsFormDesignJsonServiceDesignTimeImpl.java
@@ -3,8 +3,11 @@
  */
 package org.flowable.app.service.editor;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.flowable.app.domain.editor.AbstractModel;
 import org.flowable.app.domain.editor.Model;
 import org.flowable.app.repository.editor.ModelRepository;
@@ -34,6 +37,20 @@ public class RdsFormDesignJsonServiceDesignTimeImpl extends FormDesignJsonServic
     Assert.isTrue(models.size()==1, "Should get 1 and only 1 rds_form by key, but now get " + models.size());
     Model model = models.get(0);
     return model.getModelEditorJson();
+  }
+
+  /* (non-Javadoc)
+   * @see au.com.rds.schemaformbuilder.formdesignjson.FormDesignJsonService#findAllFormDesignKeyJsonPairs()
+   */
+  @Override
+  public List<Pair<String, String>> findAllFormDesignKeyJsonPairs()
+  {
+    List<Model> models =  modelRepo.findByModelType(AbstractModel.MODEL_TYPE_FORM_RDS, null);
+    List<Pair<String, String>> resultList = new LinkedList<Pair<String, String>>();
+    for (Model model:models) {
+      resultList.add(Pair.of(model.getKey(), model.getModelEditorJson()));
+    }
+    return resultList;
   }
 
 }
