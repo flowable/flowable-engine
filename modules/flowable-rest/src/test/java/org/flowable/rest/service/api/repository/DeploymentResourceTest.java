e07c61483393316c1d51979aff13a02ce0ecde9e
Tijs Rademakers <tijs.rademakers@gmail.com>
2017-06-02 08:54:38 +0200

    Upgrade httpclient version

--- modules/flowable-rest/src/test/java/org/flowable/rest/service/api/repository/DeploymentResourceTest.java
+++ modules/flowable-rest/src/test/java/org/flowable/rest/service/api/repository/DeploymentResourceTest.java
@@ -211,9 +211,10 @@ public class DeploymentResourceTest extends BaseSpringRestTestCase {
 
         HttpGet httpGet = new HttpGet(SERVER_URL_PREFIX + RestUrls.createRelativeResourceUrl(RestUrls.URL_DEPLOYMENT, existingDeployment.getId()));
         CloseableHttpResponse response = executeRequest(httpGet, HttpStatus.SC_OK);
-        closeResponse(response);
 
         JsonNode responseNode = objectMapper.readTree(response.getEntity().getContent());
+        
+        closeResponse(response);
 
         String deploymentId = responseNode.get("id").textValue();
         String name = responseNode.get("name").textValue();
