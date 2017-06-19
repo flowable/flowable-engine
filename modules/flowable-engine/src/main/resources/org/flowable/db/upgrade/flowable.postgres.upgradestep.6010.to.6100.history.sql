f449acb6aff95e8986b139b48c6c980dd991b052
Tijs Rademakers <tijs.rademakers@gmail.com>
2017-06-19 11:34:04 +0200

    Added upgrade scripts + qa config

--- modules/flowable-engine/src/main/resources/org/flowable/db/upgrade/flowable.postgres.upgradestep.6010.to.6100.history.sql
+++ modules/flowable-engine/src/main/resources/org/flowable/db/upgrade/flowable.postgres.upgradestep.6010.to.6100.history.sql
@@ -1,1 +1,7 @@
 create index ACT_IDX_HI_PROCVAR_EXE on ACT_HI_VARINST(EXECUTION_ID_);
+
+alter table ACT_HI_PROCINST add column REV_ integer default 1;
+alter table ACT_HI_ACTINST add column REV_ integer default 1;
+alter table ACT_HI_TASKINST add column REV_ integer default 1;
+alter table ACT_HI_TASKINST add column LAST_UPDATED_TIME_ timestamp;
+alter table ACT_HI_VARINST alter column REV_ set default 1;
