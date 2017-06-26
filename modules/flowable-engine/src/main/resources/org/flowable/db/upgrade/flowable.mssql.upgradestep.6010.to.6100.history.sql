a8839edc3392003d2817569624fb9a3c6b9a652f
Tijs Rademakers <tijs.rademakers@gmail.com>
2017-06-26 11:14:28 +0200

    MSSQL does it a bit different of course

--- modules/flowable-engine/src/main/resources/org/flowable/db/upgrade/flowable.mssql.upgradestep.6010.to.6100.history.sql
+++ modules/flowable-engine/src/main/resources/org/flowable/db/upgrade/flowable.mssql.upgradestep.6010.to.6100.history.sql
@@ -4,4 +4,4 @@ alter table ACT_HI_PROCINST add REV_ int default 1;
 alter table ACT_HI_ACTINST add REV_ int default 1;
 alter table ACT_HI_TASKINST add REV_ int default 1;
 alter table ACT_HI_TASKINST add LAST_UPDATED_TIME_ datetime;
-alter table ACT_HI_VARINST alter column REV_ default 1;
+alter table ACT_HI_VARINST ADD DEFAULT 1 FOR REV_;
