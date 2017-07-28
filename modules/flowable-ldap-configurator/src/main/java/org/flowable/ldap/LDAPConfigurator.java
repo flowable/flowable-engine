flowable/flowable-engine

commit 148f4f249e01b976b2f91694ef32966988a43c98

Refactor of Engine services + bug fixes

modules/flowable-ldap-configurator/src/main/java/org/flowable/ldap/LDAPConfigurator.java
@@ -16,6 +16,7 @@ import org.flowable.engine.cfg.AbstractProcessEngineConfigurator;
 import org.flowable.engine.cfg.ProcessEngineConfigurator;
 import org.flowable.engine.common.api.FlowableException;
 import org.flowable.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.flowable.engine.impl.util.EngineServiceUtil;
 
 /**
  * A {@link ProcessEngineConfigurator} that integrates a LDAP system with the Flowable process engine. The LDAP system will be consulted primarily for getting user information and in particular for
@@ -53,7 +54,8 @@ public class LDAPConfigurator extends AbstractProcessEngineConfigurator {
             }
         }
 
-        processEngineConfiguration.setIdmIdentityService(new LDAPIdentityServiceImpl(ldapConfiguration, ldapGroupCache));
+        EngineServiceUtil.getIdmEngineConfiguration(processEngineConfiguration)
+                .setIdmIdentityService(new LDAPIdentityServiceImpl(ldapConfiguration, ldapGroupCache));
     }
 
     // Getters and Setters //////////////////////////////////////////////////

modules/flowable-app-rest/src/main/java/org/flowable/rest/conf/FlowableEngineConfiguration.java
@@ -2,8 +2,10 @@ package org.flowable.rest.conf;
 
 import javax.sql.DataSource;
 
+import org.flowable.content.api.ContentEngineConfigurationApi;
 import org.flowable.content.api.ContentService;
 import org.flowable.content.spring.configurator.SpringContentEngineConfigurator;
+import org.flowable.dmn.api.DmnEngineConfigurationApi;
 import org.flowable.dmn.api.DmnRepositoryService;
 import org.flowable.dmn.api.DmnRuleService;
 import org.flowable.dmn.spring.configurator.SpringDmnEngineConfigurator;
@@ -12,10 +14,13 @@ import org.flowable.engine.HistoryService;
 import org.flowable.engine.IdentityService;
 import org.flowable.engine.ManagementService;
 import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
 import org.flowable.engine.RepositoryService;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
 import org.flowable.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.flowable.engine.impl.util.EngineServiceUtil;
+import org.flowable.form.api.FormEngineConfigurationApi;
 import org.flowable.form.api.FormRepositoryService;
 import org.flowable.form.spring.configurator.SpringFormEngineConfigurator;
 import org.flowable.spring.ProcessEngineFactoryBean;
@@ -58,6 +63,24 @@ public class FlowableEngineConfiguration {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    @Bean(name = "dmnEngineConfiguration")
+    public DmnEngineConfigurationApi dmnEngineConfiguration() {
+        ProcessEngineConfiguration processEngineConfiguration = processEngine().getProcessEngineConfiguration();
+        return EngineServiceUtil.getDmnEngineConfiguration(processEngineConfiguration);
+    }
+    
+    @Bean(name = "formEngineConfiguration")
+    public FormEngineConfigurationApi formEngineConfiguration() {
+        ProcessEngineConfiguration processEngineConfiguration = processEngine().getProcessEngineConfiguration();
+        return EngineServiceUtil.getFormEngineConfiguration(processEngineConfiguration);
+    }
+    
+    @Bean(name = "contentEngineConfiguration")
+    public ContentEngineConfigurationApi contentEngineConfiguration() {
+        ProcessEngineConfiguration processEngineConfiguration = processEngine().getProcessEngineConfiguration();
+        return EngineServiceUtil.getContentEngineConfiguration(processEngineConfiguration);
     }
 
     @Bean(name = "processEngineConfiguration")
@@ -113,26 +136,26 @@ public class FlowableEngineConfiguration {
 
     @Bean
     public FormRepositoryService formRepositoryService() {
-        return processEngine().getFormEngineRepositoryService();
+        return formEngineConfiguration().getFormRepositoryService();
     }
 
     @Bean
     public org.flowable.form.api.FormService formEngineFormService() {
-        return processEngine().getFormEngineFormService();
+        return formEngineConfiguration().getFormService();
     }
 
     @Bean
     public DmnRepositoryService dmnRepositoryService() {
-        return processEngine().getDmnRepositoryService();
+        return dmnEngineConfiguration().getDmnRepositoryService();
     }
 
     @Bean
     public DmnRuleService dmnRuleService() {
-        return processEngine().getDmnRuleService();
+        return dmnEngineConfiguration().getDmnRuleService();
     }
 
     @Bean
     public ContentService contentService() {
-        return processEngine().getContentService();
+        return contentEngineConfiguration().getContentService();
     }
 }

modules/flowable-content-api/src/main/java/org/flowable/content/api/ContentEngineConfigurationApi.java
@@ -1,0 +1,19 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flowable.content.api;
+
+public interface ContentEngineConfigurationApi {
+
+    ContentService getContentService();
+    
+}

modules/flowable-content-engine-configurator/src/main/java/org/flowable/content/engine/configurator/ContentEngineConfigurator.java
@@ -52,12 +52,11 @@ public class ContentEngineConfigurator extends AbstractEngineConfigurator {
     public void configure(ProcessEngineConfigurationImpl processEngineConfiguration) {
         if (contentEngineConfiguration == null) {
             contentEngineConfiguration = new StandaloneContentEngineConfiguration();
-            initialiseCommonProperties(processEngineConfiguration, contentEngineConfiguration, EngineConfigurationConstants.KEY_CONTENT_ENGINE_CONFIG);
         }
+        
+        initialiseCommonProperties(processEngineConfiguration, contentEngineConfiguration, EngineConfigurationConstants.KEY_CONTENT_ENGINE_CONFIG);
 
-        ContentEngine contentEngine = initContentEngine();
-        processEngineConfiguration.setContentEngineInitialized(true);
-        processEngineConfiguration.setContentService(contentEngine.getContentService());
+        initContentEngine();
     }
     
     @Override

modules/flowable-content-engine/src/main/java/org/flowable/content/engine/ContentEngineConfiguration.java
@@ -26,6 +26,7 @@ import javax.sql.DataSource;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.transaction.TransactionFactory;
+import org.flowable.content.api.ContentEngineConfigurationApi;
 import org.flowable.content.api.ContentManagementService;
 import org.flowable.content.api.ContentService;
 import org.flowable.content.api.ContentStorage;
@@ -71,7 +72,7 @@ import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.ClassLoaderResourceAccessor;
 
-public class ContentEngineConfiguration extends AbstractEngineConfiguration {
+public class ContentEngineConfiguration extends AbstractEngineConfiguration implements ContentEngineConfigurationApi {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(ContentEngineConfiguration.class);
 

modules/flowable-content-spring-configurator/src/main/java/org/flowable/content/spring/configurator/SpringContentEngineConfigurator.java
@@ -32,13 +32,11 @@ public class SpringContentEngineConfigurator extends ContentEngineConfigurator {
     public void configure(ProcessEngineConfigurationImpl processEngineConfiguration) {
         if (contentEngineConfiguration == null) {
             contentEngineConfiguration = new SpringContentEngineConfiguration();
-            initialiseCommonProperties(processEngineConfiguration, contentEngineConfiguration, EngineConfigurationConstants.KEY_CONTENT_ENGINE_CONFIG);
         }
+        initialiseCommonProperties(processEngineConfiguration, contentEngineConfiguration, EngineConfigurationConstants.KEY_CONTENT_ENGINE_CONFIG);
         contentEngineConfiguration.setTransactionManager(((SpringProcessEngineConfiguration) processEngineConfiguration).getTransactionManager());
         
-        ContentEngine contentEngine = initContentEngine();
-        processEngineConfiguration.setContentEngineInitialized(true);
-        processEngineConfiguration.setContentService(contentEngine.getContentService());
+        initContentEngine();
     }
 
     protected synchronized ContentEngine initContentEngine() {

modules/flowable-dmn-api/src/main/java/org/flowable/dmn/api/DmnEngineConfigurationApi.java
@@ -1,0 +1,23 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flowable.dmn.api;
+
+public interface DmnEngineConfigurationApi {
+
+    DmnRepositoryService getDmnRepositoryService();
+    
+    DmnRuleService getDmnRuleService();
+    
+    DmnManagementService getDmnManagementService();
+
+}

modules/flowable-dmn-engine-configurator/src/main/java/org/flowable/dmn/engine/configurator/DmnEngineConfigurator.java
@@ -56,13 +56,11 @@ public class DmnEngineConfigurator extends AbstractEngineConfigurator {
     public void configure(ProcessEngineConfigurationImpl processEngineConfiguration) {
         if (dmnEngineConfiguration == null) {
             dmnEngineConfiguration = new StandaloneInMemDmnEngineConfiguration();
-            initialiseCommonProperties(processEngineConfiguration, dmnEngineConfiguration, EngineConfigurationConstants.KEY_DMN_ENGINE_CONFIG);
         }
+        
+        initialiseCommonProperties(processEngineConfiguration, dmnEngineConfiguration, EngineConfigurationConstants.KEY_DMN_ENGINE_CONFIG);
 
-        DmnEngine dmnEngine = initDmnEngine();
-        processEngineConfiguration.setDmnEngineInitialized(true);
-        processEngineConfiguration.setDmnEngineRepositoryService(dmnEngine.getDmnRepositoryService());
-        processEngineConfiguration.setDmnEngineRuleService(dmnEngine.getDmnRuleService());
+        initDmnEngine();
     }
     
     @Override

commit 670d0e4c8a3c7f835596a3b948f377c0d0156486

[IDM] Fix a regression about LDAP integration introduced with commit 148f4f2 (Perhaps the LDAP integration is no more working)

modules/flowable-ldap-configurator/src/main/java/org/flowable/ldap/LDAPConfigurator.java
@@ -12,9 +12,9 @@
  */
 package org.flowable.ldap;
 
-import org.flowable.engine.cfg.AbstractProcessEngineConfigurator;
 import org.flowable.engine.cfg.ProcessEngineConfigurator;
 import org.flowable.engine.common.api.FlowableException;
+import org.flowable.engine.impl.cfg.IdmEngineConfigurator;
 import org.flowable.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.flowable.engine.impl.util.EngineServiceUtil;
 
@@ -28,7 +28,7 @@ import org.flowable.engine.impl.util.EngineServiceUtil;
  * 
  * @author Joram Barrez
  */
-public class LDAPConfigurator extends AbstractProcessEngineConfigurator {
+public class LDAPConfigurator extends IdmEngineConfigurator {
 
     protected LDAPConfiguration ldapConfiguration;
 
@@ -40,6 +40,8 @@ public class LDAPConfigurator extends AbstractProcessEngineConfigurator {
     @Override
     public void configure(ProcessEngineConfigurationImpl processEngineConfiguration) {
         
+        super.configure(processEngineConfiguration);
+
         if (ldapConfiguration == null) {
             throw new FlowableException("ldapConfiguration is not set");
         }

modules/flowable-ldap-configurator/src/test/resources/flowable-context.xml
@@ -28,37 +28,38 @@
 		<property name="dataSource" ref="dataSource" />
 		<property name="transactionManager" ref="transactionManager" />
 		<property name="databaseSchemaUpdate" value="true" />
-		<property name="idmProcessEngineConfigurator">
-		  <bean class="org.flowable.ldap.LDAPConfigurator">
-		    
-		    <property name="ldapConfiguration">
-		    	<bean class="org.flowable.ldap.LDAPConfiguration">
-		    		<!-- Server connection params -->
-		            <property name="server" value="ldap://localhost" />
-		            <property name="port" value="33389" />
-		            <property name="user" value="uid=admin, ou=users, o=flowable" />
-		            <property name="password" value="pass" />
-		            
-		            <!-- Query params -->
-		            <property name="baseDn" value="o=flowable" />
-		            <property name="queryUserByUserId" value="(&amp;(objectClass=inetOrgPerson)(uid={0}))" />
-		            <property name="queryUserByFullNameLike" value="(&amp;(objectClass=inetOrgPerson)(|({0}=*{1}*)({2}=*{3}*)))" />
-		            <property name="queryAllUsers" value="(objectClass=inetOrgPerson)" />
-		            <property name="queryGroupsForUser" value="(&amp;(objectClass=groupOfUniqueNames)(uniqueMember={0}))" />
-		            <property name="queryAllGroups" value="(objectClass=groupOfUniqueNames)" />
-		            
-		            <!-- Attribute config -->
-		            <property name="userIdAttribute" value="uid" />
-		            <property name="userFirstNameAttribute" value="cn" />
-		            <property name="userLastNameAttribute" value="sn" />
-		            <property name="userEmailAttribute" value="mail" />
-		            
-		            <property name="groupIdAttribute" value="uid" />
-		            <property name="groupNameAttribute" value="cn" />
-		    	</bean>
-		    </property>
-               
-	      </bean>
+		<property name="configurators">
+		  <list>
+      		  <bean class="org.flowable.ldap.LDAPConfigurator">
+      		    <property name="ldapConfiguration">
+      		    	<bean class="org.flowable.ldap.LDAPConfiguration">
+      		    		<!-- Server connection params -->
+      		            <property name="server" value="ldap://localhost" />
+      		            <property name="port" value="33389" />
+      		            <property name="user" value="uid=admin, ou=users, o=flowable" />
+      		            <property name="password" value="pass" />
+      		            
+      		            <!-- Query params -->
+      		            <property name="baseDn" value="o=flowable" />
+      		            <property name="queryUserByUserId" value="(&amp;(objectClass=inetOrgPerson)(uid={0}))" />
+      		            <property name="queryUserByFullNameLike" value="(&amp;(objectClass=inetOrgPerson)(|({0}=*{1}*)({2}=*{3}*)))" />
+      		            <property name="queryAllUsers" value="(objectClass=inetOrgPerson)" />
+      		            <property name="queryGroupsForUser" value="(&amp;(objectClass=groupOfUniqueNames)(uniqueMember={0}))" />
+      		            <property name="queryAllGroups" value="(objectClass=groupOfUniqueNames)" />
+      		            
+      		            <!-- Attribute config -->
+      		            <property name="userIdAttribute" value="uid" />
+      		            <property name="userFirstNameAttribute" value="cn" />
+      		            <property name="userLastNameAttribute" value="sn" />
+      		            <property name="userEmailAttribute" value="mail" />
+      		            
+      		            <property name="groupIdAttribute" value="uid" />
+      		            <property name="groupNameAttribute" value="cn" />
+      		    	</bean>
+      		    </property>
+                     
+      	      </bean>
+		  </list>
 		</property>
 	</bean>
 