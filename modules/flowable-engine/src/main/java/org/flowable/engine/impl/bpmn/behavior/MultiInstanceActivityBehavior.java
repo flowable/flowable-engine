da3a5704d1a2cba22ed2a03679f2eb64336dc60a
Lori Small <Lori.Small@sas.com>
2017-08-09 16:55:50 -0400

    520 Add support for revised syntax collectionString parsing function
    1st draft - hard-coded function prefix and method name

--- modules/flowable-engine/src/main/java/org/flowable/engine/impl/bpmn/behavior/MultiInstanceActivityBehavior.java
+++ modules/flowable-engine/src/main/java/org/flowable/engine/impl/bpmn/behavior/MultiInstanceActivityBehavior.java
@@ -18,6 +18,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.flowable.bpmn.model.Activity;
 import org.flowable.bpmn.model.BoundaryEvent;
 import org.flowable.bpmn.model.CompensateEventDefinition;
@@ -26,6 +27,7 @@ import org.flowable.bpmn.model.FlowNode;
 import org.flowable.bpmn.model.Process;
 import org.flowable.engine.DynamicBpmnConstants;
 import org.flowable.engine.common.api.FlowableIllegalArgumentException;
+import org.flowable.engine.common.api.delegate.FlowableFunctionDelegate;
 import org.flowable.engine.common.impl.util.CollectionUtil;
 import org.flowable.engine.delegate.BpmnError;
 import org.flowable.engine.delegate.DelegateExecution;
@@ -301,7 +303,7 @@ public abstract class MultiInstanceActivityBehavior extends FlowNodeActivityBeha
             }
             
         } else {
-            throw new FlowableIllegalArgumentException("Couldn't resolve collection expression nor variable reference");
+            throw new FlowableIllegalArgumentException("Couldn't resolve collection expression, variable reference or string");
             
         }
     }
@@ -314,8 +316,20 @@ public abstract class MultiInstanceActivityBehavior extends FlowNodeActivityBeha
         } else if (collectionVariable != null) {
             collection = execution.getVariable(collectionVariable);
         } else if (collectionString != null) {
-            // TODO: add dynamic function support for resolving an explicit String value collection here - issue 512
-        	// https://github.com/flowable/flowable-engine/issues/512
+            ProcessEngineConfigurationImpl processEngineConfiguration = CommandContextUtil.getProcessEngineConfiguration();
+            ExpressionManager expressionManager = processEngineConfiguration.getExpressionManager();
+
+            List<FlowableFunctionDelegate> functionDelegates = expressionManager.getFunctionDelegates();
+            for (FlowableFunctionDelegate delegate : functionDelegates) {
+            	delegate.localName();
+            }
+
+            // remove \n and \r characters
+            String escString = getCollectionString().replaceAll("\\r\\n|\\r|\\n"," ");
+            // escape the JSON string value because JUEL doesn't support '[]'
+            escString = StringEscapeUtils.escapeJson(escString);
+
+            collection = expressionManager.createExpression("${json:parseJson(\"" + escString + "\")}").getValue(execution);
         }
         return collection;
     }
