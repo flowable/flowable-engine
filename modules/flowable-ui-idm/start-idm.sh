b1747d09a07e1c8e64847c52f1352abd932dbb2a
Joram Barrez <jbarrez@users.noreply.github.com>
2017-09-23 17:46:03 +0200

    JDK 9 : fixes for booting up UI's

--- modules/flowable-ui-idm/start-idm.sh
+++ modules/flowable-ui-idm/start-idm.sh
@@ -10,6 +10,5 @@ then
 	export MAVEN_OPTS="$MAVEN_OPTS -noverify -Xms512m -Xmx1024m -Xdebug -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8002,server=y,suspend=n"
 	mvn clean tomcat7:run -Pmysql
 else
-	say -v Cellos "Dum dum dum dum dum dum dum he he he ho ho ho fa lah lah lah lah lah lah fa lah full hoo hoo hoo"
     echo "Error while building root pom. Halting."
 fi
