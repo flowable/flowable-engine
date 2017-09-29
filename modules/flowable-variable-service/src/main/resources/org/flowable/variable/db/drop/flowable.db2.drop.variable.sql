59ad181a5c0e976777a61d27ad01866c2ef94ef6
Tijs Rademakers <tijs.rademakers@gmail.com>
2017-09-29 15:54:02 +0200

    Changed index name due to length issue

--- modules/flowable-variable-service/src/main/resources/org/flowable/variable/db/drop/flowable.db2.drop.variable.sql
+++ modules/flowable-variable-service/src/main/resources/org/flowable/variable/db/drop/flowable.db2.drop.variable.sql
@@ -1,5 +1,5 @@
 drop index ACT_IDX_RU_VAR_SCOPE_ID_TYPE;
-drop index ACT_IDX_RU_VAR_SUB_SCOPE_ID_TYPE;
+drop index ACT_IDX_RU_VAR_SUB_ID_TYPE;
 
 alter table ACT_RU_VARIABLE
     drop foreign key ACT_FK_VAR_BYTEARRAY;
