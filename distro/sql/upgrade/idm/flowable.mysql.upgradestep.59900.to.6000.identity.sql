e2569c77dd4bccbd30c9f9da7bcc6dca473453c0
Tijs Rademakers <tijs.rademakers@gmail.com>
2017-10-13 13:45:57 +0200

    Distro build fixes

---
+++ distro/sql/upgrade/idm/flowable.mysql.upgradestep.59900.to.6000.identity.sql
@@ -0,0 +1,52 @@
+create table ACT_ID_PROPERTY (
+    NAME_ varchar(64),
+    VALUE_ varchar(300),
+    REV_ integer,
+    primary key (NAME_)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+insert into ACT_ID_PROPERTY
+values ('schema.version', '6.0.0.0', 1);
+
+create table ACT_ID_BYTEARRAY (
+    ID_ varchar(64),
+    REV_ integer,
+    NAME_ varchar(255),
+    BYTES_ LONGBLOB,
+    primary key (ID_)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+create table ACT_ID_TOKEN (
+    ID_ varchar(64) not null,
+    REV_ integer,
+    TOKEN_VALUE_ varchar(255),
+    TOKEN_DATE_ timestamp(3),
+    IP_ADDRESS_ varchar(255),
+    USER_AGENT_ varchar(255),
+    USER_ID_ varchar(255),
+    TOKEN_DATA_ varchar(2000),
+    primary key (ID_)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+create table ACT_ID_PRIV (
+    ID_ varchar(64) not null,
+    NAME_ varchar(255),
+    primary key (ID_)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+create table ACT_ID_PRIV_MAPPING (
+    ID_ varchar(64) not null,
+    PRIV_ID_ varchar(64) not null,
+    USER_ID_ varchar(255),
+    GROUP_ID_ varchar(255),
+    primary key (ID_)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+alter table ACT_ID_PRIV_MAPPING 
+    add constraint ACT_FK_PRIV_MAPPING 
+    foreign key (PRIV_ID_) 
+    references ACT_ID_PRIV (ID_);
+    
+create index ACT_IDX_PRIV_USER on ACT_ID_PRIV_MAPPING(USER_ID_);
+create index ACT_IDX_PRIV_GROUP on ACT_ID_PRIV_MAPPING(GROUP_ID_);   
+
