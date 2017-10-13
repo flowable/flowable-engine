db95fb6a2fa2114eac2ba66b14a73f9534f1b261
Martin Grofcik <martin.grofcik@edorasware.com>
2017-10-13 16:31:19 +0200

    default process - DMN engine expression manager configuration
    (cherry picked from commit c65d36e)

--- modules/flowable-dmn-spring-configurator/src/main/java/org/flowable/dmn/spring/configurator/SpringDmnEngineConfigurator.java
+++ modules/flowable-dmn-spring-configurator/src/main/java/org/flowable/dmn/spring/configurator/SpringDmnEngineConfigurator.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,14 +34,13 @@ public class SpringDmnEngineConfigurator extends DmnEngineConfigurator {
             dmnEngineConfiguration = new SpringDmnEngineConfiguration();
         }
         initialiseCommonProperties(processEngineConfiguration, dmnEngineConfiguration);
-        
+
         SpringProcessEngineConfiguration springProcessEngineConfiguration = (SpringProcessEngineConfiguration) processEngineConfiguration;
         dmnEngineConfiguration.setTransactionManager(springProcessEngineConfiguration.getTransactionManager());
-        dmnEngineConfiguration.setExpressionManager(new SpringDmnExpressionManager(
-                        springProcessEngineConfiguration.getApplicationContext(), springProcessEngineConfiguration.getBeans()));
+        dmnEngineConfiguration.setExpressionManager(dmnEngineConfiguration.getExpressionManager());
 
         initDmnEngine();
-        
+
         initServiceConfigurations(processEngineConfiguration, dmnEngineConfiguration);
     }
 
