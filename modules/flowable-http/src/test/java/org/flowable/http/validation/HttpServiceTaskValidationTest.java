6036fc6d2e46435bebb4eb36530ab407557a7933
Martin Grofčík <martin.grofcik@edorasware.com>
2017-11-28 13:01:07 +0100

    Http fix (#672)
    * http fix
    
    (cherry picked from commit 2118f33)
    
    * http fix CR changes

--- modules/flowable-http/src/test/java/org/flowable/http/validation/HttpServiceTaskValidationTest.java
+++ modules/flowable-http/src/test/java/org/flowable/http/validation/HttpServiceTaskValidationTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,16 +12,19 @@
  */
 package org.flowable.http.validation;
 
-import static org.flowable.http.HttpActivityBehavior.HTTP_TASK_REQUEST_FIELD_INVALID;
-import static org.flowable.http.HttpActivityBehavior.HTTP_TASK_REQUEST_HEADERS_INVALID;
-import static org.flowable.http.HttpActivityBehavior.HTTP_TASK_REQUEST_METHOD_INVALID;
+import org.flowable.engine.common.api.FlowableException;
+import org.flowable.engine.test.Deployment;
+import org.flowable.http.HttpServiceTaskTestCase;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.flowable.engine.common.api.FlowableException;
-import org.flowable.engine.test.Deployment;
-import org.flowable.http.HttpServiceTaskTestCase;
+import static org.flowable.http.HttpActivityBehavior.HTTP_TASK_REQUEST_FIELD_INVALID;
+import static org.flowable.http.HttpActivityBehavior.HTTP_TASK_REQUEST_HEADERS_INVALID;
+import static org.flowable.http.HttpActivityBehavior.HTTP_TASK_REQUEST_METHOD_INVALID;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * @author Harsha Teja Kanna
@@ -56,7 +59,7 @@ public class HttpServiceTaskValidationTest extends HttpServiceTaskTestCase {
             fail("FlowableException expected");
         } catch (Exception e) {
             assertTrue(e instanceof FlowableException);
-            assertEquals(HTTP_TASK_REQUEST_FIELD_INVALID, e.getMessage());
+            assertThat(e.getMessage(), is("String value \"Accept application/json\" is not alloved in boolean expression"));
         }
     }
 
