cdf5f616a5972559af276ee41749fb9dc7bdae86
Martin Grofcik <martin.grofcik@edorasware.com>
2017-11-29 13:41:26 +0100

    cleanup

--- modules/flowable-dmn-engine-configurator/src/test/java/org/flowable/cmmn/test/runtime/DecisionTaskTest.java
+++ modules/flowable-dmn-engine-configurator/src/test/java/org/flowable/cmmn/test/runtime/DecisionTaskTest.java
@@ -25,7 +25,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
