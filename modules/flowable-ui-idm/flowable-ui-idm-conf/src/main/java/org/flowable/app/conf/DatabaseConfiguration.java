00783285f4016f9c3ba85e5cb1eaed597da5fc3d
Pascal Schumacher <pascalschumacher@gmx.net>
2017-11-29 18:19:20 +0100

    simplify DatabaseConfiguration classes by using PropertyResolver#getProperty(String key, Class<T> targetType, T defaultValue)

--- modules/flowable-ui-idm/flowable-ui-idm-conf/src/main/java/org/flowable/app/conf/DatabaseConfiguration.java
+++ modules/flowable-ui-idm/flowable-ui-idm-conf/src/main/java/org/flowable/app/conf/DatabaseConfiguration.java
@@ -60,42 +60,21 @@ public class DatabaseConfiguration {
             String dataSourceUsername = env.getProperty("datasource.username", "sa");
             String dataSourcePassword = env.getProperty("datasource.password", "");
 
-            Integer minPoolSize = env.getProperty("datasource.min-pool-size", Integer.class);
-            if (minPoolSize == null) {
-                minPoolSize = 10;
-            }
+            Integer minPoolSize = env.getProperty("datasource.min-pool-size", Integer.class, 10);
 
-            Integer maxPoolSize = env.getProperty("datasource.max-pool-size", Integer.class);
-            if (maxPoolSize == null) {
-                maxPoolSize = 100;
-            }
+            Integer maxPoolSize = env.getProperty("datasource.max-pool-size", Integer.class, 100);
 
-            Integer acquireIncrement = env.getProperty("datasource.acquire-increment", Integer.class);
-            if (acquireIncrement == null) {
-                acquireIncrement = 5;
-            }
+            Integer acquireIncrement = env.getProperty("datasource.acquire-increment", Integer.class, 5);
 
             String preferredTestQuery = env.getProperty("datasource.preferred-test-query");
 
-            Boolean testConnectionOnCheckin = env.getProperty("datasource.test-connection-on-checkin", Boolean.class);
-            if (testConnectionOnCheckin == null) {
-                testConnectionOnCheckin = true;
-            }
+            Boolean testConnectionOnCheckin = env.getProperty("datasource.test-connection-on-checkin", Boolean.class, true);
 
-            Boolean testConnectionOnCheckOut = env.getProperty("datasource.test-connection-on-checkout", Boolean.class);
-            if (testConnectionOnCheckOut == null) {
-                testConnectionOnCheckOut = true;
-            }
+            Boolean testConnectionOnCheckOut = env.getProperty("datasource.test-connection-on-checkout", Boolean.class, true);
 
-            Integer maxIdleTime = env.getProperty("datasource.max-idle-time", Integer.class);
-            if (maxIdleTime == null) {
-                maxIdleTime = 1800;
-            }
+            Integer maxIdleTime = env.getProperty("datasource.max-idle-time", Integer.class, 1800);
 
-            Integer maxIdleTimeExcessConnections = env.getProperty("datasource.max-idle-time-excess-connections", Integer.class);
-            if (maxIdleTimeExcessConnections == null) {
-                maxIdleTimeExcessConnections = 1800;
-            }
+            Integer maxIdleTimeExcessConnections = env.getProperty("datasource.max-idle-time-excess-connections", Integer.class, 1800);
 
             if (LOGGER.isInfoEnabled()) {
                 LOGGER.info("Configuring Datasource with following properties (omitted password for security)");
