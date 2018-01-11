267f29cd5e0ae17235d286b5e3b410cd6d7cc26c
Seif El Deen Khaled <skhaled@Seifs-MacBook-Pro.local>
2018-01-11 18:11:38 +0200

    Issue#719: changing business process model name and key does not change process name and identifier

--- modules/flowable-ui-modeler/flowable-ui-modeler-rest/src/main/java/org/flowable/app/rest/editor/ModelResource.java
+++ modules/flowable-ui-modeler/flowable-ui-modeler-rest/src/main/java/org/flowable/app/rest/editor/ModelResource.java
@@ -102,10 +102,21 @@ public class ModelResource {
         try {
             updatedModel.updateModel(model);
             
-            if (model.getModelType() != null && (Model.MODEL_TYPE_DECISION_TABLE == model.getModelType() || Model.MODEL_TYPE_FORM == model.getModelType())) {
+            if (model.getModelType() != null) {
                 ObjectNode modelNode = (ObjectNode) objectMapper.readTree(model.getModelEditorJson());
                 modelNode.put("name", model.getName());
                 modelNode.put("key", model.getKey());
+
+                if(Model.MODEL_TYPE_BPMN == model.getModelType()) {
+                    ObjectNode propertiesNode = (ObjectNode) modelNode.get("properties");
+                    String processId = model.getName().replaceAll(" ", "");
+                    propertiesNode.put("process_id", processId);
+                    propertiesNode.put("name", model.getName());
+                    if (StringUtils.isNotEmpty(model.getDescription())) {
+                        propertiesNode.put("documentation", model.getDescription());
+                    }
+                    modelNode.set("properties", propertiesNode);
+                }
                 model.setModelEditorJson(modelNode.toString());
             }
             
