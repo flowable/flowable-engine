beb9edc6c14bfbe840b04644628ce436588ef401
Martin Grofčík <martin.grofcik@edorasware.com>
2018-02-02 15:51:53 +0100

    variable service update methods (#769)

--- modules/flowable5-engine/src/main/java/org/activiti/engine/delegate/event/impl/ActivitiVariableEventImpl.java
+++ modules/flowable5-engine/src/main/java/org/activiti/engine/delegate/event/impl/ActivitiVariableEventImpl.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +18,7 @@ import org.flowable.variable.api.types.VariableType;
 
 /**
  * Implementation of {@link FlowableVariableEvent}.
- * 
+ *
  * @author Frederik Heremans
  */
 public class ActivitiVariableEventImpl extends ActivitiEventImpl implements FlowableVariableEvent {
@@ -27,6 +27,8 @@ public class ActivitiVariableEventImpl extends ActivitiEventImpl implements Flow
     protected Object variableValue;
     protected VariableType variableType;
     protected String taskId;
+    protected String scopeId;
+    protected String scopeType;
 
     public ActivitiVariableEventImpl(FlowableEngineEventType type) {
         super(type);
@@ -68,4 +70,21 @@ public class ActivitiVariableEventImpl extends ActivitiEventImpl implements Flow
         this.taskId = taskId;
     }
 
+    @Override
+    public String getScopeId() {
+        return scopeId;
+    }
+
+    public void setScopeId(String scopeId) {
+        this.scopeId = scopeId;
+    }
+
+    @Override
+    public String getScopeType() {
+        return scopeType;
+    }
+
+    public void setScopeType(String scopeType) {
+        this.scopeType = scopeType;
+    }
 }
