9c04dfd2bae734f668002a85b3cfab2e864ef5d6
Filip Hrisafov <filip.hrisafov@gmail.com>
2018-02-24 19:02:57 +0100

    Add Sample application with the form engine

---
+++ modules/flowable-spring-boot/flowable-spring-boot-samples/flowable-spring-boot-sample-form/src/main/java/flowable/FormSampleApplication.java
@@ -0,0 +1,12 @@
+package flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormSampleApplication.class, args);
+    }
+}
