1c79fd4b2f1c21ffed0e4b76345143478755dd32
Filip Hrisafov <filip.hrisafov@gmail.com>
2018-02-24 14:12:38 +0100

    Expose form services as beans and add support for standalone form engine

---
+++ modules/flowable-spring-boot/flowable-spring-boot-starters/flowable-spring-boot-starter-basic/src/main/java/org/flowable/spring/boot/FormEngineServicesAutoConfiguration.java
@@ -0,0 +1,71 @@
+package org.flowable.spring.boot;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.form.api.FormEngineConfigurationApi;
+import org.flowable.form.api.FormManagementService;
+import org.flowable.form.api.FormRepositoryService;
+import org.flowable.form.api.FormService;
+import org.flowable.form.engine.FormEngine;
+import org.flowable.form.engine.FormEngineConfiguration;
+import org.flowable.form.spring.FormEngineFactoryBean;
+import org.flowable.form.spring.configurator.SpringFormEngineConfigurator;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Auto configuration for the form engine.
+ *
+ * @author Filip Hrisafov
+ * @author Javier Casal
+ */
+@Configuration
+@ConditionalOnClass({
+    SpringFormEngineConfigurator.class
+})
+@ConditionalOnProperty(prefix = "flowable.form", name = "enabled", havingValue = "true", matchIfMissing = true)
+@EnableConfigurationProperties({
+    FlowableProperties.class,
+    FlowableFormProperties.class
+})
+@AutoConfigureAfter({
+    FormEngineAutoConfiguration.class,
+    DataSourceProcessEngineAutoConfiguration.class,
+    JpaProcessEngineAutoConfiguration.class
+})
+public class FormEngineServicesAutoConfiguration {
+
+    /**
+     * If there is no process engine configuration, then trigger a creation of the form engine.
+     */
+    @Bean
+    @ConditionalOnMissingBean({
+        FormEngine.class,
+        ProcessEngine.class
+    })
+    public FormEngineFactoryBean formEngine(FormEngineConfiguration formEngineConfiguration) {
+        FormEngineFactoryBean factory = new FormEngineFactoryBean();
+        factory.setFormEngineConfiguration(formEngineConfiguration);
+        return factory;
+    }
+
+    @Bean
+    public FormService formService(FormEngineConfigurationApi formEngine) {
+        return formEngine.getFormService();
+    }
+
+    @Bean
+    public FormRepositoryService formRepositoryService(FormEngineConfigurationApi formEngine) {
+        return formEngine.getFormRepositoryService();
+    }
+
+    @Bean
+    public FormManagementService formManagementService(FormEngineConfigurationApi formEngine) {
+        return formEngine.getFormManagementService();
+    }
+}
+
