f61a9db4fcd3fbe1b117987df4bb3819130a7dd4
Tijs Rademakers <tijs.rademakers@gmail.com>
2018-03-07 14:28:25 +0100

    Fix osgi issues

--- modules/flowable-engine-common/src/main/java/org/flowable/engine/common/impl/ScriptingEngineAwareEngineConfiguration.java
+++ modules/flowable-engine-common/src/main/java/org/flowable/engine/common/impl/ScriptingEngineAwareEngineConfiguration.java
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.flowable.engine.common;
+package org.flowable.engine.common.impl;
 
-import org.flowable.engine.common.impl.AbstractEngineConfiguration;
 import org.flowable.engine.common.impl.scripting.ScriptingEngines;
 
 /**
