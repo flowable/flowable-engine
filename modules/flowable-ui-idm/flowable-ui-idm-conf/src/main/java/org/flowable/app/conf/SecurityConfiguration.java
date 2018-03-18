264c79caca552728ebc6c3a76bc880611cc33948
Filip Hrisafov <filip.hrisafov@gmail.com>
2018-03-18 17:51:22 +0100

    Remove EnableWebSecurity to allow default kick in from the Spring Boot security configuration

--- modules/flowable-ui-idm/flowable-ui-idm-conf/src/main/java/org/flowable/app/conf/SecurityConfiguration.java
+++ modules/flowable-ui-idm/flowable-ui-idm-conf/src/main/java/org/flowable/app/conf/SecurityConfiguration.java
@@ -36,7 +36,6 @@ import org.springframework.security.authentication.RememberMeAuthenticationProvi
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -51,7 +50,6 @@ import org.springframework.security.web.header.writers.XXssProtectionHeaderWrite
  * @author Tijs Rademakers
  */
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
 
