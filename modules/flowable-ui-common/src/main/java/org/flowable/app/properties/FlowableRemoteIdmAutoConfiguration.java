89f769d58a4d9cc1b9de57e6fa9123af7ee71cf3
Filip Hrisafov <filip.hrisafov@gmail.com>
2018-03-28 17:19:55 +0200

    Use Spring Boot properties for the tenant id property

--- modules/flowable-ui-common/src/main/java/org/flowable/app/properties/FlowableRemoteIdmAutoConfiguration.java
+++ modules/flowable-ui-common/src/main/java/org/flowable/app/properties/FlowableRemoteIdmAutoConfiguration.java
@@ -20,6 +20,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @EnableConfigurationProperties({
     FlowableRemoteIdmProperties.class,
+    FlowableCommonAppProperties.class,
     FlowableRestAppProperties.class
 })
 @Configuration
