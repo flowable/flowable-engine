b303764407c31fa6bbf377bb7b3bddc0c059b78d
Pascal Schumacher <pascalschumacher@gmx.net>
2018-04-03 08:47:26 +0200

    Upgrade Mockito version to 2.18.0
    Unused stubbing is reported as an error by Mockito 2, so remove it were possible and make Mockito ignore it where easy removal is not possible.

--- modules/flowable-dmn-spring/src/test/java/org/flowable/spring/test/autodeployment/DefaultAutoDeploymentStrategyTest.java
+++ modules/flowable-dmn-spring/src/test/java/org/flowable/spring/test/autodeployment/DefaultAutoDeploymentStrategyTest.java
@@ -17,8 +17,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isA;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -32,13 +32,13 @@ import org.flowable.engine.common.api.FlowableException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.core.io.Resource;
 
 /**
  * @author Tiese Barrell
  */
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class DefaultAutoDeploymentStrategyTest extends AbstractAutoDeploymentStrategyTest {
 
     private DefaultAutoDeploymentStrategy classUnderTest;
--- modules/flowable-dmn-spring/src/test/java/org/flowable/spring/test/autodeployment/ResourceParentFolderAutoDeploymentStrategyTest.java
+++ modules/flowable-dmn-spring/src/test/java/org/flowable/spring/test/autodeployment/ResourceParentFolderAutoDeploymentStrategyTest.java
@@ -16,8 +16,8 @@ package org.flowable.spring.test.autodeployment;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isA;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -33,7 +33,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.core.io.Resource;
 
 /**
@@ -150,11 +150,6 @@ public class ResourceParentFolderAutoDeploymentStrategyTest extends AbstractAuto
 
         final Resource[] resources = new Resource[] { resourceMock1, resourceMock2, resourceMock3 };
         classUnderTest.deployResources(deploymentNameHint, resources, repositoryServiceMock);
-
-        when(fileMock1.getParentFile()).thenReturn(null);
-        when(fileMock2.getParentFile()).thenReturn(parentFile2Mock);
-        when(parentFile2Mock.isDirectory()).thenReturn(false);
-        when(fileMock3.getParentFile()).thenReturn(null);
 
         verify(repositoryServiceMock, times(3)).createDeployment();
         verify(deploymentBuilderMock, times(3)).enableDuplicateFiltering();
