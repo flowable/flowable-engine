d868fc45d05a294c717bd2ec2703b9a73a1fdb03
Yvo Swillens <yvoswillens@gmail.com>
2018-04-06 10:59:21 +0200

    removed unused profiles and changed dev profile to mysql

--- modules/flowable-ui-admin/start.sh
+++ modules/flowable-ui-admin/start.sh
@@ -8,7 +8,7 @@ then
     # Run war
     echo "Running war file"
     export MAVEN_OPTS="$MAVEN_OPTS -noverify -Xms512m -Xmx1024m -Xdebug -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=9000,server=y,suspend=n"
-    mvn clean install -Pdev spring-boot:run
+    mvn clean install -Pmysql spring-boot:run
 else
     say -v Cellos "Dum dum dum dum dum dum dum he he he ho ho ho fa lah lah lah lah lah lah fa lah full hoo hoo hoo"
     echo "Error while building root pom. Halting."
