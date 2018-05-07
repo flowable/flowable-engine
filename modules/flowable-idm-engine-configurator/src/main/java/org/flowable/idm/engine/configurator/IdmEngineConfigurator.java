f8922dda061d6eb7e27de05f92fc1e4bfc30df60
Filip Hrisafov <filip.hrisafov@gmail.com>
2018-05-07 18:15:51 +0200

    Extract IdmConfigurators to their own modules and remove specific ones from App, CMMN and Process engines

--- modules/flowable-idm-engine-configurator/src/main/java/org/flowable/idm/engine/configurator/IdmEngineConfigurator.java
+++ modules/flowable-idm-engine-configurator/src/main/java/org/flowable/idm/engine/configurator/IdmEngineConfigurator.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.flowable.app.engine.impl.cfg;
+package org.flowable.idm.engine.configurator;
 
 import java.util.List;
 
