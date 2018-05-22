0744c65b40ff7383620bbf6bbc6eb13acf399f49
Joram Barrez <jbarrez@users.noreply.github.com>
2018-05-22 12:38:53 +0200

    Add generated sql files for 6.3.1

---
+++ distro/sql/upgrade/bpmn/flowable.oracle.upgradestep.6301.to.6310.engine.sql
@@ -0,0 +1,13 @@
+update ACT_GE_PROPERTY set VALUE_ = '6.3.1.0' where NAME_ = 'identitylink.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '6.3.1.0' where NAME_ = 'task.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '6.3.1.0' where NAME_ = 'variable.schema.version';
+
+alter table ACT_RU_HISTORY_JOB add SCOPE_TYPE_ NVARCHAR2(255);
+
+update ACT_GE_PROPERTY set VALUE_ = '6.3.1.0' where NAME_ = 'job.schema.version';
+alter table ACT_RE_DEPLOYMENT add PARENT_DEPLOYMENT_ID_ NVARCHAR2(255);
+
+update ACT_GE_PROPERTY set VALUE_ = '6.3.1.0' where NAME_ = 'schema.version';
+
