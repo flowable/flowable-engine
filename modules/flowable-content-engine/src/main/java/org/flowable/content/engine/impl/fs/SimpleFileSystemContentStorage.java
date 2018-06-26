8289d14d21d91eb5cb594695641f9f7c041d0ea1
Martin Grofcik <martin.grofcik@edorasware.com>
2018-06-26 11:38:08 +0200

    #1099(feat): SimpleFileSystemContentStorage reverting refactoring changes

--- modules/flowable-content-engine/src/main/java/org/flowable/content/engine/impl/fs/SimpleFileSystemContentStorage.java
+++ modules/flowable-content-engine/src/main/java/org/flowable/content/engine/impl/fs/SimpleFileSystemContentStorage.java
@@ -12,33 +12,11 @@
  */
 package org.flowable.content.engine.impl.fs;
 
-import static java.util.stream.Collectors.toList;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
-
-import org.apache.commons.io.FileUtils;
+import com.fasterxml.uuid.EthernetAddress;
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedGenerator;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.filefilter.AndFileFilter;
-import org.apache.commons.io.filefilter.DirectoryFileFilter;
-import org.apache.commons.io.filefilter.FalseFileFilter;
-import org.apache.commons.io.filefilter.IOFileFilter;
-import org.apache.commons.io.filefilter.NameFileFilter;
-import org.apache.commons.io.filefilter.NotFileFilter;
-import org.apache.commons.io.filefilter.OrFileFilter;
 import org.apache.commons.lang3.StringUtils;
-import org.flowable.common.engine.api.FlowableException;
 import org.flowable.common.engine.api.FlowableObjectNotFoundException;
 import org.flowable.content.api.ContentMetaDataKeys;
 import org.flowable.content.api.ContentObject;
@@ -47,9 +25,11 @@ import org.flowable.content.api.ContentStorageException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.uuid.EthernetAddress;
-import com.fasterxml.uuid.Generators;
-import com.fasterxml.uuid.impl.TimeBasedGenerator;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 /**
  * (Very) simple implementation of the {@link ContentStorage} that relies on the passed metadata to store content.
@@ -73,30 +53,17 @@ public class SimpleFileSystemContentStorage implements ContentStorage {
     public static final String TYPE_PROCESS_INSTANCE = "process-instance-content";
     public static final String TYPE_CASE_INSTANCE = "cmmn";
     public static final String TYPE_UNCATEGORIZED = "uncategorized";
-    private static final List<IOFileFilter> DEFAULT_DIRECTORY_NAMES_FILTER = Collections.unmodifiableList(
-        Stream.of(
-            TYPE_TASK, TYPE_PROCESS_INSTANCE, TYPE_CASE_INSTANCE, TYPE_UNCATEGORIZED
-        )
-            .map(NameFileFilter::new)
-            .collect(toList())
-    );
+
     public static final String TASK_PREFIX = "task";
     public static final String PROCESS_INSTANCE_PREFIX = "proc";
     public static final String CASE_PREFIX = "case";
     public static final String UNCATEGORIZED_PREFIX = "uncategorized";
-    private static final Function<Map<String, Object>, String> GET_SCOPE_ID_FROM_METADATA = metaData -> {
-        String scopeId = (String) metaData.get(ContentMetaDataKeys.SCOPE_ID);
-        return StringUtils.isNotEmpty(scopeId) ? scopeId : "";
-    };
-    protected static final BiFunction<String, Map<String, Object>, String> GENERATE_CONTENTID_FOR_CASE = (uuid, metaData) -> CASE_PREFIX + "." + metaData
-        .get(ContentMetaDataKeys.SCOPE_ID) + "." + uuid;
-    protected static final BiFunction<String, Map<String, Object>, String> GENERATE_CONTENTID_FOR_SCOPE = (uuid, metaData) -> metaData
-        .get(ContentMetaDataKeys.SCOPE_TYPE) + "." + metaData
-        .get(ContentMetaDataKeys.SCOPE_ID) + "." + uuid;
 
     protected File contentFolderRoot;
-    protected List<ContentTypeHandler> contentTypeHandlers;
-    protected ContentTypeHandler uncategorizedContentTypeHandler;
+    protected File taskFolder;
+    protected File processInstanceFolder;
+    protected File caseFolder;
+    protected File uncategorizedFolder;
 
     public SimpleFileSystemContentStorage(File contentFolderRoot) {
         this.contentFolderRoot = contentFolderRoot;
@@ -104,52 +71,10 @@ public class SimpleFileSystemContentStorage implements ContentStorage {
     }
 
     protected void validateOrCreateSubfolders() {
-        this.uncategorizedContentTypeHandler = ContentTypeHandler.of(TYPE_UNCATEGORIZED, validateOrCreateFolder(TYPE_UNCATEGORIZED),
-            metaData -> "",
-            (uuid, metaData) -> UNCATEGORIZED_PREFIX + "." + uuid
-        );
-        this.contentTypeHandlers = refreshTypeFolders();
-    }
-
-    protected List<ContentTypeHandler> refreshTypeFolders() {
-        return Stream.concat(
-            Stream.concat(
-                // default folders without UNCATEGORIZED
-                Stream.of(
-                    ContentTypeHandler.of(TASK_PREFIX, validateOrCreateFolder(TYPE_TASK),
-                        metaData -> (String) metaData.get(ContentMetaDataKeys.TASK_ID),
-                        (uuid, metaData) -> TASK_PREFIX + "." + metaData.get(ContentMetaDataKeys.TASK_ID) + "." + uuid),
-                    ContentTypeHandler.of(PROCESS_INSTANCE_PREFIX, validateOrCreateFolder(TYPE_PROCESS_INSTANCE),
-                        metaData -> (String) metaData.get(ContentMetaDataKeys.PROCESS_INSTANCE_ID),
-                        (uuid, metaData) -> PROCESS_INSTANCE_PREFIX + "." + metaData.get(ContentMetaDataKeys.PROCESS_INSTANCE_ID) + "." + uuid),
-                    ContentTypeHandler.of(CASE_PREFIX, validateOrCreateFolder(TYPE_CASE_INSTANCE),
-                        GET_SCOPE_ID_FROM_METADATA,
-                        GENERATE_CONTENTID_FOR_CASE)
-                ),
-                // all directories without defaults
-                FileUtils.listFilesAndDirs(contentFolderRoot,
-                    FalseFileFilter.INSTANCE,
-                    new AndFileFilter(
-                        DirectoryFileFilter.INSTANCE,
-                        new NotFileFilter(
-                            new OrFileFilter(
-                                DEFAULT_DIRECTORY_NAMES_FILTER
-                            )
-                        )
-                    )
-                )
-                    .stream()
-                    .map(directory -> ContentTypeHandler.of(directory.getName(), directory,
-                        GET_SCOPE_ID_FROM_METADATA,
-                        GENERATE_CONTENTID_FOR_SCOPE
-
-                    ))
-            )
-                .sorted(Comparator.comparingInt(contentTypeHandler -> contentTypeHandler.getType().length()))
-            ,
-            // with uncategorized folder at the end
-            Stream.of(this.uncategorizedContentTypeHandler)
-        ).collect(toList());
+        taskFolder = validateOrCreateFolder(TYPE_TASK);
+        processInstanceFolder = validateOrCreateFolder(TYPE_PROCESS_INSTANCE);
+        caseFolder = validateOrCreateFolder(TYPE_CASE_INSTANCE);
+        uncategorizedFolder = validateOrCreateFolder(TYPE_UNCATEGORIZED);
     }
 
     protected File validateOrCreateFolder(String folderName) {
@@ -165,55 +90,52 @@ public class SimpleFileSystemContentStorage implements ContentStorage {
         return subFolder;
     }
 
-    protected File createFolder(String folderName) {
-        File subFolder = new File(contentFolderRoot, folderName);
-        if (!subFolder.exists()) {
-            boolean created = subFolder.mkdir();
-            if (created) {
-                LOGGER.info("Created content folder in {}", subFolder.getAbsolutePath());
-            } else {
-                LOGGER.warn("Could not create content folder. This might impact the storage of related content");
-            }
-        } else {
-            throw new FlowableException("Folder already exists [" + folderName + "]");
-        }
-        return subFolder;
-    }
-
     @Override
     public ContentObject createContentObject(InputStream contentStream, Map<String, Object> metaData) {
         String uuid = UUID_GENERATOR.generate().toString();
-        ContentTypeHandler contentTypeHandler = getOrCreateContentTypeHandler(metaData);
-        File file = getContentFile(metaData, uuid, contentTypeHandler);
-        return storeContentToFile(contentStream, metaData, uuid, contentTypeHandler, file);
-    }
-
-    protected ContentObject storeContentToFile(InputStream contentStream, Map<String, Object> metaData, String uuid,
-        ContentTypeHandler contentTypeHandler, File file) {
+        File file = getContentFile(metaData, uuid);
         try (FileOutputStream fos = new FileOutputStream(file, false)) {
             long length = IOUtils.copy(contentStream, fos);
-            String contentId = contentTypeHandler.getGenerateContentId().apply(uuid, metaData);
+            String contentId = generateContentId(uuid, metaData);
             return new FileSystemContentObject(file, contentId, length);
         } catch (IOException e) {
             throw new ContentStorageException("Could not write content to " + file.getAbsolutePath(), e);
         }
     }
 
+    protected String generateContentId(String uuid, Map<String, Object> metaData) {
+        String contentId = "";
+        String type = determineType(metaData);
+        switch (type) {
+            case TYPE_PROCESS_INSTANCE:
+                String processInstanceId = (String) metaData.get(ContentMetaDataKeys.PROCESS_INSTANCE_ID);
+                contentId = PROCESS_INSTANCE_PREFIX + "." + processInstanceId;
+                break;
+
+            case TYPE_TASK:
+                String taskId = (String) metaData.get(ContentMetaDataKeys.TASK_ID);
+                contentId = TASK_PREFIX + "." + taskId;
+                break;
+
+            case TYPE_CASE_INSTANCE:
+                String caseId = (String) metaData.get(ContentMetaDataKeys.SCOPE_ID);
+                contentId = CASE_PREFIX + "." + caseId;
+                break;
+
+            case TYPE_UNCATEGORIZED:
+                contentId = type;
+                break;
+
+            default:
+                contentId = type + "." + metaData.get(ContentMetaDataKeys.SCOPE_ID);
+        }
+        contentId += "." + uuid;
+        return contentId;
+    }
+
     @Override
     public ContentObject updateContentObject(String id, InputStream contentStream, Map<String, Object> metaData) {
-        String[] ids = id.split("\\.");
-
-        File contentFile = getContentFile(ids, subFolder -> {
-                File idFolder = new File(subFolder.getFolder(), ids[1]);
-            return new File(idFolder, ids[2]);
-            }, () ->
-            {
-                File subFolder = createFolder(ids[0]);
-                refreshTypeFolders();
-                File idFolder = new File(subFolder, ids[1]);
-                return new File(idFolder, ids[2]);
-            }
-        );
+        File contentFile = getContentFile(id);
 
         // Write stream to a temporary file and rename when content is read
         // successfully to prevent overriding existing file and failing or keeping existing file
@@ -267,37 +189,36 @@ public class SimpleFileSystemContentStorage implements ContentStorage {
 
     @Override
     public ContentObject getContentObject(String id) {
-        String[] ids = id.split("\\.");
-        return new FileSystemContentObject(getContentFile(ids,
-            subFolder -> {
-                File contentFile = new File(subFolder.getFolder(), ids[1]);
-                if (ids.length == 3) {
-                    contentFile = new File(contentFile, ids[2]);
-                }
-                if (!contentFile.exists()) {
-                    throw new FlowableObjectNotFoundException("No content found for id " + Arrays.toString(ids));
-                }
-                return contentFile;
-            },
-            () -> {
-                File contentFile = new File(uncategorizedContentTypeHandler.getFolder(), ids[1]);
-                if (!contentFile.exists()) {
-                    throw new FlowableObjectNotFoundException("No content found for id " + Arrays.toString(ids));
-                }
-                return contentFile;
-            }), id);
+        return new FileSystemContentObject(getContentFile(id), id);
     }
 
-    protected File getContentFile(String[] ids,
-        Function<ContentTypeHandler, File> recognizedContentTypeFunction,
-        Supplier<File> UnrecognizedContentTypeSupplier) {
-        String type = ids[0];
+    protected File getContentFile(String id) {
+        String[] ids = id.split("\\.");
+        String typePrefix = ids[0];
+        File contentFile;
 
-        return this.contentTypeHandlers.stream()
-            .filter(typeFolder -> type.equals(typeFolder.getType()))
-            .findFirst()
-            .map(recognizedContentTypeFunction)
-            .orElseGet(UnrecognizedContentTypeSupplier);
+        if (!UNCATEGORIZED_PREFIX.equals(typePrefix)) {
+            File subFolder=null;
+            if (PROCESS_INSTANCE_PREFIX.equals(typePrefix)) {
+                subFolder = processInstanceFolder;
+            } else if(TASK_PREFIX.equals(typePrefix)) {
+                subFolder = taskFolder;
+            } else if (CASE_PREFIX.equals(typePrefix)) {
+                subFolder = caseFolder;
+            } else {
+                subFolder = new File(this.contentFolderRoot, typePrefix);
+            }
+            File idFolder = new File(subFolder, ids[1]);
+            contentFile = new File(idFolder, ids[2]);
+
+        } else {
+            contentFile = new File(uncategorizedFolder, ids[1]);
+        }
+
+        if (!contentFile.exists()) {
+            throw new FlowableObjectNotFoundException("No content found for id " + id);
+        }
+        return contentFile;
     }
 
     @Override
@@ -309,16 +230,7 @@ public class SimpleFileSystemContentStorage implements ContentStorage {
     @Override
     public void deleteContentObject(String id) {
         try {
-            String[] ids = id.split("\\.");
-            File contentFile = getContentFile(ids, contentTypeHandler -> {
-                    File localContentFile = new File(contentTypeHandler.getFolder(), ids[1]);
-                    if (ids.length == 3) {
-                        localContentFile = new File(localContentFile, ids[2]);
-                    }
-                return localContentFile;
-                }, () -> {
-                throw new FlowableException("Unrecognized content object ["+id+"]");
-            });
+            File contentFile = getContentFile(id);
             File parentFile = contentFile.getParentFile();
             contentFile.delete();
 
@@ -335,8 +247,8 @@ public class SimpleFileSystemContentStorage implements ContentStorage {
         return "file";
     }
 
-    protected File getContentFile(Map<String, Object> metaData, String contentId, ContentTypeHandler contentTypeHandler) {
-        return new File(createOrGetFolderBasedOnMetaData(metaData, contentTypeHandler), contentId);
+    protected File getContentFile(Map<String, Object> metaData, String contentId) {
+        return new File(createOrGetFolderBasedOnMetaData(metaData), contentId);
     }
 
     protected String determineType(Map<String, Object> metaData) {
@@ -358,24 +270,27 @@ public class SimpleFileSystemContentStorage implements ContentStorage {
         return TYPE_UNCATEGORIZED;
     }
 
-    protected File createOrGetFolderBasedOnMetaData(Map<String, Object> metaData, ContentTypeHandler contentTypeHandler) {
-        return
-            internalCreateOrGetFolder(
-                contentTypeHandler.getFolder(), contentTypeHandler.getGetIdFromMetadata().apply(metaData)
-            );
-    }
-
-    protected ContentTypeHandler getOrCreateContentTypeHandler(Map<String, Object> metaData) {
+    protected File createOrGetFolderBasedOnMetaData(Map<String, Object> metaData) {
         String type = determineType(metaData);
-        return contentTypeHandlers.stream()
-            .filter(anyContentTypeHandler -> type.equals(anyContentTypeHandler.getFolder().getName()))
-            .findFirst()
-            .orElseGet(() -> {
-                    File typeFolder = createFolder(type);
-                    this.contentTypeHandlers = refreshTypeFolders();
-                    return ContentTypeHandler.of(type, typeFolder, GET_SCOPE_ID_FROM_METADATA, GENERATE_CONTENTID_FOR_SCOPE);
-                }
-            );
+        switch (type) {
+        case TYPE_PROCESS_INSTANCE:
+            String processInstanceId = (String) metaData.get(ContentMetaDataKeys.PROCESS_INSTANCE_ID);
+            return internalCreateOrGetFolder(processInstanceFolder, processInstanceId);
+
+        case TYPE_TASK:
+            String taskId = (String) metaData.get(ContentMetaDataKeys.TASK_ID);
+            return internalCreateOrGetFolder(taskFolder, taskId);
+
+        case TYPE_CASE_INSTANCE:
+            String caseId = (String) metaData.get(ContentMetaDataKeys.SCOPE_ID);
+            return internalCreateOrGetFolder(caseFolder, caseId);
+
+        case TYPE_UNCATEGORIZED:
+            return uncategorizedFolder;
+
+        default:
+            return internalCreateOrGetFolder(validateOrCreateFolder(type), (String) metaData.get(ContentMetaDataKeys.SCOPE_ID));
+        }
     }
 
     protected File internalCreateOrGetFolder(File parentFolder, String id) {
@@ -386,41 +301,4 @@ public class SimpleFileSystemContentStorage implements ContentStorage {
         return folder;
     }
 
-    protected static class ContentTypeHandler {
-        final protected String type;
-        final protected File Folder;
-        final protected Function<Map<String, Object>, String> getIdFromMetadata;
-        final protected BiFunction<String, Map<String, Object>, String> generateContentId;
-
-        protected ContentTypeHandler(String type, File folder,
-            Function<Map<String, Object>, String> getIdFromMetadata,
-            BiFunction<String, Map<String, Object>, String> generateContentId) {
-            this.type = type;
-            Folder = folder;
-            this.getIdFromMetadata = getIdFromMetadata;
-            this.generateContentId = generateContentId;
-        }
-
-        public String getType() {
-            return type;
-        }
-        public File getFolder() {
-            return Folder;
-        }
-
-        public Function<Map<String, Object>, String> getGetIdFromMetadata() {
-            return getIdFromMetadata;
-        }
-
-        public BiFunction<String, Map<String, Object>, String> getGenerateContentId() {
-            return generateContentId;
-        }
-
-        public static ContentTypeHandler of(String type, File folder, Function<Map<String, Object>, String> getIdFromMetadataFunction,
-            BiFunction<String, Map<String, Object>, String> generateContentId
-            ) {
-            return new ContentTypeHandler(type, folder, getIdFromMetadataFunction, generateContentId);
-        }
-
-    }
 }
