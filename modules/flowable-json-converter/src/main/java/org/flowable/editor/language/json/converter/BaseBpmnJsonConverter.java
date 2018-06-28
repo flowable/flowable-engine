d304a0e77b9c14c2f50bbc97b9e8a93ff987a584
Martin Grofcik <martin.grofcik@edorasware.com>
2018-06-28 15:40:12 +0200

    #1067(feat): allowing expressions in isSequential flag - modeler

--- modules/flowable-json-converter/src/main/java/org/flowable/editor/language/json/converter/BaseBpmnJsonConverter.java
+++ modules/flowable-json-converter/src/main/java/org/flowable/editor/language/json/converter/BaseBpmnJsonConverter.java
@@ -160,7 +160,11 @@ public abstract class BaseBpmnJsonConverter implements EditorJsonConstants, Sten
                 if (StringUtils.isNotEmpty(loopDef.getLoopCardinality()) || StringUtils.isNotEmpty(loopDef.getInputDataItem()) || StringUtils.isNotEmpty(loopDef.getCompletionCondition())) {
 
                     if (StringUtils.isNotEmpty(loopDef.getSequential())) {
-                        propertiesNode.put(PROPERTY_MULTIINSTANCE_TYPE, loopDef.getSequential());
+                        if (loopDef.getSequential().startsWith("${") || loopDef.getSequential().startsWith("#{")) {
+                            propertiesNode.put(PROPERTY_MULTIINSTANCE_TYPE_EXPRESSION, loopDef.getSequential());
+                        } else {
+                            propertiesNode.put(PROPERTY_MULTIINSTANCE_TYPE, loopDef.getSequential());
+                        }
                     }
                     if (StringUtils.isNotEmpty(loopDef.getLoopCardinality())) {
                         propertiesNode.put(PROPERTY_MULTIINSTANCE_CARDINALITY, loopDef.getLoopCardinality());
@@ -307,7 +311,8 @@ public abstract class BaseBpmnJsonConverter implements EditorJsonConstants, Sten
                 activity.setAsynchronous(getPropertyValueAsBoolean(PROPERTY_ASYNCHRONOUS, elementNode));
                 activity.setNotExclusive(!getPropertyValueAsBoolean(PROPERTY_EXCLUSIVE, elementNode));
 
-                String multiInstanceType = getPropertyValueAsString(PROPERTY_MULTIINSTANCE_TYPE, elementNode);
+                String multiInstanceTypeExpression = getPropertyValueAsString(PROPERTY_MULTIINSTANCE_TYPE_EXPRESSION, elementNode);
+                String multiInstanceType = StringUtils.isNotEmpty(multiInstanceTypeExpression) ? multiInstanceTypeExpression : getPropertyValueAsString(PROPERTY_MULTIINSTANCE_TYPE, elementNode);
                 String multiInstanceCardinality = getPropertyValueAsString(PROPERTY_MULTIINSTANCE_CARDINALITY, elementNode);
                 String multiInstanceCollection = getPropertyValueAsString(PROPERTY_MULTIINSTANCE_COLLECTION, elementNode);
                 String multiInstanceCondition = getPropertyValueAsString(PROPERTY_MULTIINSTANCE_CONDITION, elementNode);
