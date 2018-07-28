30e842157a2d861f76c94edc2a4bb2493926bd73
Pascal Schumacher <pascalschumacher@gmx.net>
2018-07-28 12:36:55 +0200

    Remove unused local variables of tests

--- modules/flowable-engine/src/test/java/org/flowable/standalone/history/FullHistoryTest.java
+++ modules/flowable-engine/src/test/java/org/flowable/standalone/history/FullHistoryTest.java
@@ -281,7 +281,7 @@ public class FullHistoryTest extends ResourceFlowableTestCase {
     public void testHistoricVariableInstanceQuery() {
         Map<String, Object> variables = new HashMap<>();
         variables.put("process", "one");
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("receiveTask", variables);
+        runtimeService.startProcessInstanceByKey("receiveTask", variables);
         runtimeService.trigger(runtimeService.createExecutionQuery().activityId("waitState").singleResult().getId());
 
         assertEquals(1, historyService.createHistoricVariableInstanceQuery().variableName("process").count());
@@ -289,7 +289,7 @@ public class FullHistoryTest extends ResourceFlowableTestCase {
 
         Map<String, Object> variables2 = new HashMap<>();
         variables2.put("process", "two");
-        ProcessInstance processInstance2 = runtimeService.startProcessInstanceByKey("receiveTask", variables2);
+        runtimeService.startProcessInstanceByKey("receiveTask", variables2);
         runtimeService.trigger(runtimeService.createExecutionQuery().activityId("waitState").singleResult().getId());
 
         assertEquals(2, historyService.createHistoricVariableInstanceQuery().variableName("process").count());
@@ -303,7 +303,7 @@ public class FullHistoryTest extends ResourceFlowableTestCase {
         Map<String, Object> variables3 = new HashMap<>();
         variables3.put("long", 1000l);
         variables3.put("double", 25.43d);
-        ProcessInstance processInstance3 = runtimeService.startProcessInstanceByKey("receiveTask", variables3);
+        runtimeService.startProcessInstanceByKey("receiveTask", variables3);
         runtimeService.trigger(runtimeService.createExecutionQuery().activityId("waitState").singleResult().getId());
 
         assertEquals(1, historyService.createHistoricVariableInstanceQuery().variableName("long").count());
