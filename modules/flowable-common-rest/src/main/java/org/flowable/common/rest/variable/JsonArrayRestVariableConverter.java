d28a3b38f0593ec2305d3f62762a8f853b47b354
Tobia De Koninck <LEDfan@users.noreply.github.com>
2018-09-11 10:25:34 +0200

    Support JSON in the REST api and flowable-admin

---
+++ modules/flowable-common-rest/src/main/java/org/flowable/common/rest/variable/JsonArrayRestVariableConverter.java
@@ -0,0 +1,53 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.flowable.common.rest.variable;
+
+import org.flowable.common.engine.api.FlowableIllegalArgumentException;
+import java.util.ArrayList;
+
+
+public class JsonArrayRestVariableConverter implements RestVariableConverter {
+
+    @Override
+    public String getRestTypeName() {
+        return "json";
+    }
+
+    @Override
+    public Class<?> getVariableType() {
+
+        return ArrayList.class;
+    }
+
+    @Override
+    public Object getVariableValue(EngineRestVariable result) {
+        if (result.getValue() != null) {
+            return result.getValue();
+        }
+        return null;
+    }
+
+    @Override
+    public void convertVariableValue(Object variableValue, EngineRestVariable result) {
+        if (variableValue != null) {
+            if (!(variableValue instanceof java.util.ArrayList)) {
+                throw new FlowableIllegalArgumentException("Converter can only convert java.util.ArrayList");
+            }
+            result.setValue(variableValue);
+        } else {
+            result.setValue(null);
+        }
+    }
+
+}
