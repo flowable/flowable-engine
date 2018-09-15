7ccc4856bb9e8cedb6453ed2482c5521edb86696
shareniu <1515308@qq.com>
2018-09-15 12:43:17 +0800

    task names missing (box too small?)

--- modules/flowable-image-generator/src/main/java/org/flowable/image/impl/DefaultProcessDiagramCanvas.java
+++ modules/flowable-image-generator/src/main/java/org/flowable/image/impl/DefaultProcessDiagramCanvas.java
@@ -782,6 +782,12 @@ public class DefaultProcessDiagramCanvas {
                         lastLine = lastLine.substring(0, lastLine.length() - 4) + "...";
                     }
                     layouts.add(new TextLayout(lastLine, g.getFont(), g.getFontRenderContext()));
+                }else {
+                    // at least, draw one line
+                    // even if text does not fit
+                    // in order to avoid empty box
+                    layouts.add(layout);
+                    currentHeight += height;
                 }
                 break;
             } else {
