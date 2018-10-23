084f3c0a3d01eeea639f50f4921d987fedd01b73
Alberto Domínguez Pérez <alberto.dominguezperez@dowjones.com>
2018-10-23 21:58:01 +0200

    add loaded check because there are 5 Spring contexts

--- modules/flowable-app-rest/src/main/java/org/flowable/rest/conf/DJConfig.java
+++ modules/flowable-app-rest/src/main/java/org/flowable/rest/conf/DJConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -22,16 +23,22 @@ public class DJConfig {
 
 	@Value("${url.dj.adapter:http://localhost:8080}")
 	private String defaulDjAdapterUrl;
+
+    private boolean loaded = false;
 	
 	//runs when Spring context is loaded
 	@EventListener
-	public void onApplicationEvent(ContextRefreshedEvent event) {
+	public synchronized void onApplicationEvent(ContextRefreshedEvent event) {
 
-	    processEngineConfiguration.getEventDispatcher()
-            .addEventListener(new CustomEventListener(defaulDjAdapterUrl));
-    
-	    processEngineConfiguration.getVariableTypes().addType(new JsonVariableType(true), 
-	            processEngineConfiguration.getVariableTypes().getTypeIndex(SerializableType.TYPE_NAME));
+	    if(!loaded) {
+	        loaded=true;
+	        processEngineConfiguration.getEventDispatcher()
+	        .addEventListener(new CustomEventListener(defaulDjAdapterUrl));
+	        
+	        processEngineConfiguration.getVariableTypes().addType(new JsonVariableType(true), 
+	                processEngineConfiguration.getVariableTypes().getTypeIndex(SerializableType.TYPE_NAME));
+	    }
+	    
 	}
 
 
