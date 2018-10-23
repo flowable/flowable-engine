b0fca8f62bc6e012692b55242e7c1a00930f7924
Joram Barrez <jbarrez@users.noreply.github.com>
2018-10-23 21:12:13 +0200

    Cmmn: make stage-overview rest call use runtime data

--- modules/flowable-cmmn-engine/src/main/java/org/flowable/cmmn/engine/impl/runtime/PlanItemInstanceQueryProperty.java
+++ modules/flowable-cmmn-engine/src/main/java/org/flowable/cmmn/engine/impl/runtime/PlanItemInstanceQueryProperty.java
@@ -28,7 +28,7 @@ public class PlanItemInstanceQueryProperty implements QueryProperty {
     private static final Map<String, PlanItemInstanceQueryProperty> properties = new HashMap<>();
 
     public static final PlanItemInstanceQueryProperty CREATE_TIME = new PlanItemInstanceQueryProperty("RES.CREATE_TIME_");
-    public static final PlanItemInstanceQueryProperty END_TIME = new PlanItemInstanceQueryProperty("RES.END_TIME_");
+    public static final PlanItemInstanceQueryProperty END_TIME = new PlanItemInstanceQueryProperty("RES.ENDED_TIME_");
     public static final PlanItemInstanceQueryProperty NAME = new PlanItemInstanceQueryProperty("RES.NAME_");
 
     private String name;
