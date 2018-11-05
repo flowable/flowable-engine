b16138b1a06ba3e52d3d5b370d2bf72e6004d9d5
Joram Barrez <jbarrez@users.noreply.github.com>
2018-11-05 20:45:01 +0100

    Cmmn: improve support for nested sentries and event listeners

--- modules/flowable-cmmn-engine/src/main/java/org/flowable/cmmn/engine/impl/agenda/operation/AbstractPlanItemInstanceOperation.java
+++ modules/flowable-cmmn-engine/src/main/java/org/flowable/cmmn/engine/impl/agenda/operation/AbstractPlanItemInstanceOperation.java
@@ -14,11 +14,14 @@ package org.flowable.cmmn.engine.impl.agenda.operation;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.apache.commons.lang3.StringUtils;
 import org.flowable.cmmn.api.runtime.PlanItemInstanceState;
 import org.flowable.cmmn.converter.util.PlanItemDependencyUtil;
 import org.flowable.cmmn.engine.impl.persistence.entity.CaseInstanceEntity;
@@ -30,6 +33,7 @@ import org.flowable.cmmn.engine.impl.util.CommandContextUtil;
 import org.flowable.cmmn.model.EventListener;
 import org.flowable.cmmn.model.PlanFragment;
 import org.flowable.cmmn.model.PlanItem;
+import org.flowable.cmmn.model.Stage;
 import org.flowable.common.engine.impl.interceptor.CommandContext;
 
 /**
@@ -95,69 +99,95 @@ public abstract class AbstractPlanItemInstanceOperation extends CmmnOperation {
          * 3. For those event listeners, get the dependent plan items.
          *    These dependents are the plan items that depend in their sentries on this event listener.
          * 4. Fetch the plan item instances for these dependent plan items.
-         *    If the event listener is used for an entry criterion: if they are all in a state different from available, it is an orphan.
-         *    If the event listener is used for an exit criterion: if they are all in a terminal state, the user event listener is an orphan and can be deleted.
+         *    - If the event listener is used for an entry criterion: if they are all in a state different from available, it is an orphan.
+         *    - If the event listener is used for an exit criterion: if they are all in a terminal state, the user event listener is an orphan and can be deleted.
+         *    Note: if the plan item instance has a parent that is non-terminal, the plan item instance can still become active later and the event listener still is needed
          */
 
         PlanItem planItem = planItemInstanceEntity.getPlanItem();
         if (planItem != null) {
 
-            // Step 1 and 2
-            List<PlanItem> eventListenerDependencies = new ArrayList<>();
-            eventListenerDependencies.addAll(planItem.getEntryDependencies().stream()
-                .filter(p -> p.getPlanItemDefinition() instanceof EventListener).collect(Collectors.toList()));
-            eventListenerDependencies.addAll(planItem.getExitDependencies().stream()
-                .filter(p -> p.getPlanItemDefinition() instanceof EventListener).collect(Collectors.toList()));
+            // Step 1 and 2 (gather and filter)
+            List<PlanItem> eventListenerDependencies = gatherEventListenerDependencies(planItem);
 
-            // Special case: if the current plan item is a stage, we need to also verify all event listeners
-            // that reference a child plan item of this stage. Normally this will happen automatically, unless
-            // the child plan item instances haven't been created yet (e.g with nested stages).
-            // As such, we need to check all plan items for which there hasn't been a plan item instance yet.
+            // Step 3 and 4 (determine dependent plan items for the event listener and check if it's orphaned)
+            if (!eventListenerDependencies.isEmpty()) {
+                terminateOrphanedEventListeners(eventListenerDependencies);
+            }
+        }
 
-            if (planItem.getPlanItemDefinition() instanceof PlanFragment
-                && PlanItemInstanceState.isInTerminalState(planItemInstanceEntity)) { //  This is only true when stopping, not when a plan item gets activated (in that case, the child plan items can still be created and no special care is needed)
+    }
 
-                List<PlanItem> childPlanItemsWithDependencies = getChildPlanItemsWithDependencies((PlanFragment) planItem.getPlanItemDefinition());
+    protected List<PlanItem> gatherEventListenerDependencies(PlanItem planItem) {
 
-                CaseInstanceEntity caseInstanceEntity = CommandContextUtil
-                    .getCaseInstanceEntityManager(commandContext).findById(planItemInstanceEntity.getCaseInstanceId());
-                Map<String, List<PlanItemInstanceEntity>> childPlanItemInstancesMap = CaseInstanceUtil
-                    .findChildPlanItemInstancesMap(caseInstanceEntity, childPlanItemsWithDependencies);
+        List<PlanItem> eventListenerDependencies = Stream.concat(
+            planItem.getEntryDependencies().stream().filter(p -> p.getPlanItemDefinition() instanceof EventListener),
+            planItem.getExitDependencies().stream().filter(p -> p.getPlanItemDefinition() instanceof EventListener))
+        .collect(Collectors.toList());
 
-                for (PlanItem childPlanItemWithDependencies : childPlanItemsWithDependencies) {
-                    if (!childPlanItemInstancesMap.containsKey(childPlanItemWithDependencies.getId())) {
-                        eventListenerDependencies.addAll(childPlanItemWithDependencies.getEntryDependencies().stream()
-                            .filter(p -> p.getPlanItemDefinition() instanceof EventListener).collect(Collectors.toList()));
-                        eventListenerDependencies.addAll(childPlanItemWithDependencies.getExitDependencies().stream()
-                            .filter(p -> p.getPlanItemDefinition() instanceof EventListener).collect(Collectors.toList()));
-                    }
+        // Special case: if the current plan item is a stage, we need to also verify all event listeners
+        // that reference a child plan item of this stage. Normally this will happen automatically, unless
+        // the child plan item instances haven't been created yet (e.g with nested stages).
+        // As such, we need to check all plan items for which there hasn't been a plan item instance yet.
+
+        if (planItem.getPlanItemDefinition() instanceof PlanFragment
+            && PlanItemInstanceState.isInTerminalState(planItemInstanceEntity)) { //  This is only true when stopping, not when a plan item gets activated (in that case, the child plan items can still be created and no special care is needed)
+
+            List<PlanItem> childPlanItemsWithDependencies = getChildPlanItemsWithDependencies((PlanFragment) planItem.getPlanItemDefinition());
+
+            CaseInstanceEntity caseInstanceEntity = CommandContextUtil
+                .getCaseInstanceEntityManager(commandContext).findById(planItemInstanceEntity.getCaseInstanceId());
+            Map<String, List<PlanItemInstanceEntity>> childPlanItemInstancesMap = CaseInstanceUtil
+                .findChildPlanItemInstancesMap(caseInstanceEntity, childPlanItemsWithDependencies);
+
+            for (PlanItem childPlanItemWithDependencies : childPlanItemsWithDependencies) {
+                if (!childPlanItemInstancesMap.containsKey(childPlanItemWithDependencies.getId())) {
+                    eventListenerDependencies.addAll(childPlanItemWithDependencies.getEntryDependencies().stream()
+                        .filter(p -> p.getPlanItemDefinition() instanceof EventListener).collect(Collectors.toList()));
+                    eventListenerDependencies.addAll(childPlanItemWithDependencies.getExitDependencies().stream()
+                        .filter(p -> p.getPlanItemDefinition() instanceof EventListener).collect(Collectors.toList()));
                 }
             }
+        }
+
+        return eventListenerDependencies;
+    }
+
+    protected void terminateOrphanedEventListeners(List<PlanItem> eventListenerDependencies) {
+
+        CaseInstanceEntity caseInstanceEntity = CommandContextUtil
+            .getCaseInstanceEntityManager(commandContext).findById(planItemInstanceEntity.getCaseInstanceId());
+
+        for (PlanItem eventListenerPlanItem : eventListenerDependencies) {
+
+            List<PlanItemInstanceEntity> eventListenerPlanItemInstance = CaseInstanceUtil
+                .findNonTerminalChildPlanItemInstances(caseInstanceEntity, eventListenerPlanItem);
 
             // Step 3
-            if (!eventListenerDependencies.isEmpty()) {
-                CaseInstanceEntity caseInstanceEntity = CommandContextUtil
-                    .getCaseInstanceEntityManager(commandContext).findById(planItemInstanceEntity.getCaseInstanceId());
-                for (PlanItem eventListenerPlanItem : eventListenerDependencies) {
+            if (!eventListenerPlanItemInstance.isEmpty()) {
 
-                    List<PlanItemInstanceEntity> eventListenerPlanItemInstance = CaseInstanceUtil
-                        .findNonTerminalChildPlanItemInstances(caseInstanceEntity, eventListenerPlanItem);
+                List<PlanItem> dependentPlanItems = eventListenerPlanItem.getDependentPlanItems();
+                Map<String, List<PlanItemInstanceEntity>> dependentPlanItemInstancesMap = CaseInstanceUtil
+                    .findChildPlanItemInstancesMap(caseInstanceEntity, dependentPlanItems);
 
-                    if (!eventListenerPlanItemInstance.isEmpty()) {
+                // Step 4
+                boolean isOrphan = true;
+                Iterator<PlanItem> planItemIterator = dependentPlanItems.iterator();
+                while (isOrphan && planItemIterator.hasNext()) {
 
-                        List<PlanItem> dependentPlanItems = eventListenerPlanItem.getDependentPlanItems();
-                        List<PlanItemInstanceEntity> dependentPlanItemInstances = CaseInstanceUtil
-                            .findChildPlanItemInstances(caseInstanceEntity, dependentPlanItems);
+                    PlanItem dependentPlanItem = planItemIterator.next();
 
-                        // Step 4
-                        boolean isOrphan = true;
+                    List<PlanItemInstanceEntity> dependentPlanItemInstances = dependentPlanItemInstancesMap.get(dependentPlanItem.getId());
+                    if (dependentPlanItemInstances != null && !dependentPlanItemInstances.isEmpty()) {
+
+                        // In case there are instances, check the state
+
                         for (PlanItemInstanceEntity dependentPlanItemInstance : dependentPlanItemInstances) {
-
                             if (PlanItemDependencyUtil.isEntryDependency(dependentPlanItemInstance.getPlanItem(), eventListenerPlanItem)) {
-                              if (PlanItemInstanceState.AVAILABLE.equals(dependentPlanItemInstance.getState())
+                                if (PlanItemInstanceState.AVAILABLE.equals(dependentPlanItemInstance.getState())
                                     || PlanItemInstanceState.WAITING_FOR_REPETITION.equals(dependentPlanItemInstance.getState())) {
-                                  isOrphan = false;
-                              }
+                                    isOrphan = false;
+                                }
                             }
 
                             if (PlanItemDependencyUtil.isExitDependency(dependentPlanItemInstance.getPlanItem(), eventListenerPlanItem)) {
@@ -165,20 +195,37 @@ public abstract class AbstractPlanItemInstanceOperation extends CmmnOperation {
                                     isOrphan = false;
                                 }
                             }
-
                         }
 
-                        if (isOrphan) {
-                            for (PlanItemInstanceEntity eventListenerPlanItemInstanceEntity : eventListenerPlanItemInstance) {
-                                CommandContextUtil.getAgenda(commandContext).planTerminatePlanItemInstanceOperation(eventListenerPlanItemInstanceEntity);
+                    } else {
+
+                        // In case there are no instances, we need to check potential parents.
+                        // If there is a nonterminal parent, the event listener still might be needed and cannot be terminated
+
+                        Stage parentStage = dependentPlanItem.getParentStage();
+                        while (isOrphan && parentStage != null && !parentStage.isPlanModel()) {
+                            List<PlanItemInstanceEntity> nonTerminalStagePlanItemInstances = CaseInstanceUtil
+                                .findNonTerminalChildPlanItemInstances(caseInstanceEntity, parentStage.getPlanItem());
+                            if (!nonTerminalStagePlanItemInstances.isEmpty()) {
+                                isOrphan = false;
+                            } else {
+                                parentStage = parentStage.getParentStage();
                             }
                         }
+
+                    }
+
+                }
+
+                if (isOrphan) {
+                    for (PlanItemInstanceEntity eventListenerPlanItemInstanceEntity : eventListenerPlanItemInstance) {
+                        CommandContextUtil.getAgenda(commandContext).planTerminatePlanItemInstanceOperation(eventListenerPlanItemInstanceEntity);
                     }
                 }
+
             }
         }
-
-     }
+    }
 
     protected boolean isPlanItemRepeatableOnComplete(PlanItem planItem) {
         return  planItem != null
@@ -208,5 +255,5 @@ public abstract class AbstractPlanItemInstanceOperation extends CmmnOperation {
             }
         }
     }
-    
+
 }
