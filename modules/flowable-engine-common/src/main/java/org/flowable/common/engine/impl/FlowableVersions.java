8cb305f5fc3339df7506b0f307dd480d19394ad5
Martin Grofcik <martin.grofcik@flowable.com>
2018-11-14 09:24:07 +0100

    DB schema upgrade to 6.4.1.1

--- modules/flowable-engine-common/src/main/java/org/flowable/common/engine/impl/FlowableVersions.java
+++ modules/flowable-engine-common/src/main/java/org/flowable/common/engine/impl/FlowableVersions.java
@@ -23,7 +23,7 @@ import org.flowable.common.engine.api.FlowableException;
  */
 public class FlowableVersions {
     
-    public static final String CURRENT_VERSION = "6.4.1.0"; // Note the extra .x at the end. To cater for snapshot releases with different database changes
+    public static final String CURRENT_VERSION = "6.4.1.1"; // Note the extra .x at the end. To cater for snapshot releases with different database changes
     
     public static final List<FlowableVersion> FLOWABLE_VERSIONS = new ArrayList<>();
     
@@ -98,6 +98,8 @@ public class FlowableVersions {
         FLOWABLE_VERSIONS.add(new FlowableVersion("6.3.1.0"));
         FLOWABLE_VERSIONS.add(new FlowableVersion("6.3.2.0"));
         FLOWABLE_VERSIONS.add(new FlowableVersion("6.4.0.0"));
+        FLOWABLE_VERSIONS.add(new FlowableVersion("6.4.0.0"));
+        FLOWABLE_VERSIONS.add(new FlowableVersion("6.4.1.0"));
 
         /* Current */
         FLOWABLE_VERSIONS.add(new FlowableVersion(CURRENT_VERSION));
