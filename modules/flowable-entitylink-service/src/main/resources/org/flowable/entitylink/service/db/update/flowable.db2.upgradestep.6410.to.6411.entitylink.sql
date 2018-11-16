bc45d0500dfba974e86f59c5dceb70637f8fed62
Javier Casal <javier.casal@flowable.com>
2018-11-16 16:51:57 +0100

    Add entity link update scripts

---
+++ modules/flowable-entitylink-service/src/main/resources/org/flowable/entitylink/service/db/update/flowable.db2.upgradestep.6410.to.6411.entitylink.sql
@@ -0,0 +1,3 @@
+alter table ACT_RU_ENTITYLINK add column ROOT_SCOPE_ID_ varchar(255);
+alter table ACT_RU_ENTITYLINK add column ROOT_SCOPE_TYPE_ varchar(255);
+alter table ACT_RU_ENTITYLINK add column ROOT_SCOPE_DEFINITION_ID_ varchar(255);
