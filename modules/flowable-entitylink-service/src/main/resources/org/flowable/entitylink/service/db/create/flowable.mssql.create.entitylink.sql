04ec296500435967c693fdf52dc9b55f007e81a2
Javier Casal <javier.casal@flowable.com>
2018-11-16 15:51:36 +0100

    Add entity link root scope columns

--- modules/flowable-entitylink-service/src/main/resources/org/flowable/entitylink/service/db/create/flowable.mssql.create.entitylink.sql
+++ modules/flowable-entitylink-service/src/main/resources/org/flowable/entitylink/service/db/create/flowable.mssql.create.entitylink.sql
@@ -9,6 +9,9 @@ create table ACT_RU_ENTITYLINK (
     REF_SCOPE_ID_ nvarchar(255),
     REF_SCOPE_TYPE_ nvarchar(255),
     REF_SCOPE_DEFINITION_ID_ nvarchar(255),
+    ROOT_SCOPE_ID_ nvarchar(255),
+    ROOT_SCOPE_TYPE_ nvarchar(255),
+    ROOT_SCOPE_DEFINITION_ID_ nvarchar(255),
     primary key (ID_)
 );
 
