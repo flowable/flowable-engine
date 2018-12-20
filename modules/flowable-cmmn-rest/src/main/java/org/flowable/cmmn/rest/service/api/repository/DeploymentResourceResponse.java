535ee55dc8c945e26e5d620c66de22b6b9f46ead
David B Malkovsky <david@malkovsky.org>
2018-12-20 05:36:33 -0500

    Add missing license to java files.  This is just under half of the (#1459)
    ones that need to be done.

--- modules/flowable-cmmn-rest/src/main/java/org/flowable/cmmn/rest/service/api/repository/DeploymentResourceResponse.java
+++ modules/flowable-cmmn-rest/src/main/java/org/flowable/cmmn/rest/service/api/repository/DeploymentResourceResponse.java
@@ -1,5 +1,3 @@
-package org.flowable.cmmn.rest.service.api.repository;
-
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,6 +10,7 @@ package org.flowable.cmmn.rest.service.api.repository;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.flowable.cmmn.rest.service.api.repository;
 
 import io.swagger.annotations.ApiModelProperty;
 
