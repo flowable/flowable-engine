4740aa88611dd03b44b79c1c1d488850599d280e
Tijs Rademakers <tijs.rademakers@gmail.com>
2018-12-21 09:30:24 +0100

    fix test

--- modules/flowable-app-engine/src/test/java/org/flowable/app/engine/test/persistence/EntitiesTest.java
+++ modules/flowable-app-engine/src/test/java/org/flowable/app/engine/test/persistence/EntitiesTest.java
@@ -14,6 +14,7 @@ package org.flowable.app.engine.test.persistence;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.HashSet;
 import java.util.Set;
