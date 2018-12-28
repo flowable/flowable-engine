776e4484d783332e14a3005599d5399b79f31e51
Filip Hrisafov <filip.hrisafov@gmail.com>
2018-12-28 11:22:45 +0100

    Add update scripts for removing the not needed item and message variables for the Web Service activity
    Transient variables are used instead.
    Done in 11353d2ee403e5b0ab8639fdecbc7cd0797bfcf7

--- modules/flowable-entitylink-service/src/main/resources/org/flowable/entitylink/service/db/create/flowable.postgres.create.entitylink.sql
+++ modules/flowable-entitylink-service/src/main/resources/org/flowable/entitylink/service/db/create/flowable.postgres.create.entitylink.sql
@@ -16,4 +16,4 @@ create table ACT_RU_ENTITYLINK (
 create index ACT_IDX_ENT_LNK_SCOPE on ACT_RU_ENTITYLINK(SCOPE_ID_, SCOPE_TYPE_, LINK_TYPE_);
 create index ACT_IDX_ENT_LNK_SCOPE_DEF on ACT_RU_ENTITYLINK(SCOPE_DEFINITION_ID_, SCOPE_TYPE_, LINK_TYPE_);
 
-insert into ACT_GE_PROPERTY values ('entitylink.schema.version', '6.4.1.2', 1);
+insert into ACT_GE_PROPERTY values ('entitylink.schema.version', '6.4.1.3', 1);
