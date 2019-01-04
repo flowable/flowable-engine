8dbe8b7de4d5a0ac564eda7c6bdf6c2acae58738
dbm <david@malkovsky.org>
2019-01-04 08:20:30 -0500

    Make import type/order match idea-code-style-configuration.xml config

--- tooling/archetypes/flowable-archetype-unittest/src/main/resources/archetype-resources/src/test/java/MyUnitTest.java
+++ tooling/archetypes/flowable-archetype-unittest/src/main/resources/archetype-resources/src/test/java/MyUnitTest.java
@@ -1,13 +1,13 @@
 package ${package};
 
+import static org.junit.Assert.assertEquals;
+
 import org.flowable.engine.runtime.ProcessInstance;
-import org.flowable.task.api.Task;
 import org.flowable.engine.test.FlowableRule;
 import org.flowable.engine.test.Deployment;
+import org.flowable.task.api.Task;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class MyUnitTest {
 
