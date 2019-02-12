5c8378ede32a85a2dcf5943744f5b50d193613f5
Tijs Rademakers <tijs.rademakers@gmail.com>
2019-02-12 12:07:15 +0100

    Update swagger doc for form instance query

--- modules/flowable-form-rest/src/main/java/org/flowable/form/rest/service/api/form/FormInstanceCollectionResource.java
+++ modules/flowable-form-rest/src/main/java/org/flowable/form/rest/service/api/form/FormInstanceCollectionResource.java
@@ -55,7 +55,7 @@ public class FormInstanceCollectionResource extends BaseFormInstanceResource {
             @ApiImplicitParam(name = "processDefinitionIdLike", dataType = "string", value = "Only return form instances with a process definition id like the given value.", paramType = "query"),
             @ApiImplicitParam(name = "scopeId", dataType = "string", value = "Only return form instances with the given scope id.", paramType = "query"),
             @ApiImplicitParam(name = "scopeType", dataType = "string", value = "Only return form instances with a scope type like the given value.", paramType = "query"),
-            @ApiImplicitParam(name = "scopeId", dataType = "string", value = "Only return form instances with the given scope definition id.", paramType = "query"),
+            @ApiImplicitParam(name = "scopeDefinitionId", dataType = "string", value = "Only return form instances with the given scope definition id.", paramType = "query"),
             @ApiImplicitParam(name = "submittedBy", dataType = "string", value = "Only return form instances submitted by the given value.", paramType = "query"),
             @ApiImplicitParam(name = "submittedByLike", dataType = "string", value = "Only return form instances submitted by like the given value.", paramType = "query"),
             @ApiImplicitParam(name = "tenantId", dataType = "string", value = "Only return form instances with the given tenantId.", paramType = "query"),
