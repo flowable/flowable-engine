8ed7e9bf6b642c6d9a2844b808dbb68f40fdad8f
Tijs Rademakers <tijs.rademakers@gmail.com>
2019-04-03 21:18:30 +0200

    Fix upgrade tests + osgi test

--- modules/flowable-eventsubscription-service/src/main/resources/org/flowable/eventsubscription/service/db/upgrade/flowable.mssql.upgradestep.6413.to.6500.eventsubscription.sql
+++ modules/flowable-eventsubscription-service/src/main/resources/org/flowable/eventsubscription/service/db/upgrade/flowable.mssql.upgradestep.6413.to.6500.eventsubscription.sql
@@ -3,4 +3,4 @@ alter table ACT_RU_EVENT_SUBSCR add column SCOPE_ID_ nvarchar(64);
 alter table ACT_RU_EVENT_SUBSCR add column SCOPE_DEFINITION_ID_ nvarchar(64);
 alter table ACT_RU_EVENT_SUBSCR add column SCOPE_TYPE_ nvarchar(64);
 
-update ACT_GE_PROPERTY set VALUE_ = '6.5.0.0' where NAME_ = 'eventsubscription.schema.version';
+insert into ACT_GE_PROPERTY values ('eventsubscription.schema.version', '6.5.0.0', 1);
