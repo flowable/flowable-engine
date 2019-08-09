773465d0213fc947b134a94fe03974f9e7b8f196
Tijs Rademakers <tijs.rademakers@gmail.com>
2019-08-09 12:37:53 +0200

    Skip tests when building app images

--- docker/build-all-images.sh
+++ docker/build-all-images.sh
@@ -5,22 +5,22 @@ mvn -Pdistro clean install -DskipTests
 
 echo "Building REST app image"
 cd modules/flowable-app-rest
-mvn -Pdocker,swagger clean package
+mvn -Pdocker,swagger -DskipTests clean package
 
 echo "Building ADMIN app image"
 cd ../flowable-ui-admin
-mvn -Pdocker clean package
+mvn -Pdocker -DskipTests clean package
 
 echo "Building IDM app image"
 cd ../flowable-ui-idm
-mvn -Pdocker clean package
+mvn -Pdocker -DskipTests clean package
 
 echo "Building MODELER app image"
 cd ../flowable-ui-modeler
-mvn -Pdocker clean package
+mvn -Pdocker -DskipTests clean package
 
 echo "Building TASK app image"
 cd ../flowable-ui-task
-mvn -Pdocker clean package
+mvn -Pdocker -DskipTests clean package
 
 echo "Done..."
