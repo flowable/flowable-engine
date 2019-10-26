13cc0fee29558e794e76a77c591ed1be7f005c56
micha.kiener <micha.kiener@gmail.com>
2019-10-26 11:52:35 +0200

    small repetition handling change for completion detection, adding tests

--- modules/flowable-cmmn-converter/src/main/java/org/flowable/cmmn/converter/CmmnXmlConverter.java
+++ modules/flowable-cmmn-converter/src/main/java/org/flowable/cmmn/converter/CmmnXmlConverter.java
@@ -510,8 +510,6 @@ public class CmmnXmlConverter implements CmmnXmlConstants {
                 }
             } else if (planItemDefinition instanceof CasePageTask) {
                 // check, if the parent completion rule is set and if not, set it to the default value for a case page which is always ignore
-                CasePageTask casePageTask = (CasePageTask) planItemDefinition;
-
                 if (planItem.getItemControl() == null) {
                     PlanItemControl planItemControl = new PlanItemControl();
                     planItem.setItemControl(planItemControl);
