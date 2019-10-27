a9541636627b2a4482bc0c5985341d7037d746c0
micha.kiener <micha.kiener@gmail.com>
2019-10-27 12:42:58 +0100

    add tests for complex completion, including conditions, repetition, manual activation and required combinations

--- modules/flowable-cmmn-engine/src/test/java/org/flowable/cmmn/test/runtime/PlanItemCompletionTest.java
+++ modules/flowable-cmmn-engine/src/test/java/org/flowable/cmmn/test/runtime/PlanItemCompletionTest.java
@@ -5,6 +5,7 @@ import static org.flowable.cmmn.api.runtime.PlanItemInstanceState.AVAILABLE;
 import static org.flowable.cmmn.api.runtime.PlanItemInstanceState.COMPLETED;
 import static org.flowable.cmmn.api.runtime.PlanItemInstanceState.ENABLED;
 import static org.flowable.cmmn.api.runtime.PlanItemInstanceState.UNAVAILABLE;
+import static org.flowable.cmmn.api.runtime.PlanItemInstanceState.WAITING_FOR_REPETITION;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
@@ -294,5 +295,166 @@ public class PlanItemCompletionTest extends FlowableCmmnTestCase {
         assertEquals(0, cmmnRuntimeService.createPlanItemInstanceQuery().count());
         assertEquals(0, cmmnRuntimeService.createCaseInstanceQuery().count());
         assertEquals(1, cmmnHistoryService.createHistoricCaseInstanceQuery().finished().count());
+    }
+
+    @Test
+    @CmmnDeployment
+    public void testComplexCompletionWithoutAutocompletion() {
+        CaseInstance caseInstance = runComplexCompletionTestScenario(false);
+
+        // because autocompletion is off, we still stay in Stage A
+        List<PlanItemInstance> planItemInstances = cmmnRuntimeService.createPlanItemInstanceQuery()
+            .caseInstanceId(caseInstance.getId())
+            .orderByName().asc()
+            .list();
+
+        assertEquals(4, planItemInstances.size());
+        String[] expectedNames = new String[] { "Stage A", "Task D", "Task E", "Task G" };
+        String[] expectedStates = new String[] { ACTIVE, WAITING_FOR_REPETITION, AVAILABLE, ENABLED };
+        for (int i = 0; i < planItemInstances.size(); i++) {
+            assertEquals(expectedNames[i], planItemInstances.get(i).getName());
+            assertEquals(expectedStates[i], planItemInstances.get(i).getState());
+        }
+    }
+
+    @Test
+    @CmmnDeployment
+    public void testComplexCompletionWithAutocompletion() {
+        CaseInstance caseInstance = runComplexCompletionTestScenario(true);
+
+        // because autocompletion is on, the case will be completed
+        assertEquals(0, cmmnRuntimeService.createPlanItemInstanceQuery().count());
+        assertEquals(0, cmmnRuntimeService.createCaseInstanceQuery().count());
+        assertEquals(1, cmmnHistoryService.createHistoricCaseInstanceQuery().finished().count());
+    }
+
+    protected CaseInstance runComplexCompletionTestScenario(boolean autocompleteEnabled) {
+        CaseInstance caseInstance = cmmnRuntimeService.createCaseInstanceBuilder()
+            .caseDefinitionKey("testCompletionWithConditions")
+            .variable("autocompleteEnabled", autocompleteEnabled)
+            .start();
+
+        List<PlanItemInstance> planItemInstances = cmmnRuntimeService.createPlanItemInstanceQuery()
+            .caseInstanceId(caseInstance.getId())
+            .orderByName().asc()
+            .list();
+
+        assertEquals(9, planItemInstances.size());
+        String[] expectedNames = new String[] { "Stage A", "Task A", "Task B", "Task C", "Task D", "Task E", "Task F", "Task G", "Task H" };
+        String[] expectedStates = new String[] { ACTIVE, ACTIVE, AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE, ENABLED, ENABLED };
+        for (int i = 0; i < planItemInstances.size(); i++) {
+            assertEquals(expectedNames[i], planItemInstances.get(i).getName());
+            assertEquals(expectedStates[i], planItemInstances.get(i).getState());
+        }
+
+        // complete Task A -> will enable B and D, C stays in available as it has a condition
+        cmmnRuntimeService.triggerPlanItemInstance(planItemInstances.get(1).getId());
+
+        planItemInstances = cmmnRuntimeService.createPlanItemInstanceQuery()
+            .caseInstanceId(caseInstance.getId())
+            .orderByName().asc()
+            .list();
+
+        assertEquals(9, planItemInstances.size());
+        expectedNames = new String[] { "Stage A", "Task B", "Task C", "Task D", "Task D", "Task E", "Task F", "Task G", "Task H" };
+        expectedStates = new String[] { ACTIVE, ENABLED, AVAILABLE, ENABLED, WAITING_FOR_REPETITION, AVAILABLE, AVAILABLE, ENABLED, ENABLED };
+        for (int i = 0; i < planItemInstances.size(); i++) {
+            assertEquals(expectedNames[i], planItemInstances.get(i).getName());
+            assertEquals(expectedStates[i], planItemInstances.get(i).getState());
+        }
+
+        // activate Task C by setting the flag making its condition true
+        cmmnRuntimeService.setVariable(caseInstance.getId(), "activateTaskC", true);
+
+        planItemInstances = cmmnRuntimeService.createPlanItemInstanceQuery()
+            .caseInstanceId(caseInstance.getId())
+            .orderByName().asc()
+            .list();
+
+        assertEquals(9, planItemInstances.size());
+        expectedNames = new String[] { "Stage A", "Task B", "Task C", "Task D", "Task D", "Task E", "Task F", "Task G", "Task H" };
+        expectedStates = new String[] { ACTIVE, ENABLED, ACTIVE, ENABLED, WAITING_FOR_REPETITION, AVAILABLE, AVAILABLE, ENABLED, ENABLED };
+        for (int i = 0; i < planItemInstances.size(); i++) {
+            assertEquals(expectedNames[i], planItemInstances.get(i).getName());
+            assertEquals(expectedStates[i], planItemInstances.get(i).getState());
+        }
+
+        // start Task B and D and complete C
+        cmmnRuntimeService.startPlanItemInstance(planItemInstances.get(1).getId());
+        cmmnRuntimeService.triggerPlanItemInstance(planItemInstances.get(2).getId());
+        cmmnRuntimeService.startPlanItemInstance(planItemInstances.get(3).getId());
+
+        planItemInstances = cmmnRuntimeService.createPlanItemInstanceQuery()
+            .caseInstanceId(caseInstance.getId())
+            .orderByName().asc()
+            .list();
+
+        assertEquals(8, planItemInstances.size());
+        expectedNames = new String[] { "Stage A", "Task B", "Task D", "Task D", "Task E", "Task F", "Task G", "Task H" };
+        expectedStates = new String[] { ACTIVE, ACTIVE, ACTIVE, WAITING_FOR_REPETITION, AVAILABLE, AVAILABLE, ENABLED, ENABLED };
+        for (int i = 0; i < planItemInstances.size(); i++) {
+            assertEquals(expectedNames[i], planItemInstances.get(i).getName());
+            assertEquals(expectedStates[i], planItemInstances.get(i).getState());
+        }
+
+        // complete Task B and D
+        cmmnRuntimeService.triggerPlanItemInstance(planItemInstances.get(1).getId());
+        cmmnRuntimeService.triggerPlanItemInstance(planItemInstances.get(2).getId());
+
+        planItemInstances = cmmnRuntimeService.createPlanItemInstanceQuery()
+            .caseInstanceId(caseInstance.getId())
+            .orderByName().asc()
+            .list();
+
+        assertEquals(6, planItemInstances.size());
+        expectedNames = new String[] { "Stage A", "Task D", "Task E", "Task F", "Task G", "Task H" };
+        expectedStates = new String[] { ACTIVE, WAITING_FOR_REPETITION, AVAILABLE, AVAILABLE, ENABLED, ENABLED };
+        for (int i = 0; i < planItemInstances.size(); i++) {
+            assertEquals(expectedNames[i], planItemInstances.get(i).getName());
+            assertEquals(expectedStates[i], planItemInstances.get(i).getState());
+        }
+
+        // start and complete Task H
+        cmmnRuntimeService.startPlanItemInstance(planItemInstances.get(5).getId());
+        cmmnRuntimeService.triggerPlanItemInstance(planItemInstances.get(5).getId());
+
+        // enable Task F through making its condition true and then start it
+        cmmnRuntimeService.setVariable(caseInstance.getId(), "enableTaskF", true);
+        cmmnRuntimeService.startPlanItemInstance(planItemInstances.get(3).getId());
+
+        planItemInstances = cmmnRuntimeService.createPlanItemInstanceQuery()
+            .caseInstanceId(caseInstance.getId())
+            .orderByName().asc()
+            .list();
+
+        assertEquals(5, planItemInstances.size());
+        expectedNames = new String[] { "Stage A", "Task D", "Task E", "Task F", "Task G" };
+        expectedStates = new String[] { ACTIVE, WAITING_FOR_REPETITION, AVAILABLE, ACTIVE, ENABLED };
+        for (int i = 0; i < planItemInstances.size(); i++) {
+            assertEquals(expectedNames[i], planItemInstances.get(i).getName());
+            assertEquals(expectedStates[i], planItemInstances.get(i).getState());
+        }
+
+        // complete Task F and start Task G
+        cmmnRuntimeService.triggerPlanItemInstance(planItemInstances.get(3).getId());
+        cmmnRuntimeService.startPlanItemInstance(planItemInstances.get(4).getId());
+
+        planItemInstances = cmmnRuntimeService.createPlanItemInstanceQuery()
+            .caseInstanceId(caseInstance.getId())
+            .orderByName().asc()
+            .list();
+
+        assertEquals(4, planItemInstances.size());
+        expectedNames = new String[] { "Stage A", "Task D", "Task E", "Task G" };
+        expectedStates = new String[] { ACTIVE, WAITING_FOR_REPETITION, AVAILABLE, ACTIVE };
+        for (int i = 0; i < planItemInstances.size(); i++) {
+            assertEquals(expectedNames[i], planItemInstances.get(i).getName());
+            assertEquals(expectedStates[i], planItemInstances.get(i).getState());
+        }
+
+        // complete Task G and depending on autocompletion being on or off, we stay in Stage A or the case will be completed
+        cmmnRuntimeService.triggerPlanItemInstance(planItemInstances.get(3).getId());
+
+        return caseInstance;
     }
 }
