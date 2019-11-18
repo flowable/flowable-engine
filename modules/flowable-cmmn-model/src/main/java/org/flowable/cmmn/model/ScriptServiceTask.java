40b961744eeb8706773840d1899a8415156b2092
Manjunath CH <manjunath.c@kuliza.com>
2019-11-18 20:57:44 +0530

    Fix name issue with script task feild extension.

--- modules/flowable-cmmn-model/src/main/java/org/flowable/cmmn/model/ScriptServiceTask.java
+++ modules/flowable-cmmn-model/src/main/java/org/flowable/cmmn/model/ScriptServiceTask.java
@@ -39,7 +39,7 @@ public class ScriptServiceTask extends ServiceTask {
 
     public String getScript() {
         Optional<String> script = fieldExtensions.stream()
-                .filter(e -> e.getFieldName().equalsIgnoreCase("script"))
+                .filter(e -> e.getFieldName().equalsIgnoreCase("scripttext"))
                 .findFirst()
                 .map(FieldExtension::getStringValue);
         return script.orElseThrow(() -> new FlowableException("Missing script"));
