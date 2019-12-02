cf344bf15fcbde2b3df0086042a7191a991f0fa3
Joram Barrez <jbarrez@users.noreply.github.com>
2019-12-02 23:41:31 +0100

    Extract upgrades from #2032 to separate version

--- modules/flowable-engine/src/main/resources/org/flowable/db/create/flowable.db2.create.engine.sql
+++ modules/flowable-engine/src/main/resources/org/flowable/db/create/flowable.db2.create.engine.sql
@@ -336,7 +336,7 @@ alter table ACT_PROCDEF_INFO
     unique (PROC_DEF_ID_);
 
 insert into ACT_GE_PROPERTY
-values ('schema.version', '6.5.0.4', 1);
+values ('schema.version', '6.5.0.5', 1);
 
 insert into ACT_GE_PROPERTY
-values ('schema.history', 'create(6.5.0.4)', 1);
+values ('schema.history', 'create(6.5.0.5)', 1);
--- modules/flowable-task-service/src/main/resources/org/flowable/task/service/db/upgrade/flowable.mysql.upgradestep.6503.to.6504.task.sql
+++
@@ -1,3 +0,0 @@
-alter table ACT_RU_TASK add column PROPAGATED_STAGE_INST_ID_ varchar(255);
-
-update ACT_GE_PROPERTY set VALUE_ = '6.5.0.4' where NAME_ = 'task.schema.version';
