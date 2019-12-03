f73efbd86a0775ce547838ad23665c963728ba6a
Nick <mohammadnick92@yahoo.com>
2019-12-03 13:37:02 +0330

    changing imports according to project standards

--- modules/flowable-json-converter/src/test/java/org/flowable/editor/language/UserTaskLocalizationConverterTest.java
+++ modules/flowable-json-converter/src/test/java/org/flowable/editor/language/UserTaskLocalizationConverterTest.java
@@ -19,7 +19,9 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class UserTaskLocalizationConverterTest extends AbstractConverterTest {
 
