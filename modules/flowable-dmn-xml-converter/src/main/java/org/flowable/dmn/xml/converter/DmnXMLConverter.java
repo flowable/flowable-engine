44f62c1c9bda74843c932f5b00c6059f7568ff16
Yvo Swillens <yvoswillens@gmail.com>
2020-01-13 12:57:39 +0100

    Dev dmn12 collect (#2111)
    * add option to force DMN 1.1 spec compliant execution when saving decision table
    
    * add forceDMN11 property to DMN decision model
    
    * implemented conditional 1.1 spec compliant execution for collect aggregation logic
    
    * added convenience flag forceDMN11

--- modules/flowable-dmn-xml-converter/src/main/java/org/flowable/dmn/xml/converter/DmnXMLConverter.java
+++ modules/flowable-dmn-xml-converter/src/main/java/org/flowable/dmn/xml/converter/DmnXMLConverter.java
@@ -246,6 +246,11 @@ public class DmnXMLConverter implements DmnXMLConstants {
                     model.addDecision(decision);
                     decision.setId(xtr.getAttributeValue(null, ATTRIBUTE_ID));
                     decision.setName(xtr.getAttributeValue(null, ATTRIBUTE_NAME));
+
+                    if (Boolean.parseBoolean(xtr.getAttributeValue(FLOWABLE_EXTENSIONS_NAMESPACE, ATTRIBUTE_FORCE_DMN_11))) {
+                        decision.setForceDMN11(true);
+                    }
+
                     parentElement = decision;
                 } else if (ELEMENT_DECISION_TABLE.equals(xtr.getLocalName())) {
                     currentDecisionTable = new DecisionTable();
@@ -342,6 +347,11 @@ public class DmnXMLConverter implements DmnXMLConstants {
                 xtw.writeAttribute(ATTRIBUTE_ID, decision.getId());
                 if (StringUtils.isNotEmpty(decision.getName())) {
                     xtw.writeAttribute(ATTRIBUTE_NAME, decision.getName());
+                }
+
+                if (decision.isForceDMN11()) {
+                    xtw.writeNamespace(FLOWABLE_EXTENSIONS_PREFIX, FLOWABLE_EXTENSIONS_NAMESPACE);
+                    xtw.writeAttribute(FLOWABLE_EXTENSIONS_PREFIX, FLOWABLE_EXTENSIONS_NAMESPACE, ATTRIBUTE_FORCE_DMN_11, "true");
                 }
 
                 DmnXMLUtil.writeElementDescription(decision, xtw);
