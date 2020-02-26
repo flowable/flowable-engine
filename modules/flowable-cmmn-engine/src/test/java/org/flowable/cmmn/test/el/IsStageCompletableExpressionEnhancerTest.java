b04f3858abb28f462a233e323ab4cb67072b00c5
dbmalkovsky <david@malkovsky.org>
2020-02-26 07:02:11 -0500

    Use assertj fluent style in flowable-cmmn-engine (part 1)
    Breaking the changes into groups to make review easier.

--- modules/flowable-cmmn-engine/src/test/java/org/flowable/cmmn/test/el/IsStageCompletableExpressionEnhancerTest.java
+++ modules/flowable-cmmn-engine/src/test/java/org/flowable/cmmn/test/el/IsStageCompletableExpressionEnhancerTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,7 +12,7 @@
  */
 package org.flowable.cmmn.test.el;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.flowable.cmmn.engine.impl.function.IsStageCompletableExpressionFunction;
 import org.junit.Test;
@@ -21,16 +21,16 @@ import org.junit.Test;
  * @author Joram Barrez
  */
 public class IsStageCompletableExpressionEnhancerTest {
-    
+
     private IsStageCompletableExpressionFunction expressionFunction = new IsStageCompletableExpressionFunction();
-    
+
     @Test
     public void testRegexDefault() {
         assertRegexCorrect("${cmmn:isStageCompletable()}", "${cmmn:isStageCompletable(planItemInstance)}");
     }
 
     public void assertRegexCorrect(String in, String out) {
-        assertEquals(out, expressionFunction.enhance(in));
+        assertThat(expressionFunction.enhance(in)).isEqualTo(out);
     }
 
 }
--- modules/flowable-cmmn-engine/src/test/java/org/flowable/cmmn/test/el/VariableNotEqualsExpressionEnhancerTest.java
+++ modules/flowable-cmmn-engine/src/test/java/org/flowable/cmmn/test/el/VariableNotEqualsExpressionEnhancerTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,7 +12,7 @@
  */
 package org.flowable.cmmn.test.el;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.flowable.common.engine.impl.el.function.VariableNotEqualsExpressionFunction;
 import org.junit.Test;
@@ -21,22 +21,22 @@ import org.junit.Test;
  * @author Joram Barrez
  */
 public class VariableNotEqualsExpressionEnhancerTest {
-    
+
     private VariableNotEqualsExpressionFunction expressionFunction = new VariableNotEqualsExpressionFunction();
-    
+
     @Test
     public void testRegexNameReplacement() {
         assertRegexCorrect("${variables:notEquals(myVar,123)}", "${variables:notEquals(variableContainer,'myVar',123)}");
         assertRegexCorrect("${vars:notEquals(myVar,123)}", "${variables:notEquals(variableContainer,'myVar',123)}");
         assertRegexCorrect("${var:notEquals(myVar,123)}", "${variables:notEquals(variableContainer,'myVar',123)}");
-        
+
         assertRegexCorrect("${variables:ne(myVar,123)}", "${variables:notEquals(variableContainer,'myVar',123)}");
         assertRegexCorrect("${vars:ne(myVar,123)}", "${variables:notEquals(variableContainer,'myVar',123)}");
         assertRegexCorrect("${var:ne(myVar,123)}", "${variables:notEquals(variableContainer,'myVar',123)}");
     }
-        
+
     public void assertRegexCorrect(String in, String out) {
-        assertEquals(out, expressionFunction.enhance(in));
+        assertThat(expressionFunction.enhance(in)).isEqualTo(out);
     }
 
 }
