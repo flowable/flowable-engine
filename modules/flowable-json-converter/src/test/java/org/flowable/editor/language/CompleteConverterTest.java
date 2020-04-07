af683fda2c819501dccfc6222abf822a7ee797bb
dbmalkovsky <david@malkovsky.org>
2020-04-07 09:53:11 -0400

    Use assertj fluent style in flowable-json-converter module. All test files in the `flowable-json-converter` module.
    Converted a mixture of Junit 4 and 5 and assertj code into all Junit 5
    and assertj statements.

--- modules/flowable-json-converter/src/test/java/org/flowable/editor/language/CompleteConverterTest.java
+++ modules/flowable-json-converter/src/test/java/org/flowable/editor/language/CompleteConverterTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,8 +13,6 @@
 package org.flowable.editor.language;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.Arrays;
 
 import org.flowable.bpmn.model.BpmnModel;
 import org.flowable.bpmn.model.FlowElement;
@@ -72,7 +70,7 @@ public class CompleteConverterTest extends AbstractConverterTest {
         assertThat(flowElement).isInstanceOf(ReceiveTask.class);
         assertThat(flowElement.getId()).isEqualTo("receiveTask");
 
-        assertThat(model.getMainProcess().getCandidateStarterUsers()).isEqualTo(Arrays.asList("user1", "user2"));
-        assertThat(model.getMainProcess().getCandidateStarterGroups()).isEqualTo(Arrays.asList("group1", "group2"));
+        assertThat(model.getMainProcess().getCandidateStarterUsers()).containsExactly("user1", "user2");
+        assertThat(model.getMainProcess().getCandidateStarterGroups()).containsExactly("group1", "group2");
     }
 }
