ef59519c34ba3e3a530411dfe41ba1c8eb513845
David B Malkovsky <david@malkovsky.org>
2020-04-21 03:26:45 -0400

    Use assertj fluent style in flowable-engine module. (#2284)
    * Use assertj fluent style in flowable-engine module.
    Various test files in the `org.flowable.standalone` directory.
    
    Specifically test files in the `initialization`, `interceptor`, `jpa`,
    `parsing`, `rules`, `scripting` and `validation` packages.
    
    * Use assertj fluent style in flowable-engine module.
    Various test files in the `org.flowable.standalone` directory.
    
    Specifically test files in the `initialization`, `interceptor`, `jpa`,
    `parsing`, `rules`, `scripting` and `validation` packages.

--- modules/flowable-engine/src/test/java/org/flowable/standalone/initialization/ProcessEngineInitializationTest.java
+++ modules/flowable-engine/src/test/java/org/flowable/standalone/initialization/ProcessEngineInitializationTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,11 +12,16 @@
  */
 package org.flowable.standalone.initialization;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.flowable.bpmn.model.Process;
 import org.flowable.common.engine.api.FlowableException;
 import org.flowable.common.engine.api.FlowableWrongDbException;
 import org.flowable.common.engine.impl.db.DbSqlSession;
@@ -34,23 +39,22 @@ public class ProcessEngineInitializationTest extends AbstractTestCase {
 
     @Test
     public void testNoTables() {
-        try {
-            ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("org/flowable/standalone/initialization/notables.flowable.cfg.xml").buildProcessEngine();
-            fail("expected exception");
-        } catch (Exception e) {
-            // OK
-            assertTextPresent("no flowable tables in db", e.getMessage());
-        }
+        assertThatThrownBy(() -> ProcessEngineConfiguration
+                .createProcessEngineConfigurationFromResource("org/flowable/standalone/initialization/notables.flowable.cfg.xml").buildProcessEngine())
+                .isInstanceOf(FlowableException.class)
+                .hasMessageContaining("no flowable tables in db");
     }
 
     @Test
     public void testVersionMismatch() {
         // first create the schema
-        ProcessEngineImpl processEngine = (ProcessEngineImpl) ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("org/flowable/standalone/initialization/notables.flowable.cfg.xml")
+        ProcessEngineImpl processEngine = (ProcessEngineImpl) ProcessEngineConfiguration
+                .createProcessEngineConfigurationFromResource("org/flowable/standalone/initialization/notables.flowable.cfg.xml")
                 .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_CREATE_DROP).buildProcessEngine();
 
         // then update the version to something that is different to the library version
-        DbSqlSessionFactory dbSqlSessionFactory = (DbSqlSessionFactory) processEngine.getProcessEngineConfiguration().getSessionFactories().get(DbSqlSession.class);
+        DbSqlSessionFactory dbSqlSessionFactory = (DbSqlSessionFactory) processEngine.getProcessEngineConfiguration().getSessionFactories()
+                .get(DbSqlSession.class);
         SqlSessionFactory sqlSessionFactory = dbSqlSessionFactory.getSqlSessionFactory();
         SqlSession sqlSession = sqlSessionFactory.openSession();
         boolean success = false;
@@ -73,18 +77,17 @@ public class ProcessEngineInitializationTest extends AbstractTestCase {
             sqlSession.close();
         }
 
-        try {
-            // now we can see what happens if when a process engine is being
-            // build with a version mismatch between library and db tables
-            ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("org/flowable/standalone/initialization/notables.flowable.cfg.xml")
-                    .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_FALSE).buildProcessEngine();
-
-            fail("expected exception");
-        } catch (FlowableWrongDbException e) {
-            assertTextPresent("version mismatch", e.getMessage());
-            assertEquals("25.7", e.getDbVersion());
-            assertEquals(ProcessEngine.VERSION, e.getLibraryVersion());
-        }
+        // now we can see what happens if when a process engine is being
+        // build with a version mismatch between library and db tables
+        Consumer<FlowableWrongDbException> flowableWrongDbExceptionRequirements = flowableWrongDbException -> {
+            assertThat(flowableWrongDbException.getDbVersion()).isEqualTo("25.7");
+            assertThat(flowableWrongDbException.getLibraryVersion()).isEqualTo(ProcessEngine.VERSION);
+        };
+        assertThatThrownBy(() -> ProcessEngineConfiguration
+                .createProcessEngineConfigurationFromResource("org/flowable/standalone/initialization/notables.flowable.cfg.xml")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_FALSE).buildProcessEngine())
+                .isInstanceOfSatisfying(FlowableWrongDbException.class, flowableWrongDbExceptionRequirements)
+                .hasMessageContaining("version mismatch");
 
         // closing the original process engine to drop the db tables
         processEngine.close();
--- modules/flowable-engine/src/test/java/org/flowable/standalone/validation/DisabledSchemaValidationTest.java
+++ modules/flowable-engine/src/test/java/org/flowable/standalone/validation/DisabledSchemaValidationTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,6 +12,8 @@
  */
 package org.flowable.standalone.validation;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.flowable.bpmn.exceptions.XMLException;
 import org.flowable.common.engine.api.FlowableException;
 import org.flowable.engine.ProcessEngine;
@@ -19,7 +21,6 @@ import org.flowable.engine.ProcessEngines;
 import org.flowable.engine.RepositoryService;
 import org.flowable.engine.impl.cfg.StandaloneInMemProcessEngineConfiguration;
 import org.flowable.engine.repository.Deployment;
-import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -57,21 +58,16 @@ public class DisabledSchemaValidationTest {
     public void testDisableValidation() {
 
         // Should fail
-        try {
-            repositoryService.createDeployment().addClasspathResource("org/flowable/standalone/validation/invalid_process_xsd_error.bpmn20.xml").deploy();
-            Assert.fail();
-        } catch (XMLException e) {
-            // expected exception
-        }
+        assertThatThrownBy(
+                () -> repositoryService.createDeployment().addClasspathResource("org/flowable/standalone/validation/invalid_process_xsd_error.bpmn20.xml")
+                        .deploy())
+                .isExactlyInstanceOf(XMLException.class);
 
         // Should fail with validation errors
-        try {
-            repositoryService.createDeployment().addClasspathResource("org/flowable/standalone/validation/invalid_process_xsd_error.bpmn20.xml").disableSchemaValidation().deploy();
-            Assert.fail();
-        } catch (FlowableException e) {
-            // expected exception
-        }
-
+        assertThatThrownBy(
+                () -> repositoryService.createDeployment().addClasspathResource("org/flowable/standalone/validation/invalid_process_xsd_error.bpmn20.xml")
+                        .disableSchemaValidation().deploy())
+                .isExactlyInstanceOf(FlowableException.class);
     }
 
 }
