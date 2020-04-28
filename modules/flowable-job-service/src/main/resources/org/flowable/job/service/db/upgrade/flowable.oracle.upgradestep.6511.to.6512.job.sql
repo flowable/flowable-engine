262436b8a5a144ce7dfe32358357a91443ded7f4
Joram Barrez <jbarrez@users.noreply.github.com>
2020-04-28 16:45:11 +0200

    Fix upgrade scripts

--- modules/flowable-job-service/src/main/resources/org/flowable/job/service/db/upgrade/flowable.oracle.upgradestep.6511.to.6512.job.sql
+++ modules/flowable-job-service/src/main/resources/org/flowable/job/service/db/upgrade/flowable.oracle.upgradestep.6511.to.6512.job.sql
@@ -6,4 +6,4 @@ alter table ACT_RU_SUSPENDED_JOB add CATEGORY_ NVARCHAR2(255);
 
 alter table ACT_RU_DEADLETTER_JOB add CATEGORY_ NVARCHAR2(255);
 
-update ACT_GE_PROPERTY set VALUE_ = '6.5.1.3' where NAME_ = 'job.schema.version';
+update ACT_GE_PROPERTY set VALUE_ = '6.5.1.2' where NAME_ = 'job.schema.version';
