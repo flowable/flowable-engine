ac2599659b0cde9d2712dccfda23eeabef5eba44
David B Malkovsky <david@malkovsky.org>
2020-04-28 04:42:27 -0400

    Use assertj fluent style in flowable-spring module. (#2289)
    Various test files in the `org.flowable.spring.text` directory.
    
    Specifically test files in the `autodeployment`, `email`, `engine`, `eventregistry`, `executionListener`, `expression`, and `fieldinjection` packages.

--- modules/flowable-spring/src/test/java/org/flowable/spring/test/autodeployment/SingleResourceAutoDeploymentStrategyTest.java
+++ modules/flowable-spring/src/test/java/org/flowable/spring/test/autodeployment/SingleResourceAutoDeploymentStrategyTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,9 +13,7 @@
 
 package org.flowable.spring.test.autodeployment;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.never;
@@ -45,14 +43,14 @@ public class SingleResourceAutoDeploymentStrategyTest extends AbstractAutoDeploy
     public void before() throws Exception {
         super.before();
         classUnderTest = new SingleResourceAutoDeploymentStrategy();
-        assertNotNull(classUnderTest);
+        assertThat(classUnderTest).isNotNull();
     }
 
     @Test
     public void testHandlesMode() {
-        assertTrue(classUnderTest.handlesMode(SingleResourceAutoDeploymentStrategy.DEPLOYMENT_MODE));
-        assertFalse(classUnderTest.handlesMode("other-mode"));
-        assertFalse(classUnderTest.handlesMode(null));
+        assertThat(classUnderTest.handlesMode(SingleResourceAutoDeploymentStrategy.DEPLOYMENT_MODE)).isTrue();
+        assertThat(classUnderTest.handlesMode("other-mode")).isFalse();
+        assertThat(classUnderTest.handlesMode(null)).isFalse();
     }
 
     @Test
