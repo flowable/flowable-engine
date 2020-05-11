4995a540977ad47d401a87d94352ce9fb6b53b68
dbmalkovsky <david@malkovsky.org>
2020-05-11 11:05:53 -0400

    Use assertj fluent style in flowable-rest module. The module had a mix of assertion styles.  This is part 4 for this module. This is for the `org.flowable.rest.service.api.repository` package. Once the first part is merged I will come back and remove the private method that I included in some of the files as it is part of the base class.

--- modules/flowable-rest/src/test/java/org/flowable/rest/service/api/repository/DeploymentResourcesResourceTest.java
+++ modules/flowable-rest/src/test/java/org/flowable/rest/service/api/repository/DeploymentResourcesResourceTest.java
@@ -12,9 +12,8 @@
  */
 package org.flowable.rest.service.api.repository;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
@@ -29,9 +28,11 @@ import org.junit.Test;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import net.javacrumbs.jsonunit.core.Option;
+
 /**
  * Test for all REST-operations related to listing the resources that are part of a deployment.
- * 
+ *
  * @author Frederik Heremans
  */
 public class DeploymentResourcesResourceTest extends BaseSpringRestTestCase {
@@ -43,32 +44,29 @@ public class DeploymentResourcesResourceTest extends BaseSpringRestTestCase {
     public void testGetDeploymentResources() throws Exception {
 
         try {
-            Deployment deployment = repositoryService.createDeployment().name("Deployment 1").addClasspathResource("org/flowable/rest/service/api/repository/oneTaskProcess.bpmn20.xml")
+            Deployment deployment = repositoryService.createDeployment().name("Deployment 1")
+                    .addClasspathResource("org/flowable/rest/service/api/repository/oneTaskProcess.bpmn20.xml")
                     .addInputStream("test.txt", new ByteArrayInputStream("Test content".getBytes())).deploy();
 
             HttpGet httpGet = new HttpGet(SERVER_URL_PREFIX + RestUrls.createRelativeResourceUrl(RestUrls.URL_DEPLOYMENT_RESOURCES, deployment.getId()));
             CloseableHttpResponse response = executeRequest(httpGet, HttpStatus.SC_OK);
             JsonNode responseNode = objectMapper.readTree(response.getEntity().getContent());
             closeResponse(response);
-            assertTrue(responseNode.isArray());
-            assertEquals(2, responseNode.size());
 
-            // Since resources can be returned in any arbitrary order, find the
-            // right one to check
-            JsonNode txtNode = null;
-            for (int i = 0; i < responseNode.size(); i++) {
-                if ("test.txt".equals(responseNode.get(i).get("id").textValue())) {
-                    txtNode = responseNode.get(i);
-                    break;
-                }
-            }
+            assertThat(responseNode.isArray()).isTrue();
+            assertThat(responseNode).hasSize(2);
 
-            // Check URL's for the resource
-            assertNotNull(txtNode);
-            assertTrue(txtNode.get("url").textValue().endsWith(RestUrls.createRelativeResourceUrl(RestUrls.URL_DEPLOYMENT_RESOURCE, deployment.getId(), "test.txt")));
-            assertTrue(txtNode.get("contentUrl").textValue().endsWith(RestUrls.createRelativeResourceUrl(RestUrls.URL_DEPLOYMENT_RESOURCE_CONTENT, deployment.getId(), "test.txt")));
-            assertTrue(txtNode.get("mediaType").isNull());
-            assertEquals("resource", txtNode.get("type").textValue());
+            // Since resources can be returned in any arbitrary order, find the right one to check
+            assertThatJson(responseNode)
+                    .when(Option.IGNORING_EXTRA_FIELDS, Option.IGNORING_EXTRA_ARRAY_ITEMS, Option.IGNORING_ARRAY_ORDER)
+                    .isEqualTo("[{"
+                            + "    url: '" + SERVER_URL_PREFIX + RestUrls
+                            .createRelativeResourceUrl(RestUrls.URL_DEPLOYMENT_RESOURCE, deployment.getId(), "test.txt") + "',"
+                            + "    contentUrl: '" + SERVER_URL_PREFIX + RestUrls
+                            .createRelativeResourceUrl(RestUrls.URL_DEPLOYMENT_RESOURCE_CONTENT, deployment.getId(), "test.txt") + "',"
+                            + "    mediaType: null,"
+                            + "    type: 'resource'"
+                            + "}]");
 
         } finally {
             // Always cleanup any created deployments, even if the test failed
