c4d1408a2c8a82d61387feb48986c15cc64b40ce
Yevgeniy Bespal <yevbes@yevbes.com>
2020-06-11 11:06:41 +0200

    Scoped Variables builder

--- modules/flowable-task-service-api/src/main/java/org/flowable/task/api/TaskCompletionBuilder.java
+++ modules/flowable-task-service-api/src/main/java/org/flowable/task/api/TaskCompletionBuilder.java
@@ -20,32 +20,71 @@ import java.util.Map;
 public interface TaskCompletionBuilder {
 
 
+    /**
+     * Sets a variables before the task is completed.
+     * The variables will be stored on the plan item instance.
+     */
     TaskCompletionBuilder variables(Map<String, Object> variables);
 
     /**
-     * Sets a local variable before the transition is executed.
-     * The variable will be stored locally on the plan item instance.
+     * Sets a local variables before the task is completed.
+     * The variables will be stored locally on the plan item instance.
      */
     TaskCompletionBuilder variablesLocal(Map<String, Object> variablesLocal);
 
+    /**
+     * Sets a non-persisted variables before the task is completed.
+     * The transient variables will not be persisted at the end of the database transaction.
+     */
     TaskCompletionBuilder transientVariables(Map<String, Object> transientVariables);
 
+    /**
+     * Sets a non-persisted local variables before the task is completed.
+     * The local transient variables will not be persisted at the end of the database transaction.
+     */
     TaskCompletionBuilder transientVariablesLocal(Map<String, Object> transientVariablesLocal);
 
+    /**
+     * Sets a variable before the task is completed.
+     * The variable will be stored on the process instance.
+     */
     TaskCompletionBuilder variable(String variableName, Object variableValue);
 
+    /**
+     * Sets a local variable before the task is completed.
+     * The local variable will be stored on the process instance.
+     */
     TaskCompletionBuilder variableLocal(String variableName, Object variableValue);
 
+    /**
+     * Sets a non-persisted transient variable before the task is completed.
+     * The transient variable will not be persisted at the end of the database transaction.
+     */
     TaskCompletionBuilder transientVariable(String variableName, Object variableValue);
 
+    /**
+     * Sets a non-persisted transient local variable before the task is completed.
+     * The transient local variable will not be persisted at the end of the database transaction.
+     */
     TaskCompletionBuilder transientVariableLocal(String variableName, Object variableValue);
 
+    /**
+     * Sets a task id
+     */
     TaskCompletionBuilder taskId(String id);
+
+    /**
+     * Sets a form definition id
+     */
     TaskCompletionBuilder formDefinitionId(String formDefinitionId);
+
+    /**
+     * Sets an outcome
+     */
     TaskCompletionBuilder outcome(String outcome);
 
     /**
-     * Completes the task
+     * Completes a task
      */
     void complete();
 }
