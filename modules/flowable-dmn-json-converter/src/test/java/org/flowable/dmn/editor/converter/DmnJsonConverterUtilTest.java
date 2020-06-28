d5c6f95c775da5913d57eff66712328411f6fd0c
dbmalkovsky <david@malkovsky.org>
2020-06-28 09:08:00 -0400

    Use assertj fluent style in flowable-dmn-json-converter module.

--- modules/flowable-dmn-json-converter/src/test/java/org/flowable/dmn/editor/converter/DmnJsonConverterUtilTest.java
+++ modules/flowable-dmn-json-converter/src/test/java/org/flowable/dmn/editor/converter/DmnJsonConverterUtilTest.java
@@ -12,8 +12,8 @@
  */
 package org.flowable.dmn.editor.converter;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,8 +24,6 @@ import org.apache.commons.io.IOUtils;
 import org.flowable.dmn.model.DecisionTable;
 import org.flowable.dmn.model.DmnDefinition;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -34,8 +32,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * @author Yvo Swillens
  */
 public class DmnJsonConverterUtilTest {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(DmnJsonConverterUtilTest.class);
 
     private static final String JSON_RESOURCE_1 = "org/flowable/dmn/editor/converter/decisiontable_regression_model_v2.json";
 
@@ -47,22 +43,22 @@ public class DmnJsonConverterUtilTest {
 
         testJsonResource = new DmnJsonConverterUtil().migrateModelV3(testJsonResource, OBJECT_MAPPER);
 
-        assertEquals("3", testJsonResource.get("modelVersion").asText());
+        assertThat(testJsonResource.get("modelVersion").asText()).isEqualTo("3");
 
         DmnDefinition dmnDefinition = new DmnJsonConverter().convertToDmn(testJsonResource, "abc", 1, new Date());
         DecisionTable decisionTable = (DecisionTable) dmnDefinition.getDecisions().get(0).getExpression();
 
-        assertEquals("${collection:noneOf(collection1, '\"TEST1\",\"TEST2\"')}",  decisionTable.getRules().get(0).getInputEntries().get(0).getInputEntry().getText());
-        assertEquals("${collection:noneOf('\"TEST1\",\"TEST2\"', input2)}",  decisionTable.getRules().get(0).getInputEntries().get(1).getInputEntry().getText());
+        assertThat(decisionTable.getRules().get(0).getInputEntries().get(0).getInputEntry().getText()).isEqualTo("${collection:noneOf(collection1, '\"TEST1\",\"TEST2\"')}");
+        assertThat(decisionTable.getRules().get(0).getInputEntries().get(1).getInputEntry().getText()).isEqualTo("${collection:noneOf('\"TEST1\",\"TEST2\"', input2)}");
 
-        assertEquals("${collection:notAllOf(collection1, '\"TEST1\",\"TEST5\"')}",  decisionTable.getRules().get(1).getInputEntries().get(0).getInputEntry().getText());
-        assertEquals("${collection:noneOf('\"TEST1\",\"TEST5\"', input2)}",  decisionTable.getRules().get(1).getInputEntries().get(1).getInputEntry().getText());
+        assertThat(decisionTable.getRules().get(1).getInputEntries().get(0).getInputEntry().getText()).isEqualTo("${collection:notAllOf(collection1, '\"TEST1\",\"TEST5\"')}");
+        assertThat(decisionTable.getRules().get(1).getInputEntries().get(1).getInputEntry().getText()).isEqualTo("${collection:noneOf('\"TEST1\",\"TEST5\"', input2)}");
 
-        assertEquals("${collection:anyOf(collection1, '\"TEST1\",\"TEST6\"')}",  decisionTable.getRules().get(2).getInputEntries().get(0).getInputEntry().getText());
-        assertEquals("${collection:allOf('\"TEST1\",\"TEST6\"', input2)}",  decisionTable.getRules().get(2).getInputEntries().get(1).getInputEntry().getText());
+        assertThat(decisionTable.getRules().get(2).getInputEntries().get(0).getInputEntry().getText()).isEqualTo("${collection:anyOf(collection1, '\"TEST1\",\"TEST6\"')}");
+        assertThat(decisionTable.getRules().get(2).getInputEntries().get(1).getInputEntry().getText()).isEqualTo("${collection:allOf('\"TEST1\",\"TEST6\"', input2)}");
 
-        assertEquals("${collection:allOf(collection1, \"TEST1\")}",  decisionTable.getRules().get(3).getInputEntries().get(0).getInputEntry().getText());
-        assertEquals("${collection:allOf('\"TEST1\",\"TEST6\"', input2)}",  decisionTable.getRules().get(3).getInputEntries().get(1).getInputEntry().getText());
+        assertThat(decisionTable.getRules().get(3).getInputEntries().get(0).getInputEntry().getText()).isEqualTo("${collection:allOf(collection1, \"TEST1\")}");
+        assertThat(decisionTable.getRules().get(3).getInputEntries().get(1).getInputEntry().getText()).isEqualTo("${collection:allOf('\"TEST1\",\"TEST6\"', input2)}");
     }
 
 
