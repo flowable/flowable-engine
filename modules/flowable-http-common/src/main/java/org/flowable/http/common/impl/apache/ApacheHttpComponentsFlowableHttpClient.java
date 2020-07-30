4b398384d2fa525ea7b6cb0b5aa0cad8e87be795
Filip Hrisafov <filip.hrisafov@gmail.com>
2020-07-30 17:42:02 +0200

    Add a Spring WebClient implementation of the FlowableAsyncHttpClient

--- modules/flowable-http-common/src/main/java/org/flowable/http/common/impl/apache/ApacheHttpComponentsFlowableHttpClient.java
+++ modules/flowable-http-common/src/main/java/org/flowable/http/common/impl/apache/ApacheHttpComponentsFlowableHttpClient.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.flowable.http.client;
+package org.flowable.http.common.impl.apache;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
