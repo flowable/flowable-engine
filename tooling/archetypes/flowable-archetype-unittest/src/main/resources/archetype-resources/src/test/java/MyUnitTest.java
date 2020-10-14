82a2a1b740f00def9ead7011373fd8bbf380db71
dbmalkovsky <david@malkovsky.org>
2020-10-14 16:42:58 -0400

    Update to more current standards

--- tooling/archetypes/flowable-archetype-unittest/src/main/resources/archetype-resources/src/test/java/MyUnitTest.java
+++ tooling/archetypes/flowable-archetype-unittest/src/main/resources/archetype-resources/src/test/java/MyUnitTest.java
@@ -1,6 +1,18 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ${package};
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.engine.test.FlowableRule;
@@ -18,10 +30,10 @@ public class MyUnitTest {
     @Deployment(resources = {"${packageInPathFormat}/my-process.bpmn20.xml"})
     public void test() {
         ProcessInstance processInstance = flowableRule.getRuntimeService().startProcessInstanceByKey("my-process");
-        assertNotNull(processInstance);
+        assertThat(processInstance).isNotNull();
 
         Task task = flowableRule.getTaskService().createTaskQuery().singleResult();
-        assertEquals("Flowable is awesome!", task.getName());
+        assertThat(task.getName()).isEqualTo("Flowable is awesome!");
     }
 
 }
