85a0b0f0edde27b9bd16bb64d111a316181b46da
Yvo Swillens <yvoswillens@gmail.com>
2021-01-20 13:31:58 +0100

    fixes possible StackOverflowError when parsing DMN OutputValues

--- modules/flowable-dmn-xml-converter/src/main/java/org/flowable/dmn/converter/child/OutputValuesParser.java
+++ modules/flowable-dmn-xml-converter/src/main/java/org/flowable/dmn/converter/child/OutputValuesParser.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,6 +14,10 @@ package org.flowable.dmn.converter.child;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 import javax.xml.stream.XMLStreamReader;
 
@@ -48,12 +52,7 @@ public class OutputValuesParser extends BaseChildElementParser {
                 if (xtr.isStartElement() && ELEMENT_TEXT.equalsIgnoreCase(xtr.getLocalName())) {
                     String outputValuesText = xtr.getElementText();
 
-                    if (StringUtils.isNotEmpty(outputValuesText)) {
-                        String[] outputValuesSplit = outputValuesText.replaceAll("^\"", "").split("\"?(,|$)(?=(([^\"]*\"){2})*[^\"]*$) *\"?");
-                        outputValues.setTextValues(new ArrayList<>(Arrays.asList(outputValuesSplit)));
-                    }
-
-
+                    outputValues.setTextValues(splitAndFormatOutputValuesWithTokenizer(outputValuesText));
                 } else if (xtr.isEndElement() && getElementName().equalsIgnoreCase(xtr.getLocalName())) {
                     readyWithOutputValues = true;
                 }
@@ -63,5 +62,39 @@ public class OutputValuesParser extends BaseChildElementParser {
         }
 
         clause.setOutputValues(outputValues);
+    }
+
+    public List<Object> splitAndFormatOutputValuesWithTokenizer(String outputValuesText) {
+        if (StringUtils.isEmpty(outputValuesText)) {
+            return Collections.emptyList();
+        }
+
+        List<String> splitTokens = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(outputValuesText, ",");
+        while (tokenizer.hasMoreElements()) {
+            splitTokens.add(tokenizer.nextToken());
+        }
+
+        List<Object> formattedTokens = formatTokens(splitTokens);
+
+        return formattedTokens;
+    }
+
+    public List<Object> splitAndFormatOutputValues(String outputValuesText) {
+        if (StringUtils.isEmpty(outputValuesText)) {
+            return Collections.emptyList();
+        }
+
+        String[] outputValuesSplit = outputValuesText.replaceAll("^\"", "").split("\"?(,|$)(?=(([^\"]*\"){2})*[^\"]*$) *\"?");
+        return new ArrayList<>(Arrays.asList(outputValuesSplit));
+    }
+
+    protected List<Object> formatTokens(List<String> splitTokens) {
+        // remove start quote
+        // remove end quote
+        // remove space start quote
+        return splitTokens.stream()
+                .map(elem -> elem.replaceAll("^\"|^ \"|\"$", ""))
+                .collect(Collectors.toList());
     }
 }
