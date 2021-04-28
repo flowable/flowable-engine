f27d7bccee053e8bb6a9c7dca8eeef43cce5513d
Tijs Rademakers <tijs.rademakers@gmail.com>
2021-04-28 12:08:34 +0200

    Added artifacts map for easier access

--- modules/flowable-bpmn-model/src/main/java/org/flowable/bpmn/model/FlowElementsContainer.java
+++ modules/flowable-bpmn-model/src/main/java/org/flowable/bpmn/model/FlowElementsContainer.java
@@ -37,8 +37,12 @@ public interface FlowElementsContainer {
     Artifact getArtifact(String id);
 
     Collection<Artifact> getArtifacts();
+    
+    Map<String, Artifact> getArtifactMap();
 
     void addArtifact(Artifact artifact);
+    
+    void addArtifactToMap(Artifact artifact);
 
     void removeArtifact(String artifactId);
 
