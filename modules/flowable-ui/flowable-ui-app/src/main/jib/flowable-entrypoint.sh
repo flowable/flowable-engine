52516c7d7627e7490cbfc8e9aade278d480c10da
im-pratham <prathameshmane1234@gmail.com>
2021-06-02 17:25:34 +0530

    flowable entrypoint script and permission changes

--- modules/flowable-ui/flowable-ui-app/src/main/jib/flowable-entrypoint.sh
+++ modules/flowable-ui/flowable-ui-app/src/main/jib/flowable-entrypoint.sh
@@ -3,7 +3,15 @@ JAVA_OPTS="${JAVA_OPTS}
   -Djava.awt.headless=true
   -Dfile.encoding=UTF-8"
 
-exec su-exec flowable:flowable java ${JAVA_OPTS} \
-  -cp /app:/app/WEB-INF/classes:/app/WEB-INF/lib/* \
-  org.flowable.ui.application.FlowableUiApplication \
-  ${@}
+cmd="java ${JAVA_OPTS} \
+          -cp /app:/app/WEB-INF/classes:/app/WEB-INF/lib/* \
+          org.flowable.ui.application.FlowableUiApplication \
+          ${@}"
+
+## if user is part of root group, then 
+if [ $(id -g) -eq 0 -a $(id -u) -ne 0 ]
+then
+        exec $cmd
+else
+        exec su-exec flowable:flowable $cmd
+fi
