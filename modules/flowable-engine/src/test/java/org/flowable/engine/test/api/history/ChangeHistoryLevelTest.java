e543a3e0efa6ab77f04f9dd2d4eda13bda93cba1
martin.grofcik <gromar01@gmail.com>
2021-06-15 17:35:36 +0200

    #2953 code style fixes

--- modules/flowable-engine/src/test/java/org/flowable/engine/test/api/history/ChangeHistoryLevelTest.java
+++ modules/flowable-engine/src/test/java/org/flowable/engine/test/api/history/ChangeHistoryLevelTest.java
@@ -1,4 +1,21 @@
+
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.flowable.engine.test.api.history;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
 
 import org.flowable.common.engine.impl.history.HistoryLevel;
 import org.flowable.engine.HistoryService;
@@ -14,10 +31,6 @@ import org.flowable.variable.api.history.HistoricVariableInstance;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @FlowableTest
 public class ChangeHistoryLevelTest {
