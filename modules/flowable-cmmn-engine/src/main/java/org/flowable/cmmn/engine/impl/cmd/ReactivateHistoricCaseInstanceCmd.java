088c0a732e6202d4f74e793ae58c875bcb9392bb
micha.kiener <micha.kiener@gmail.com>
2021-08-06 14:10:53 +0200

    support available condition expression on a case reactivate event listener

--- modules/flowable-cmmn-engine/src/main/java/org/flowable/cmmn/engine/impl/cmd/ReactivateHistoricCaseInstanceCmd.java
+++ modules/flowable-cmmn-engine/src/main/java/org/flowable/cmmn/engine/impl/cmd/ReactivateHistoricCaseInstanceCmd.java
@@ -14,13 +14,16 @@ package org.flowable.cmmn.engine.impl.cmd;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang3.StringUtils;
 import org.flowable.cmmn.api.history.HistoricCaseInstance;
 import org.flowable.cmmn.api.runtime.CaseInstance;
 import org.flowable.cmmn.api.runtime.CaseInstanceState;
 import org.flowable.cmmn.engine.CmmnEngineConfiguration;
 import org.flowable.cmmn.engine.impl.persistence.entity.CaseInstanceEntity;
 import org.flowable.cmmn.engine.impl.reactivation.CaseReactivationBuilderImpl;
+import org.flowable.cmmn.engine.impl.repository.CaseDefinitionUtil;
 import org.flowable.cmmn.engine.impl.util.CommandContextUtil;
+import org.flowable.cmmn.model.ReactivateEventListener;
 import org.flowable.common.engine.api.FlowableIllegalArgumentException;
 import org.flowable.common.engine.api.FlowableIllegalStateException;
 import org.flowable.common.engine.api.FlowableObjectNotFoundException;
@@ -48,6 +51,7 @@ public class ReactivateHistoricCaseInstanceCmd implements Command<CaseInstance>,
         if (reactivationBuilder.getCaseInstanceId() == null) {
             throw new FlowableIllegalArgumentException("No historic case instance id provided");
         }
+
         // Check if the historic case instance is found and if it is no longer running
         CmmnEngineConfiguration cmmnEngineConfiguration = CommandContextUtil.getCmmnEngineConfiguration(commandContext);
         HistoricCaseInstance instance = cmmnEngineConfiguration.getHistoricCaseInstanceEntityManager().createHistoricCaseInstanceQuery()
@@ -61,6 +65,7 @@ public class ReactivateHistoricCaseInstanceCmd implements Command<CaseInstance>,
             throw new FlowableIllegalStateException("Case instance is still running, cannot reactivate historic case instance: " + reactivationBuilder.getCaseInstanceId());
         }
 
+        // move the case instance back to the runtime (this also checks, if the reactivation listener is even existent)
         CaseInstanceEntity caseInstanceEntity = cmmnEngineConfiguration.getCaseInstanceHelper()
             .copyHistoricCaseInstanceToRuntime(instance);
 
@@ -68,11 +73,6 @@ public class ReactivateHistoricCaseInstanceCmd implements Command<CaseInstance>,
         caseInstanceEntity.setState(CaseInstanceState.ACTIVE);
         caseInstanceEntity.setLastReactivationTime(cmmnEngineConfiguration.getClock().getCurrentTime());
         caseInstanceEntity.setLastReactivationUserId(Authentication.getAuthenticatedUserId());
-
-        // invoke the identity link interceptor to record the reactivation user
-        if (cmmnEngineConfiguration.getIdentityLinkInterceptor() != null) {
-            cmmnEngineConfiguration.getIdentityLinkInterceptor().handleReactivateCaseInstance(caseInstanceEntity);
-        }
 
         // set case variables, if the builder contains any
         if (reactivationBuilder.hasVariables()) {
@@ -82,6 +82,26 @@ public class ReactivateHistoricCaseInstanceCmd implements Command<CaseInstance>,
         // set transient case variables, if the builder contains any
         if (reactivationBuilder.hasTransientVariables()) {
             caseInstanceEntity.setTransientVariables(reactivationBuilder.getTransientVariables());
+        }
+
+        // if there is an available condition on the reactivation event listener, evaluate it to make sure the listener is available for reactivation
+        ReactivateEventListener reactivateEventListener = CaseDefinitionUtil.getCase(instance.getCaseDefinitionId()).getReactivateEventListener();
+        String availableConditionExpression = reactivateEventListener.getReactivationAvailableConditionExpression();
+        if (StringUtils.isNotEmpty(availableConditionExpression)) {
+            Object listenerAvailable = CommandContextUtil.getCmmnEngineConfiguration()
+                .getExpressionManager()
+                .createExpression(availableConditionExpression)
+                .getValue(caseInstanceEntity);
+
+            if (!Boolean.TRUE.equals(listenerAvailable)) {
+                throw new FlowableIllegalStateException("The case instance " + caseInstanceEntity.getId()
+                    + " cannot be reactivated, as the available condition of its reactivate event listener did not evaluate to true.");
+            }
+        }
+
+        // invoke the identity link interceptor to record the reactivation user
+        if (cmmnEngineConfiguration.getIdentityLinkInterceptor() != null) {
+            cmmnEngineConfiguration.getIdentityLinkInterceptor().handleReactivateCaseInstance(caseInstanceEntity);
         }
 
         // record the reactivation of the case in the history manager to also synchronize the new state and last reactivation data to the history
