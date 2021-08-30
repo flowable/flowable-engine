51dbafd30ae5262c4d3bda97c16d49f8b3c4049d
Tijs Rademakers <tijs.rademakers@gmail.com>
2021-08-30 12:54:36 +0200

    Move to db version 6.7.0.0

--- modules/flowable-engine/src/main/resources/org/flowable/db/create/flowable.postgres.create.engine.sql
+++ modules/flowable-engine/src/main/resources/org/flowable/db/create/flowable.postgres.create.engine.sql
@@ -347,7 +347,7 @@ alter table ACT_PROCDEF_INFO
     unique (PROC_DEF_ID_);
 
 insert into ACT_GE_PROPERTY
-values ('schema.version', '6.6.2.1', 1);
+values ('schema.version', '6.7.0.0', 1);
 
 insert into ACT_GE_PROPERTY
-values ('schema.history', 'create(6.6.2.1)', 1);
+values ('schema.history', 'create(6.7.0.0)', 1);
