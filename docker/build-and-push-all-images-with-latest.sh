de443938a7a0cea8407107bfd6c3767e9cd2e6f5
Yvo Swillens <yvoswillens@gmail.com>
2021-08-31 10:36:11 +0200

    updated build script docker images

--- docker/build-and-push-all-images-with-latest.sh
+++ docker/build-and-push-all-images-with-latest.sh
@@ -5,10 +5,10 @@ mvn -Pdistro clean install -DskipTests
 
 echo "Building and pushing REST app image"
 cd modules/flowable-app-rest
-mvn -Pdocker,swagger clean deploy
+mvn -PdockerPublishWithLatest,swagger clean package
 
 echo "Building and pushing UI app image"
 cd ../flowable-ui
-mvn -Pdocker clean deploy
+mvn -PdockerPublishWithLatest clean package
 
 echo "Done..."
