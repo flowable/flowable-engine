b404c9554af400c6ab12332bfe76d04558fe2b8b
Filip Hrisafov <filip.hrisafov@gmail.com>
2021-10-14 10:05:55 +0200

    Use Batch Part type, searchKey and searchKey2 as standalone properties

--- modules/flowable-batch-service-api/src/main/java/org/flowable/batch/api/BatchPart.java
+++ modules/flowable-batch-service-api/src/main/java/org/flowable/batch/api/BatchPart.java
@@ -16,31 +16,88 @@ import java.util.Date;
 
 public interface BatchPart {
 
+    /**
+     * The technical id of the batch part
+     */
     String getId();
 
+    /**
+     * The type of the batch part
+     */
+    String getType();
+
+    /**
+     * The type of the batch that this batch part is linked to
+     */
     String getBatchType();
 
+    /**
+     * The id of the batch that this batch part is linked to
+     */
     String getBatchId();
 
+    /**
+     * The time the batch part was created
+     */
     Date getCreateTime();
 
+    /**
+     * The time the batch part was completed
+     */
     Date getCompleteTime();
 
+    /**
+     * Whether the batch part is completed
+     */
     boolean isCompleted();
 
+    /**
+     * The search key of the batch part
+     */
+    String getSearchKey();
+
+    /**
+     * The second search key of the batch part
+     */
+    String getSearchKey2();
+
+    /**
+     * The search key of the batch that this batch part is linked to
+     */
     String getBatchSearchKey();
 
+    /**
+     * The second search key of the batch that this batch part is linked to
+     */
     String getBatchSearchKey2();
     
+    /**
+     * The status of the batch part
+     */
     String getStatus();
 
+    /**
+     * The scope id of the batch part
+     */
     String getScopeId();
     
+    /**
+     * The scope type of the batch part
+     */
     String getScopeType();
     
+    /**
+     * The sub scope id of the batch part
+     */
     String getSubScopeId();
 
+    /**
+     * The result document of the batch part
+     */
     String getResultDocumentJson(String engineType);
     
+    /**
+     * The tenant id of the batch part
+     */
     String getTenantId();
 }
