c25d2ec2886f5ff8a5d85d1041d3c71ffd3fe61f
Christopher Welsch <75045836+WelschChristopher@users.noreply.github.com>
2021-11-10 11:19:34 +0100

    refactored the different job queries so they extend a BaseJobQuery Interface to centralize method declaration

--- modules/flowable-job-service-api/src/main/java/org/flowable/job/api/TimerJobQuery.java
+++ modules/flowable-job-service-api/src/main/java/org/flowable/job/api/TimerJobQuery.java
@@ -13,83 +13,22 @@
 
 package org.flowable.job.api;
 
-import java.util.Date;
-
-import org.flowable.common.engine.api.query.Query;
-
 /**
  * Allows programmatic querying of {@link Job}s.
- * 
+ *
  * @author Joram Barrez
  * @author Tijs Rademakers
  */
-public interface TimerJobQuery extends Query<TimerJobQuery, Job> {
-
-    /** Only select jobs with the given id */
-    TimerJobQuery jobId(String jobId);
-
-    /** Only select jobs which exist for the given process instance. **/
-    TimerJobQuery processInstanceId(String processInstanceId);
-    
-    /** Only select jobs without a process instance id value. **/
-    TimerJobQuery withoutProcessInstanceId();
-
-    /** Only select jobs which exist for the given execution */
-    TimerJobQuery executionId(String executionId);
-
-    /** Only select jobs which exist for the given process definition id */
-    TimerJobQuery processDefinitionId(String processDefinitionid);
-    
-    /** Only select jobs which exist for the given category */
-    TimerJobQuery category(String category);
-    
-    /** Only select jobs like for the given category value */
-    TimerJobQuery categoryLike(String categoryLike);
-    
-    /** Only select jobs which exist for the given element id */
-    TimerJobQuery elementId(String elementId);
-    
-    /** Only select jobs which exist for the given element name */
-    TimerJobQuery elementName(String elementName);
-    
-    /** Only select jobs for the given scope identifier. */
-    TimerJobQuery scopeId(String scopeId);
-    
-    /** Only select jobs without a scope id value. **/
-    TimerJobQuery withoutScopeId();
-
-    /** Only select jobs for the given sub scope identifier. */
-    TimerJobQuery subScopeId(String subScopeId);
-    
-    /** Only select jobs for the given scope type. */
-    TimerJobQuery scopeType(String scopeType);
-
-    /**
-     * Only return jobs that do not have a scope type.
-     */
-    TimerJobQuery withoutScopeType();
-    
-    /** Only select tasks for the given scope definition identifier. */
-    TimerJobQuery scopeDefinitionId(String scopeDefinitionId);
-    
-    /** Only select jobs for the given case instance. */
-    TimerJobQuery caseInstanceId(String caseInstanceId);
-    
-    /** Only select jobs for the given case definition. */
-    TimerJobQuery caseDefinitionId(String caseDefinitionId);
-    
-    /** Only select jobs for the given plan item instance.  */
-    TimerJobQuery planItemInstanceId(String planItemInstanceId);
-
-    /** Only select jobs with the given correlationId.  */
-    TimerJobQuery correlationId(String correlationId);
+public interface TimerJobQuery extends BaseJobQuery<TimerJobQuery, Job> {
 
     /**
      * Only select jobs which are executable, ie. duedate is null or duedate is in the past
      **/
     TimerJobQuery executable();
 
-    /** Select jobs which have given job handler type */
+    /**
+     * Select jobs which have given job handler type
+     */
     TimerJobQuery handlerType(String handlerType);
 
     /**
@@ -101,69 +40,5 @@ public interface TimerJobQuery extends Query<TimerJobQuery, Job> {
      * Only select jobs that are messages. Cannot be used together with {@link #timers()}
      */
     TimerJobQuery messages();
-
-    /** Only select jobs where the duedate is lower than the given date. */
-    TimerJobQuery duedateLowerThan(Date date);
-
-    /** Only select jobs where the duedate is higher then the given date. */
-    TimerJobQuery duedateHigherThan(Date date);
-
-    /** Only select jobs that failed due to an exception. */
-    TimerJobQuery withException();
-
-    /** Only select jobs that failed due to an exception with the given message. */
-    TimerJobQuery exceptionMessage(String exceptionMessage);
-
-    /**
-     * Only select jobs that have the given tenant id.
-     */
-    TimerJobQuery jobTenantId(String tenantId);
-
-    /**
-     * Only select jobs with a tenant id like the given one.
-     */
-    TimerJobQuery jobTenantIdLike(String tenantIdLike);
-
-    /**
-     * Only select jobs that do not have a tenant id.
-     */
-    TimerJobQuery jobWithoutTenantId();
-
-    // sorting //////////////////////////////////////////
-
-    /**
-     * Order by job id (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    TimerJobQuery orderByJobId();
-
-    /**
-     * Order by duedate (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    TimerJobQuery orderByJobDuedate();
-
-    /**
-     * Order by create time (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    TimerJobQuery orderByJobCreateTime();
-
-    /**
-     * Order by retries (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    TimerJobQuery orderByJobRetries();
-
-    /**
-     * Order by process instance id (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    TimerJobQuery orderByProcessInstanceId();
-
-    /**
-     * Order by execution id (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    TimerJobQuery orderByExecutionId();
-
-    /**
-     * Order by tenant id (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    TimerJobQuery orderByTenantId();
 
 }
