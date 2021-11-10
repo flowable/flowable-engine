1f36fe71d61e15464e46364e0ded1378f2fd2fe9
Christopher Welsch <75045836+WelschChristopher@users.noreply.github.com>
2021-11-10 11:19:34 +0100

    refactored the different job queries so they extend a BaseJobQuery Interface to centralize method declaration

--- modules/flowable-job-service-api/src/main/java/org/flowable/job/api/DeadLetterJobQuery.java
+++ modules/flowable-job-service-api/src/main/java/org/flowable/job/api/DeadLetterJobQuery.java
@@ -13,79 +13,13 @@
 
 package org.flowable.job.api;
 
-import java.util.Date;
-
-import org.flowable.common.engine.api.query.Query;
-
 /**
  * Allows programmatic querying of {@link Job}s.
  *
  * @author Joram Barrez
  * @author Tijs Rademakers
  */
-public interface DeadLetterJobQuery extends Query<DeadLetterJobQuery, Job> {
-
-    /** Only select jobs with the given id */
-    DeadLetterJobQuery jobId(String jobId);
-
-    /** Only select jobs which exist for the given process instance. **/
-    DeadLetterJobQuery processInstanceId(String processInstanceId);
-    
-    /** Only select jobs without a process instance id value. **/
-    DeadLetterJobQuery withoutProcessInstanceId();
-
-    /** Only select jobs which exist for the given execution */
-    DeadLetterJobQuery executionId(String executionId);
-
-    /** Select jobs which have given job handler type */
-    DeadLetterJobQuery handlerType(String handlerType);
-
-    /** Only select jobs which exist for the given process definition id */
-    DeadLetterJobQuery processDefinitionId(String processDefinitionId);
-    
-    /** Only select jobs which exist for the given category */
-    DeadLetterJobQuery category(String category);
-    
-    /** Only select jobs like for the given category value */
-    DeadLetterJobQuery categoryLike(String categoryLike);
-    
-    /** Only select jobs which exist for the given element id */
-    DeadLetterJobQuery elementId(String elementId);
-    
-    /** Only select jobs which exist for the given element name */
-    DeadLetterJobQuery elementName(String elementName);
-    
-    /** Only select jobs for the given scope identifier. */
-    DeadLetterJobQuery scopeId(String scopeId);
-    
-    /** Only select jobs without a scope id value. **/
-    DeadLetterJobQuery withoutScopeId();
-
-    /** Only select jobs for the given sub scope identifier. */
-    DeadLetterJobQuery subScopeId(String subScopeId);
-    
-    /** Only select jobs for the given scope type. */
-    DeadLetterJobQuery scopeType(String scopeType);
-
-    /**
-     * Only select jobs that do not have a scope type.
-     */
-    DeadLetterJobQuery withoutScopeType();
-    
-    /** Only select jobs for the given scope definition identifier. */
-    DeadLetterJobQuery scopeDefinitionId(String scopeDefinitionId);
-    
-    /** Only select jobs for the given case instance. */
-    DeadLetterJobQuery caseInstanceId(String caseInstanceId);
-    
-    /** Only select jobs for the given case definition. */
-    DeadLetterJobQuery caseDefinitionId(String caseDefinitionId);
-    
-    /** Only select jobs for the given plan item instance.  */
-    DeadLetterJobQuery planItemInstanceId(String planItemInstanceId);
-
-    /** Only select jobs for with the given correlationId.  */
-    DeadLetterJobQuery correlationId(String correlationId);
+public interface DeadLetterJobQuery extends BaseJobQuery<DeadLetterJobQuery, Job> {
 
     /**
      * Only select jobs which are executable, ie. duedate is null or duedate is in the past
@@ -106,69 +40,5 @@ public interface DeadLetterJobQuery extends Query<DeadLetterJobQuery, Job> {
      * Only select external worker jobs. Cannot be used together with {@link #timers()} or {@link #messages()}
      */
     DeadLetterJobQuery externalWorkers();
-
-    /** Only select jobs where the duedate is lower than the given date. */
-    DeadLetterJobQuery duedateLowerThan(Date date);
-
-    /** Only select jobs where the duedate is higher then the given date. */
-    DeadLetterJobQuery duedateHigherThan(Date date);
-
-    /** Only select jobs that failed due to an exception. */
-    DeadLetterJobQuery withException();
-
-    /** Only select jobs that failed due to an exception with the given message. */
-    DeadLetterJobQuery exceptionMessage(String exceptionMessage);
-
-    /**
-     * Only select jobs that have the given tenant id.
-     */
-    DeadLetterJobQuery jobTenantId(String tenantId);
-
-    /**
-     * Only select jobs with a tenant id like the given one.
-     */
-    DeadLetterJobQuery jobTenantIdLike(String tenantIdLike);
-
-    /**
-     * Only select jobs that do not have a tenant id.
-     */
-    DeadLetterJobQuery jobWithoutTenantId();
-
-    // sorting //////////////////////////////////////////
-
-    /**
-     * Order by job id (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    DeadLetterJobQuery orderByJobId();
-
-    /**
-     * Order by duedate (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    DeadLetterJobQuery orderByJobDuedate();
-
-    /**
-     * Order by create time (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    DeadLetterJobQuery orderByJobCreateTime();
-
-    /**
-     * Order by retries (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    DeadLetterJobQuery orderByJobRetries();
-
-    /**
-     * Order by process instance id (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    DeadLetterJobQuery orderByProcessInstanceId();
-
-    /**
-     * Order by execution id (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    DeadLetterJobQuery orderByExecutionId();
-
-    /**
-     * Order by tenant id (needs to be followed by {@link #asc()} or {@link #desc()}).
-     */
-    DeadLetterJobQuery orderByTenantId();
 
 }
