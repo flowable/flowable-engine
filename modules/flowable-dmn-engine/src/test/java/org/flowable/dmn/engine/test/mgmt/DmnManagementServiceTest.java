37db92cc8b104230a9e05a41413a7334f82b190e
Yvo Swillens <yvoswillens@gmail.com>
2022-01-10 14:31:53 +0100

    added h2 column type

--- modules/flowable-dmn-engine/src/test/java/org/flowable/dmn/engine/test/mgmt/DmnManagementServiceTest.java
+++ modules/flowable-dmn-engine/src/test/java/org/flowable/dmn/engine/test/mgmt/DmnManagementServiceTest.java
@@ -75,7 +75,7 @@ public class DmnManagementServiceTest extends AbstractFlowableDmnTest {
         assertThat(startTimeIndex).isGreaterThanOrEqualTo(0);
 
         assertThat(tableMetaData.getColumnTypes().get(instanceIdIndex))
-                .isIn("VARCHAR", "VARCHAR2", "NVARCHAR2", "nvarchar", "NVARCHAR");
+                .isIn("VARCHAR", "VARCHAR2", "NVARCHAR2", "nvarchar", "NVARCHAR", "CHARACTER VARYING");
 
         assertThat(tableMetaData.getColumnTypes().get(startTimeIndex))
                 .isIn("TIMESTAMP", "TIMESTAMP(6)", "datetime", "DATETIME");
