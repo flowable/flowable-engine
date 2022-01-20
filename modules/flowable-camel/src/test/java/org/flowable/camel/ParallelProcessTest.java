97d13670147383080f022ee56af4d38799fb3e34
Bert Matthys <84137009+BertMatthys@users.noreply.github.com>
2022-01-20 12:54:37 +0100

    Migrate to camel v3.14.0 (#3170)
    * migrate flowable-camel to camel v3.0.0
    
    * migrate flowable-camel to camel v3.3.0
    
    * migrate flowable-camel to camel v3.14.0
    
    * migrate flowable-camel-cdi to camel v3.14.0
    
    * remove comment
    
    * updating documentation

--- modules/flowable-camel/src/test/java/org/flowable/camel/ParallelProcessTest.java
+++ modules/flowable-camel/src/test/java/org/flowable/camel/ParallelProcessTest.java
@@ -56,7 +56,7 @@ public class ParallelProcessTest extends SpringFlowableTestCase {
     public void tearDown() throws Exception {
         List<Route> routes = camelContext.getRoutes();
         for (Route r : routes) {
-            camelContext.stopRoute(r.getId());
+            camelContext.getRouteController().stopRoute(r.getId());
             camelContext.removeRoute(r.getId());
         }
     }
