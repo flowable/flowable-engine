272ead097ee0b7555695b6f25947ed9806d097e9
Tijs Rademakers <tijs.rademakers@gmail.com>
2022-02-02 10:24:44 +0100

    Fix issue with event subscription upgrade script

--- modules/flowable-eventsubscription-service/src/main/resources/org/flowable/eventsubscription/service/db/upgrade/flowable.all.upgradestep.6720.to.6721.eventsubscription.sql
+++ modules/flowable-eventsubscription-service/src/main/resources/org/flowable/eventsubscription/service/db/upgrade/flowable.all.upgradestep.6720.to.6721.eventsubscription.sql
@@ -1,3 +1,3 @@
 create index ACT_IDX_EVENT_SUBSCR_SCOPEREF_ on ACT_RU_EVENT_SUBSCR(SCOPE_ID_, SCOPE_TYPE_);
 
-insert into ACT_GE_PROPERTY values ('eventsubscription.schema.version', '6.7.2.1', 1);
+update ACT_GE_PROPERTY set VALUE_ = '6.7.2.1' where NAME_ = 'eventsubscription.schema.version';
