ad2b4632e35e9502bf00c76e57f28fe982004b06
pangu <pangubpm@139.com>
2022-02-23 19:11:01 +0800

    code style

--- modules/flowable-engine-common/src/main/java/org/flowable/common/engine/impl/cfg/BeansConfigurationHelper.java
+++ modules/flowable-engine-common/src/main/java/org/flowable/common/engine/impl/cfg/BeansConfigurationHelper.java
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.flowable.common.engine.impl.cfg;
 
 import java.io.InputStream;
