6a6b9f15ff8fd5a657763860d51a127668cb006e
Joram Barrez <jbarrez@users.noreply.github.com>
2022-05-09 22:23:06 +0200

    Upgrade sql for all services

--- modules/flowable-eventsubscription-service/src/main/resources/org/flowable/eventsubscription/service/db/create/flowable.db2.create.eventsubscription.sql
+++ modules/flowable-eventsubscription-service/src/main/resources/org/flowable/eventsubscription/service/db/create/flowable.db2.create.eventsubscription.sql
@@ -23,4 +23,4 @@ create index ACT_IDX_EVENT_SUBSCR_CONFIG_ on ACT_RU_EVENT_SUBSCR(CONFIGURATION_)
 create index ACT_IDX_EVENT_SUBSCR_EXEC_ID on ACT_RU_EVENT_SUBSCR(EXECUTION_ID_);
 create index ACT_IDX_EVENT_SUBSCR_SCOPEREF_ on ACT_RU_EVENT_SUBSCR(SCOPE_ID_, SCOPE_TYPE_);
 
-insert into ACT_GE_PROPERTY values ('eventsubscription.schema.version', '6.7.2.1', 1);
+insert into ACT_GE_PROPERTY values ('eventsubscription.schema.version', '6.7.2.2', 1);
