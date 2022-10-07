cd76f63c7e08a18af557710dfb040b6004d2c7e7
Arthur Hupka-Merle <516104+arthware@users.noreply.github.com>
2022-10-07 14:20:01 +0200

    #3452 Support throwing BPMNError from executionListeners
    BPMNErrors thrown from executionListeners can now be handled in error Event Sub-Process or boundary events from sub process invocations in the model.
    ErrorBoundary events on the activity where the executionListeners are defined are not invoked. Instead, a parent event handler is looked up, when an executionListener throws an BPMNError and invoked, if found.
    This is to be consistent with the execution behavior of timer boundary events and signal boundary events.

--- modules/flowable-engine/src/main/java/org/flowable/engine/impl/bpmn/behavior/ParallelMultiInstanceBehavior.java
+++ modules/flowable-engine/src/main/java/org/flowable/engine/impl/bpmn/behavior/ParallelMultiInstanceBehavior.java
@@ -26,7 +26,9 @@ import org.flowable.bpmn.model.SubProcess;
 import org.flowable.bpmn.model.Transaction;
 import org.flowable.common.engine.api.FlowableIllegalArgumentException;
 import org.flowable.common.engine.impl.util.CollectionUtil;
+import org.flowable.engine.delegate.BpmnError;
 import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.impl.bpmn.helper.ErrorPropagation;
 import org.flowable.engine.impl.bpmn.helper.ScopeUtil;
 import org.flowable.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.flowable.engine.impl.delegate.ActivityBehavior;
@@ -166,7 +168,6 @@ public class ParallelMultiInstanceBehavior extends MultiInstanceActivityBehavior
 
             } else {
                 internalLeave(execution, zeroNrOfInstances);
-
             }
 
         } else {
@@ -196,7 +197,12 @@ public class ParallelMultiInstanceBehavior extends MultiInstanceActivityBehavior
         ProcessEngineConfigurationImpl processEngineConfiguration = CommandContextUtil.getProcessEngineConfiguration();
         inactivateExecution(execution, processEngineConfiguration);
 
-        callActivityEndListeners(execution);
+        try {
+            callActivityEndListeners(execution);
+        } catch (BpmnError bpmnError) {
+            ErrorPropagation.propagateError(bpmnError, execution);
+            return;
+        }
 
         logLoopDetails(execution, "instance completed", loopCounter, nrOfCompletedInstances, nrOfActiveInstances, nrOfInstances);
 
