d231a232426f10c791950dc79adc4716e66a03c9
Yvo Swillens <yvo.swillens@flowable.com>
2022-12-30 12:45:31 +0100

    fixed entrypoint issues Flowable REST Docker image

--- modules/flowable-app-rest/src/main/jib/flowable-entrypoint.sh
+++ modules/flowable-app-rest/src/main/jib/flowable-entrypoint.sh
@@ -3,7 +3,9 @@ JAVA_OPTS="${JAVA_OPTS}
   -Djava.awt.headless=true
   -Dfile.encoding=UTF-8"
 
-exec su-exec flowable:flowable java ${JAVA_OPTS} \
+cmd="java ${JAVA_OPTS} \
   -cp /app:/app/WEB-INF/classes:/app/WEB-INF/lib/* \
   org.flowable.rest.app.FlowableRestApplication \
-  ${@}
+  ${@}"
+
+exec $cmd
