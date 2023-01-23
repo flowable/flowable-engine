3b2373c077f48a0b197af8d38adde61f79964f10
Arthur H <516104+arthware@users.noreply.github.com>
2023-01-23 16:25:38 +0100

    Support batch migration for CMMN (#3588)
    * Support Batch Migration for CaseInstances
    
    * Remove failed batch parts, because not used

--- modules/flowable-cmmn-api/src/main/java/org/flowable/cmmn/api/migration/CaseInstanceBatchMigrationResult.java
+++ modules/flowable-cmmn-api/src/main/java/org/flowable/cmmn/api/migration/CaseInstanceBatchMigrationResult.java
@@ -13,8 +13,110 @@
 
 package org.flowable.cmmn.api.migration;
 
-/**
- * @author Valentin Zickner
- */
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class CaseInstanceBatchMigrationResult {
+
+    public static final String STATUS_IN_PROGRESS = "inProgress";
+    public static final String STATUS_WAITING = "waiting";
+    public static final String STATUS_COMPLETED = "completed";
+
+    public static final String RESULT_SUCCESS = "success";
+    public static final String RESULT_FAIL = "fail";
+
+    protected String batchId;
+    protected String status;
+    protected Date completeTime;
+    protected String sourceProcessDefinitionId;
+    protected String targetProcessDefinitionId;
+    protected List<CaseInstanceBatchMigrationPartResult> allMigrationParts = new ArrayList<>();
+    protected List<CaseInstanceBatchMigrationPartResult> succesfulMigrationParts = new ArrayList<>();
+    protected List<CaseInstanceBatchMigrationPartResult> failedMigrationParts = new ArrayList<>();
+    protected List<CaseInstanceBatchMigrationPartResult> waitingMigrationParts = new ArrayList<>();
+
+    public String getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(String batchId) {
+        this.batchId = batchId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCompleteTime() {
+        return completeTime;
+    }
+
+    public void setCompleteTime(Date completeTime) {
+        this.completeTime = completeTime;
+    }
+
+    public String getSourceCaseDefinitionId() {
+        return sourceProcessDefinitionId;
+    }
+
+    public void setSourceCaseDefinitionId(String sourceProcessDefinitionId) {
+        this.sourceProcessDefinitionId = sourceProcessDefinitionId;
+    }
+
+    public String getTargetCaseDefinitionId() {
+        return targetProcessDefinitionId;
+    }
+
+    public void setTargetCaseDefinitionId(String targetProcessDefinitionId) {
+        this.targetProcessDefinitionId = targetProcessDefinitionId;
+    }
+
+    public List<CaseInstanceBatchMigrationPartResult> getAllMigrationParts() {
+        return allMigrationParts;
+    }
+
+    public void addMigrationPart(CaseInstanceBatchMigrationPartResult migrationPart) {
+        if (allMigrationParts == null) {
+            allMigrationParts = new ArrayList<>();
+        }
+        allMigrationParts.add(migrationPart);
+
+        if (!STATUS_COMPLETED.equals(migrationPart.getStatus())) {
+            if (waitingMigrationParts == null) {
+                waitingMigrationParts = new ArrayList<>();
+            }
+            waitingMigrationParts.add(migrationPart);
+
+        } else {
+            if (RESULT_SUCCESS.equals(migrationPart.getResult())) {
+                if (succesfulMigrationParts == null) {
+                    succesfulMigrationParts = new ArrayList<>();
+                }
+                succesfulMigrationParts.add(migrationPart);
+
+            } else {
+                if (failedMigrationParts == null) {
+                    failedMigrationParts = new ArrayList<>();
+                }
+                failedMigrationParts.add(migrationPart);
+            }
+        }
+    }
+
+    public List<CaseInstanceBatchMigrationPartResult> getSuccessfulMigrationParts() {
+        return succesfulMigrationParts;
+    }
+
+    public List<CaseInstanceBatchMigrationPartResult> getFailedMigrationParts() {
+        return failedMigrationParts;
+    }
+
+    public List<CaseInstanceBatchMigrationPartResult> getWaitingMigrationParts() {
+        return waitingMigrationParts;
+    }
 }
--- modules/flowable-cmmn-engine/src/main/java/org/flowable/cmmn/engine/impl/cmd/GetCaseInstanceMigrationBatchResultCmd.java
+++ modules/flowable-cmmn-engine/src/main/java/org/flowable/cmmn/engine/impl/cmd/GetCaseInstanceMigrationBatchResultCmd.java
@@ -13,15 +13,29 @@
 
 package org.flowable.cmmn.engine.impl.cmd;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.flowable.batch.api.Batch;
+import org.flowable.batch.api.BatchPart;
+import org.flowable.batch.api.BatchService;
+import org.flowable.cmmn.api.migration.CaseInstanceBatchMigrationPartResult;
 import org.flowable.cmmn.api.migration.CaseInstanceBatchMigrationResult;
+import org.flowable.cmmn.engine.CmmnEngineConfiguration;
+import org.flowable.cmmn.engine.impl.util.CommandContextUtil;
+import org.flowable.common.engine.api.FlowableException;
 import org.flowable.common.engine.impl.interceptor.Command;
 import org.flowable.common.engine.impl.interceptor.CommandContext;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author Valentin Zickner
  */
 public class GetCaseInstanceMigrationBatchResultCmd implements Command<CaseInstanceBatchMigrationResult> {
 
+    protected static final String BATCH_RESULT_MESSAGE_LABEL = "resultMessage";
     protected String migrationBatchId;
 
     public GetCaseInstanceMigrationBatchResultCmd(String migrationBatchId) {
@@ -30,7 +44,66 @@ public class GetCaseInstanceMigrationBatchResultCmd implements Command<CaseInsta
 
     @Override
     public CaseInstanceBatchMigrationResult execute(CommandContext commandContext) {
-        throw new UnsupportedOperationException(); // TODO
+        CmmnEngineConfiguration engineConfiguration = CommandContextUtil.getCmmnEngineConfiguration(commandContext);
+        BatchService batchService = engineConfiguration.getBatchServiceConfiguration().getBatchService();
+        Batch batch = batchService.getBatch(migrationBatchId);
+
+        if (batch != null) {
+            ObjectMapper objectMapper = engineConfiguration.getObjectMapper();
+            CaseInstanceBatchMigrationResult result = convertFromBatch(batch, objectMapper);
+            List<BatchPart> batchParts = batchService.findBatchPartsByBatchId(batch.getId());
+            if (batchParts != null && !batchParts.isEmpty()) {
+                for (BatchPart batchPart : batchParts) {
+                    result.addMigrationPart(convertFromBatchPart(batchPart, objectMapper, engineConfiguration));
+                }
+            }
+            return result;
+        }
+
+        return null;
+    }
+
+    protected CaseInstanceBatchMigrationResult convertFromBatch(Batch batch, ObjectMapper objectMapper) {
+        CaseInstanceBatchMigrationResult result = new CaseInstanceBatchMigrationResult();
+
+        result.setBatchId(batch.getId());
+        result.setSourceCaseDefinitionId(batch.getBatchSearchKey());
+        result.setTargetCaseDefinitionId(batch.getBatchSearchKey2());
+        result.setStatus(batch.getStatus());
+        result.setCompleteTime(batch.getCompleteTime());
+
+        return result;
+    }
+
+    protected CaseInstanceBatchMigrationPartResult convertFromBatchPart(BatchPart batchPart, ObjectMapper objectMapper,
+            CmmnEngineConfiguration engineConfiguration) {
+        CaseInstanceBatchMigrationPartResult partResult = new CaseInstanceBatchMigrationPartResult();
+
+        partResult.setBatchId(batchPart.getId());
+        partResult.setCaseInstanceId(batchPart.getScopeId());
+        partResult.setSourceCaseDefinitionId(batchPart.getBatchSearchKey());
+        partResult.setTargetCaseDefinitionId(batchPart.getBatchSearchKey2());
+
+        if (batchPart.getCompleteTime() != null) {
+            partResult.setStatus(CaseInstanceBatchMigrationResult.STATUS_COMPLETED);
+        }
+
+        partResult.setResult(batchPart.getStatus());
+        if (CaseInstanceBatchMigrationResult.RESULT_FAIL.equals(batchPart.getStatus()) &&
+                batchPart.getResultDocumentJson(engineConfiguration.getEngineCfgKey()) != null) {
+
+            try {
+                JsonNode resultNode = objectMapper.readTree(batchPart.getResultDocumentJson(engineConfiguration.getEngineCfgKey()));
+                if (resultNode.has(BATCH_RESULT_MESSAGE_LABEL)) {
+                    String resultMessage = resultNode.get(BATCH_RESULT_MESSAGE_LABEL).asText();
+                    partResult.setMigrationMessage(resultMessage);
+                }
+
+            } catch (IOException e) {
+                throw new FlowableException("Error reading batch part " + batchPart.getId());
+            }
+        }
+        return partResult;
     }
 
 }
