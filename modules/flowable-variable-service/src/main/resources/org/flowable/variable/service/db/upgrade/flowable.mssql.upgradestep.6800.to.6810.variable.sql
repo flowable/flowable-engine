b92c8dad27f4e2af972e17cf748a7e7133a44961
Filip Hrisafov <filip.hrisafov@gmail.com>
2023-05-23 18:24:02 +0200

    Fix DB Scripts for META_INFO_
    * Remove incorrect column for SQL Server and Oracle
    * Rename incorrectly named mysql upgrade step

--- modules/flowable-variable-service/src/main/resources/org/flowable/variable/service/db/upgrade/flowable.mssql.upgradestep.6800.to.6810.variable.sql
+++ modules/flowable-variable-service/src/main/resources/org/flowable/variable/service/db/upgrade/flowable.mssql.upgradestep.6800.to.6810.variable.sql
@@ -1,3 +1,3 @@
-alter table ACT_RU_VARIABLE add column META_INFO_ nvarchar(4000);
+alter table ACT_RU_VARIABLE add META_INFO_ nvarchar(4000);
 
 update ACT_GE_PROPERTY set VALUE_ = '6.8.1.0' where NAME_ = 'variable.schema.version';
