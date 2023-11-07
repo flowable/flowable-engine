3abcae06a16b1437c3161cd148ab365a21f3b8ed
Filip Hrisafov <filip.hrisafov@gmail.com>
2023-11-07 14:04:51 +0100

    Use PlanItemDefinitionExport for exporting the main plan model and allow extending from StageExport

--- modules/flowable-cmmn-converter/src/main/java/org/flowable/cmmn/converter/export/StageExport.java
+++ modules/flowable-cmmn-converter/src/main/java/org/flowable/cmmn/converter/export/StageExport.java
@@ -29,7 +29,7 @@ public class StageExport extends AbstractPlanItemDefinitionExport<Stage> {
         return instance;
     }
 
-    private StageExport() {
+    protected StageExport() {
     }
 
     @Override
