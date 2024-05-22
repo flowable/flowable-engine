ad80ecef1afc1cff0a61cbb31716cfc0a828710f
Maxim Gorbunkov <m.gorbunkov@haulmont.com>
2024-05-22 16:19:19 +0400

    Fix issue with invalid database upgrade scripts for HSQL

--- modules/flowable-task-service/src/main/resources/org/flowable/task/service/db/upgrade/flowable.hsql.upgradestep.7000.to.7010.task.sql
+++ modules/flowable-task-service/src/main/resources/org/flowable/task/service/db/upgrade/flowable.hsql.upgradestep.7000.to.7010.task.sql
@@ -1,10 +1,9 @@
-alter table ACT_RU_TASK add (
-    STATE_ varchar(255), 
-    IN_PROGRESS_TIME_ timestamp, 
-    IN_PROGRESS_STARTED_BY_ varchar(255),
-    CLAIMED_BY_ varchar(255), 
-    SUSPENDED_TIME_ timestamp, 
-    SUSPENDED_BY_ varchar(255), 
-    IN_PROGRESS_DUE_DATE_ timestamp);
+alter table ACT_RU_TASK add column STATE_ varchar(255);
+alter table ACT_RU_TASK add column IN_PROGRESS_TIME_ timestamp;
+alter table ACT_RU_TASK add column IN_PROGRESS_STARTED_BY_ varchar(255);
+alter table ACT_RU_TASK add column CLAIMED_BY_ varchar(255);
+alter table ACT_RU_TASK add column SUSPENDED_TIME_ timestamp;
+alter table ACT_RU_TASK add column SUSPENDED_BY_ varchar(255);
+alter table ACT_RU_TASK add column IN_PROGRESS_DUE_DATE_ timestamp;
 
 update ACT_GE_PROPERTY set VALUE_ = '7.0.1.0' where NAME_ = 'task.schema.version';
