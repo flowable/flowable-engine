06b6dfefa9298181c9cfbe5f700b9d13aa1ed791
Maxim Gorbunkov <m.gorbunkov@haulmont.com>
2024-05-22 19:18:21 +0400

    Fix issue with invalid database upgrade scripts for HSQL

--- modules/flowable-task-service/src/main/resources/org/flowable/task/service/db/upgrade/flowable.hsql.upgradestep.7000.to.7010.task.history.sql
+++ modules/flowable-task-service/src/main/resources/org/flowable/task/service/db/upgrade/flowable.hsql.upgradestep.7000.to.7010.task.history.sql
@@ -1,8 +1,8 @@
-alter table add column ACT_HI_TASKINST STATE_ varchar(255);
-alter table add column ACT_HI_TASKINST IN_PROGRESS_TIME_ timestamp;
-alter table add column ACT_HI_TASKINST IN_PROGRESS_STARTED_BY_ varchar(255);
-alter table add column ACT_HI_TASKINST CLAIMED_BY_ varchar(255);
-alter table add column ACT_HI_TASKINST SUSPENDED_TIME_ timestamp;
-alter table add column ACT_HI_TASKINST SUSPENDED_BY_ varchar(255);
-alter table add column ACT_HI_TASKINST COMPLETED_BY_ varchar(255);
-alter table add column ACT_HI_TASKINST IN_PROGRESS_DUE_DATE_ timestamp;
+alter table ACT_HI_TASKINST add column STATE_ varchar(255);
+alter table ACT_HI_TASKINST add column IN_PROGRESS_TIME_ timestamp;
+alter table ACT_HI_TASKINST add column IN_PROGRESS_STARTED_BY_ varchar(255);
+alter table ACT_HI_TASKINST add column CLAIMED_BY_ varchar(255);
+alter table ACT_HI_TASKINST add column SUSPENDED_TIME_ timestamp;
+alter table ACT_HI_TASKINST add column SUSPENDED_BY_ varchar(255);
+alter table ACT_HI_TASKINST add column COMPLETED_BY_ varchar(255);
+alter table ACT_HI_TASKINST add column IN_PROGRESS_DUE_DATE_ timestamp;
