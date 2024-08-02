24fb7f634d129b8d7624a31152391518bc481544
Filip Hrisafov <filip.hrisafov@gmail.com>
2024-08-02 10:20:06 +0200

    Oracle test adjustments
    * Make sure that DMN Tests run against all DBs
    * Split app DmnDeploymentCollectionResourceTest to test sorting and querying separately

--- modules/flowable-dmn-rest/src/test/java/org/flowable/dmn/rest/conf/engine/FlowableDmnEngineConfiguration.java
+++ modules/flowable-dmn-rest/src/test/java/org/flowable/dmn/rest/conf/engine/FlowableDmnEngineConfiguration.java
@@ -12,6 +12,8 @@
  */
 package org.flowable.dmn.rest.conf.engine;
 
+import javax.sql.DataSource;
+
 import org.flowable.common.engine.impl.AbstractEngineConfiguration;
 import org.flowable.dmn.api.DmnDecisionService;
 import org.flowable.dmn.api.DmnHistoryService;
@@ -19,8 +21,16 @@ import org.flowable.dmn.api.DmnRepositoryService;
 import org.flowable.dmn.engine.DmnEngine;
 import org.flowable.dmn.engine.DmnEngineConfiguration;
 import org.flowable.dmn.engine.impl.cfg.StandaloneInMemDmnEngineConfiguration;
+import org.flowable.dmn.spring.DmnEngineFactoryBean;
+import org.flowable.dmn.spring.SpringDmnEngineConfiguration;
+import org.flowable.idm.spring.configurator.SpringIdmEngineConfigurator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * @author Yvo Swillens
@@ -28,30 +38,71 @@ import org.springframework.context.annotation.Configuration;
 @Configuration(proxyBeanMethods = false)
 public class FlowableDmnEngineConfiguration {
 
-    public DmnEngine ruleEngine() {
-        DmnEngineConfiguration dmnEngineConfiguration = new StandaloneInMemDmnEngineConfiguration();
-        dmnEngineConfiguration.setDatabaseSchemaUpdate(AbstractEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+    @Value("${jdbc.url:jdbc:h2:mem:flowable;DB_CLOSE_DELAY=1000}")
+    protected String jdbcUrl;
+
+    @Value("${jdbc.driver:org.h2.Driver}")
+    protected String jdbcDriver;
+
+    @Value("${jdbc.username:sa}")
+    protected String jdbcUsername;
+
+    @Value("${jdbc.password:}")
+    protected String jdbcPassword;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setDriverClassName(jdbcDriver);
+        dataSource.setUsername(jdbcUsername);
+        dataSource.setPassword(jdbcPassword);
+        return dataSource;
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager annotationDrivenTransactionManager(DataSource dataSource) {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource);
+        return transactionManager;
+    }
+
+    @Bean(name = "dmnEngine")
+    public DmnEngineFactoryBean dmnEngineFactoryBean(DmnEngineConfiguration dmnEngineConfiguration) {
+        DmnEngineFactoryBean factoryBean = new DmnEngineFactoryBean();
+        factoryBean.setDmnEngineConfiguration(dmnEngineConfiguration);
+        return factoryBean;
+    }
+
+    @Bean(name = "dmnEngineConfiguration")
+    public DmnEngineConfiguration dmnEngineConfiguration(DataSource dataSource, PlatformTransactionManager transactionManager,
+            SpringIdmEngineConfigurator springIdmEngineConfigurator) {
+        SpringDmnEngineConfiguration dmnEngineConfiguration = new SpringDmnEngineConfiguration();
+        dmnEngineConfiguration.setDataSource(dataSource);
+        dmnEngineConfiguration.setDatabaseSchemaUpdate(DmnEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        dmnEngineConfiguration.setTransactionManager(transactionManager);
+        dmnEngineConfiguration.setIdmEngineConfigurator(springIdmEngineConfigurator);
         dmnEngineConfiguration.setHistoryEnabled(true);
-        return dmnEngineConfiguration.buildDmnEngine();
+        return dmnEngineConfiguration;
+    }
+
+    @Bean(name = "springIdmEngineConfigurator")
+    public SpringIdmEngineConfigurator springIdmEngineConfigurator() {
+        return new SpringIdmEngineConfigurator();
     }
 
     @Bean
-    public DmnEngineConfiguration dmnEngineConfiguration() {
-        return ruleEngine().getDmnEngineConfiguration();
+    public DmnRepositoryService dmnRepositoryService(DmnEngine dmnEngine) {
+        return dmnEngine.getDmnRepositoryService();
     }
 
     @Bean
-    public DmnRepositoryService dmnRepositoryService() {
-        return ruleEngine().getDmnRepositoryService();
-    }
-
-    @Bean
-    public DmnDecisionService dmnRuleService() {
-        return ruleEngine().getDmnDecisionService();
+    public DmnDecisionService dmnRuleService(DmnEngine dmnEngine) {
+        return dmnEngine.getDmnDecisionService();
     }
     
     @Bean
-    public DmnHistoryService dmnHistoryService() {
-        return ruleEngine().getDmnHistoryService();
+    public DmnHistoryService dmnHistoryService(DmnEngine dmnEngine) {
+        return dmnEngine.getDmnHistoryService();
     }
 }
