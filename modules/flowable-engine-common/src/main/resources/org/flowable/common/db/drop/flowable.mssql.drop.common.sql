5c73b8c093241e4316c63b8ce4519abca85b3f44
Filip Hrisafov <filip.hrisafov@gmail.com>
2024-08-02 16:08:39 +0200

    Fix SQL Server flaky tests
    * Fix incorrect SQL server drop index
    * Manually execute jobs for CaseInstanceMigrationBatchTest to avoid SQL Server deadlock exceptions

--- modules/flowable-engine-common/src/main/resources/org/flowable/common/db/drop/flowable.mssql.drop.common.sql
+++ modules/flowable-engine-common/src/main/resources/org/flowable/common/db/drop/flowable.mssql.drop.common.sql
@@ -105,9 +105,9 @@ if exists (SELECT name FROM sysindexes WHERE name = 'ACT_IDX_ACT_HI_TSK_LOG_TASK
 
 if exists (select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'ACT_HI_TSK_LOG') drop table ACT_HI_TSK_LOG;
 
-if exists (SELECT name FROM sysindexes WHERE name = 'ACT_IDX_HI_TASK_SCOPE') drop index ACT_HI_TASK.ACT_IDX_HI_TASK_SCOPE;
-if exists (SELECT name FROM sysindexes WHERE name = 'ACT_IDX_HI_TASK_SUB_SCOPE') drop index ACT_HI_TASK.ACT_IDX_HI_TASK_SUB_SCOPE;
-if exists (SELECT name FROM sysindexes WHERE name = 'ACT_IDX_HI_TASK_SCOPE_DEF') drop index ACT_HI_TASK.ACT_IDX_HI_TASK_SCOPE_DEF;
+if exists (SELECT name FROM sysindexes WHERE name = 'ACT_IDX_HI_TASK_SCOPE') drop index ACT_HI_TASKINST.ACT_IDX_HI_TASK_SCOPE;
+if exists (SELECT name FROM sysindexes WHERE name = 'ACT_IDX_HI_TASK_SUB_SCOPE') drop index ACT_HI_TASKINST.ACT_IDX_HI_TASK_SUB_SCOPE;
+if exists (SELECT name FROM sysindexes WHERE name = 'ACT_IDX_HI_TASK_SCOPE_DEF') drop index ACT_HI_TASKINST.ACT_IDX_HI_TASK_SCOPE_DEF;
 
 if exists (select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'ACT_HI_TASKINST') drop table ACT_HI_TASKINST;
 
