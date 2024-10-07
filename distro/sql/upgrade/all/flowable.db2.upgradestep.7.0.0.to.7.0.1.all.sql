9b49a1a27a1f750362224d0d0c7c06178eeba3ed
Tijs Rademakers <tijs.rademakers@flowable.com>
2024-10-07 11:31:15 +0200

    Add sql files for 7.1.0 version

---
+++ distro/sql/upgrade/all/flowable.db2.upgradestep.7.0.0.to.7.0.1.all.sql
@@ -0,0 +1,38 @@
+alter table ACT_RU_EVENT_SUBSCR add column SCOPE_DEFINITION_KEY_ varchar(255);
+
+update ACT_GE_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'common.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'entitylink.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'identitylink.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'job.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'batch.schema.version';
+
+alter table ACT_RU_TASK 
+  add column STATE_ varchar(255)
+  add column IN_PROGRESS_TIME_ timestamp
+  add column IN_PROGRESS_STARTED_BY_ varchar(255)
+  add column CLAIMED_BY_ varchar(255)
+  add column SUSPENDED_TIME_ timestamp
+  add column SUSPENDED_BY_ varchar(255)
+  add column IN_PROGRESS_DUE_DATE_ timestamp;
+
+alter table ACT_HI_TASKINST 
+  add column STATE_ varchar(255)
+  add column IN_PROGRESS_TIME_ timestamp
+  add column IN_PROGRESS_STARTED_BY_ varchar(255)
+  add column CLAIMED_BY_ varchar(255)
+  add column SUSPENDED_TIME_ timestamp
+  add column SUSPENDED_BY_ varchar(255)
+  add column COMPLETED_BY_ varchar(255)
+  add column IN_PROGRESS_DUE_DATE_ timestamp;
+
+update ACT_GE_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'task.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'variable.schema.version';
+
+update ACT_GE_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'schema.version';
+
+update ACT_ID_PROPERTY set VALUE_ = '7.0.1.1' where NAME_ = 'schema.version';
---
+++ distro/sql/upgrade/all/flowable.postgres.upgradestep.7.0.1.to.7.1.0.all.sql
@@ -0,0 +1,41 @@
+create index ACT_IDX_ACT_HI_TSK_LOG_TASK on ACT_HI_TSK_LOG(TASK_ID_);
+
+delete from ACT_GE_PROPERTY where NAME_ = 'batch.schema.version';
+delete from ACT_GE_PROPERTY where NAME_ = 'entitylink.schema.version';
+delete from ACT_GE_PROPERTY where NAME_ = 'eventsubscription.schema.version';
+delete from ACT_GE_PROPERTY where NAME_ = 'identitylink.schema.version';
+delete from ACT_GE_PROPERTY where NAME_ = 'job.schema.version';
+delete from ACT_GE_PROPERTY where NAME_ = 'task.schema.version';
+delete from ACT_GE_PROPERTY where NAME_ = 'variable.schema.version';
+
+create index ACT_IDX_EVENT_SUBSCR_PROC_ID on ACT_RU_EVENT_SUBSCR(PROC_INST_ID_);
+
+update ACT_GE_PROPERTY set VALUE_ = '7.1.0.2' where NAME_ = 'common.schema.version';
+
+insert into ACT_GE_PROPERTY
+values ('app.schema.version', '7.1.0.2', 1);
+
+drop table ACT_APP_DATABASECHANGELOG;
+drop table ACT_APP_DATABASECHANGELOGLOCK;
+
+insert into ACT_GE_PROPERTY
+values ('cmmn.schema.version', '7.1.0.2', 1);
+
+drop table ACT_CMMN_DATABASECHANGELOG;
+drop table ACT_CMMN_DATABASECHANGELOGLOCK;
+
+insert into ACT_GE_PROPERTY
+values ('dmn.schema.version', '7.1.0.2', 1);
+
+drop table ACT_DMN_DATABASECHANGELOG;
+drop table ACT_DMN_DATABASECHANGELOGLOCK;
+
+insert into ACT_GE_PROPERTY
+values ('eventregistry.schema.version', '7.1.0.2', 1);
+
+drop table FLW_EV_DATABASECHANGELOG;
+drop table FLW_EV_DATABASECHANGELOGLOCK;
+
+update ACT_GE_PROPERTY set VALUE_ = '7.1.0.2' where NAME_ = 'schema.version';
+
+update ACT_ID_PROPERTY set VALUE_ = '7.1.0.2' where NAME_ = 'schema.version';
