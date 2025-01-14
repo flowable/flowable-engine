143d03aba811d524f0580d9a323e1106749e965e
Filip Hrisafov <filip.hrisafov@gmail.com>
2025-01-14 11:55:26 +0100

    Install mssql-tools18 in GitHub action and use -C to trust server certificate

--- .github/actions/scripts/prepare-mssql.sh
+++ .github/actions/scripts/prepare-mssql.sh
@@ -4,10 +4,10 @@ sudo su
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 echo "Installing mssql-tools"
 sudo apt-get update
-sudo apt-get install mssql-tools unixodbc-dev
+sudo apt-get install mssql-tools18 unixodbc-dev
 
 echo "Creating database and user"
-/opt/mssql-tools/bin/sqlcmd -S ${MSSQL_HOST},${MSSQL_PORT} -U sa -P flowableStr0ngPassword -l 120 -i ./.github/actions/scripts/init-mssql.sql
+/opt/mssql-tools18/bin/sqlcmd -S ${MSSQL_HOST},${MSSQL_PORT} -U sa -P flowableStr0ngPassword -C -l 120 -i ./.github/actions/scripts/init-mssql.sql
 
 if [ $? -ne 0 ]
 then
