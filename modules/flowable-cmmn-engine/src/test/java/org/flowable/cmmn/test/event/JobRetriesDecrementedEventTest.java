5ccf1bd88a4c6a553d89d37d1a603e8577029c36
martin-grofcik <mgrofcik@gmail.com>
2025-06-18 15:42:19 +0200

    #4062 cmmn job_retries_decremented event impl.

---
+++ modules/flowable-cmmn-engine/src/test/java/org/flowable/cmmn/test/event/JobRetriesDecrementedEventTest.java
@@ -0,0 +1,116 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flowable.cmmn.test.event;
+
+import org.flowable.cmmn.api.CmmnRuntimeService;
+import org.flowable.cmmn.api.CmmnTaskService;
+import org.flowable.cmmn.api.runtime.CaseInstance;
+import org.flowable.cmmn.engine.CmmnEngineConfiguration;
+import org.flowable.cmmn.engine.test.CmmnDeployment;
+import org.flowable.cmmn.engine.test.FlowableCmmnTest;
+import org.flowable.cmmn.engine.test.impl.CmmnJobTestHelper;
+import org.flowable.common.engine.api.delegate.event.AbstractFlowableEventListener;
+import org.flowable.common.engine.api.delegate.event.FlowableEngineEntityEvent;
+import org.flowable.common.engine.api.delegate.event.FlowableEvent;
+import org.flowable.common.engine.api.delegate.event.FlowableEventType;
+import org.flowable.job.api.Job;
+import org.flowable.task.api.Task;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.flowable.common.engine.api.delegate.event.FlowableEngineEventType.ENTITY_UPDATED;
+import static org.flowable.common.engine.api.delegate.event.FlowableEngineEventType.JOB_RETRIES_DECREMENTED;
+
+/**
+ * @author martin.grofcik
+ */
+@FlowableCmmnTest
+public class JobRetriesDecrementedEventTest {
+
+    TestEventListener listener = new TestEventListener();
+
+    @BeforeEach
+    void addListener(CmmnEngineConfiguration configuration) {
+        listener.events.clear();
+        configuration.getEventDispatcher().addEventListener(listener);
+    }
+
+    @AfterEach
+    void removeListener(CmmnEngineConfiguration configuration) {
+        configuration.getEventDispatcher().removeEventListener(listener);
+    }
+
+    @Test
+    @CmmnDeployment(resources = "org/flowable/cmmn/test/async/AsyncTaskTest.testAsyncServiceTask.cmmn")
+    void noEventForSuccess(CmmnEngineConfiguration configuration, CmmnRuntimeService cmmnRuntimeService, CmmnTaskService cmmnTaskService) {
+        CaseInstance caseInstance = cmmnRuntimeService.createCaseInstanceBuilder().caseDefinitionKey("testAsyncServiceTask").start();
+        Task task = cmmnTaskService.createTaskQuery()
+                .caseInstanceId(caseInstance.getId())
+                .singleResult();
+        assertThat(task.getName()).isEqualTo("Task before service task");
+        cmmnTaskService.complete(task.getId());
+
+        CmmnJobTestHelper.waitForJobExecutorToProcessAllAsyncJobs(configuration, 20_000, 300, true);
+
+        assertThat(listener.events).isEmpty();
+    }
+
+    @Test
+    @CmmnDeployment(resources = "org/flowable/cmmn/test/async/AsyncTaskTest.testAsyncServiceTaskWithFailure.cmmn")
+    void jobRetriesDecrementedOnFailure(CmmnEngineConfiguration configuration, CmmnRuntimeService cmmnRuntimeService, CmmnTaskService cmmnTaskService) {
+        CaseInstance caseInstance = cmmnRuntimeService.createCaseInstanceBuilder().caseDefinitionKey("testAsyncServiceTask").start();
+        Task task = cmmnTaskService.createTaskQuery()
+                .caseInstanceId(caseInstance.getId())
+                .singleResult();
+        assertThat(task.getName()).isEqualTo("Task before service task");
+        cmmnTaskService.complete(task.getId());
+
+        CmmnJobTestHelper.waitForJobExecutorToProcessAllAsyncJobs(configuration, 20_000, 300, true);
+
+        assertThat(listener.events)
+                .filteredOn( e -> caseInstance.getId().equals(getScopeId(e)))
+                .extracting(FlowableEvent::getType)
+                .containsOnly(JOB_RETRIES_DECREMENTED, ENTITY_UPDATED);
+    }
+
+    private String getScopeId(FlowableEvent e) {
+        return ((Job) ((FlowableEngineEntityEvent) e).getEntity()).getScopeId();
+    }
+
+    private static class TestEventListener extends AbstractFlowableEventListener {
+        private static final Collection<? extends FlowableEventType> SUPPORTED_TYPES = Set.of(JOB_RETRIES_DECREMENTED, ENTITY_UPDATED);
+        List<FlowableEvent> events = new ArrayList<>();
+
+        @Override
+        public void onEvent(FlowableEvent event) {
+            events.add(event);
+        }
+
+        @Override
+        public boolean isFailOnException() {
+            return false;
+        }
+
+        @Override
+        public Collection<? extends FlowableEventType> getTypes() {
+            return SUPPORTED_TYPES;
+        }
+    }
+}
