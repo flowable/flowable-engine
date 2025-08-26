e66056771d784d5107d0832e00428c790bbe958e
OmarAlsawaf <omar.hussien.alsawaf@gmail.com>
2025-08-26 13:57:30 +0200

    reorganizing imports

--- modules/flowable-engine/src/test/java/org/flowable/engine/test/bpmn/event/signal/SignalEventTest.java
+++ modules/flowable-engine/src/test/java/org/flowable/engine/test/bpmn/event/signal/SignalEventTest.java
@@ -18,7 +18,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.tuple;
 import static org.assertj.core.data.MapEntry.entry;
 
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.Date;
 
 import org.assertj.core.groups.Tuple;
 import org.flowable.common.engine.api.FlowableException;
@@ -39,12 +44,7 @@ import org.flowable.task.api.Task;
 import org.flowable.validation.validator.Problems;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Arrays;
-import java.util.Date;
+
 
 
 /**
