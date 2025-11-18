4451bde9534444c97cb6b35f499f6dec7ee0a9c7
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-11-18 11:26:17 +0700

    Fix typos in some files

--- docs/public-api/tools/flowable-slate/slate/source/javascripts/lib/_jquery.highlight.js
+++ docs/public-api/tools/flowable-slate/slate/source/javascripts/lib/_jquery.highlight.js
@@ -11,7 +11,7 @@
  *  - highlight element tag and class names can be specified in options
  *
  * Usage:
- *   // wrap every occurrance of text 'lorem' in content
+ *   // wrap every occurrence of text 'lorem' in content
  *   // with <span class='highlight'> (default options)
  *   $('#content').highlight('lorem');
  *
@@ -26,7 +26,7 @@
  *   // don't ignore case during search of term 'lorem'
  *   $('#content').highlight('lorem', { caseSensitive: true });
  *
- *   // wrap every occurrance of term 'ipsum' in content
+ *   // wrap every occurrence of term 'ipsum' in content
  *   // with <em class='important'>
  *   $('#content').highlight('ipsum', { element: 'em', className: 'important' });
  *
