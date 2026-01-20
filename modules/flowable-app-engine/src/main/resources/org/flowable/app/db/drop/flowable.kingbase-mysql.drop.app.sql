75251ea0f79c5a7e37d2d68b3ae3e79f80f0b861
zjj <2013359110@qq.com>
2026-01-20 16:22:29 +0800

    适配kingbase数据库三种模式

---
+++ modules/flowable-app-engine/src/main/resources/org/flowable/app/db/drop/flowable.kingbase-mysql.drop.app.sql
@@ -0,0 +1,3 @@
+drop table if exists ACT_APP_APPDEF cascade;
+drop table if exists ACT_APP_DEPLOYMENT_RESOURCE cascade;
+drop table if exists ACT_APP_DEPLOYMENT cascade;
---
+++ modules/flowable-engine-common/src/main/resources/org/flowable/common/db/drop/flowable.kingbase-sqlserver.drop.common.sql
@@ -0,0 +1,29 @@
+drop table if exists FLW_RU_BATCH_PART cascade;
+drop table if exists FLW_RU_BATCH cascade;
+
+drop table if exists ACT_RU_ENTITYLINK cascade;
+drop table if exists ACT_HI_ENTITYLINK cascade;
+
+drop table if exists ACT_RU_EVENT_SUBSCR cascade;
+
+drop table if exists ACT_RU_IDENTITYLINK cascade;
+drop table if exists ACT_HI_IDENTITYLINK cascade;
+
+drop table if exists ACT_RU_JOB cascade;
+drop table if exists ACT_RU_TIMER_JOB cascade;
+drop table if exists ACT_RU_SUSPENDED_JOB cascade;
+drop table if exists ACT_RU_DEADLETTER_JOB cascade;
+drop table if exists ACT_RU_HISTORY_JOB cascade;
+drop table if exists ACT_RU_EXTERNAL_JOB cascade;
+
+drop table if exists ACT_RU_TASK cascade;
+drop table if exists ACT_HI_TSK_LOG cascade;
+drop table if exists ACT_HI_TASKINST cascade;
+
+
+drop table if exists ACT_RU_VARIABLE cascade;
+drop table if exists ACT_HI_VARINST cascade;
+
+
+drop table if exists ACT_GE_BYTEARRAY cascade;
+drop table if exists ACT_GE_PROPERTY cascade;
