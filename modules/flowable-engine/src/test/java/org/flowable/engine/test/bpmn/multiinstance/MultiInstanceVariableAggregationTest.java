c71fa2609d783847fa2e01e141d8b25758dd3845
dbm <david@malkovsky.org>
2026-03-09 16:27:36 -0400

    Chore: remove unneeded import statements and replace wildcard import

--- modules/flowable-engine/src/test/java/org/flowable/engine/test/bpmn/multiinstance/MultiInstanceVariableAggregationTest.java
+++ modules/flowable-engine/src/test/java/org/flowable/engine/test/bpmn/multiinstance/MultiInstanceVariableAggregationTest.java
@@ -21,7 +21,6 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
