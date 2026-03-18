e7d9a13bc6e9c9d61ea38d3cbfc347ffc56526f1
Filip Hrisafov <filip.hrisafov@gmail.com>
2026-03-18 21:52:42 +0100

    Fix flaky SQL Server test failures
    - HistoricTaskLogCollectionResourceTest: truncate milliseconds to avoid
      SQL Server datetime type rounding (3.33ms precision) causing >= comparisons to fail
    - CallActivityTest: skip testCallActivityAsyncCompleteRealExecutor on SQL Server
      since parallel multi-instance async call activities cause deadlocks

--- modules/flowable-rest/src/test/java/org/flowable/rest/service/api/history/HistoricTaskLogCollectionResourceTest.java
+++ modules/flowable-rest/src/test/java/org/flowable/rest/service/api/history/HistoricTaskLogCollectionResourceTest.java
@@ -93,6 +93,7 @@ public class HistoricTaskLogCollectionResourceTest extends BaseSpringRestTestCas
     @Deployment(resources = { "org/flowable/rest/api/history/HistoricTaskLogCollectionResourceTest.bpmn20.xml" })
     public void itCanQueryUsingFromToDates() throws IOException {
         Calendar startTime = Calendar.getInstance();
+        startTime.set(Calendar.MILLISECOND, 0);
         processEngineConfiguration.getClock().setCurrentTime(startTime.getTime());
 
         ProcessInstance processInstance1 = runtimeService.startProcessInstanceByKey("oneTaskProcess", "testBusinessKey");
