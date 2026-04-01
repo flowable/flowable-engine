b45a5246834293386877cff95c0b8ac1469caf5c
Joram Barrez <jbarrez@users.noreply.github.com>
2026-04-01 10:24:46 +0200

    Bump database version

--- modules/flowable-cmmn-engine/src/main/resources/org/flowable/cmmn/db/create/flowable.oracle.create.cmmn.sql
+++ modules/flowable-cmmn-engine/src/main/resources/org/flowable/cmmn/db/create/flowable.oracle.create.cmmn.sql
@@ -262,4 +262,4 @@ CREATE TABLE ACT_CMMN_HI_PLAN_ITEM_INST
 CREATE INDEX ACT_IDX_HI_PLAN_ITEM_INST_CASE ON ACT_CMMN_HI_PLAN_ITEM_INST (CASE_INST_ID_);
 
 insert into ACT_GE_PROPERTY
-values ('cmmn.schema.version', '8.0.0.0', 1);
+values ('cmmn.schema.version', '8.1.0.0', 1);
--- modules/flowable-cmmn-engine/src/main/resources/org/flowable/cmmn/db/upgrade/flowable.mssql.upgradestep.7202.to.8000.cmmn.sql
+++ modules/flowable-cmmn-engine/src/main/resources/org/flowable/cmmn/db/upgrade/flowable.mssql.upgradestep.7202.to.8000.cmmn.sql
@@ -1,6 +1,3 @@
 ALTER TABLE ACT_CMMN_HI_CASE_INST ADD END_USER_ID_ nvarchar(255);
 
-alter table ACT_CMMN_RU_PLAN_ITEM_INST add FAILED_TIME_ datetime null;
-alter table ACT_CMMN_HI_PLAN_ITEM_INST add FAILED_TIME_ datetime null;
-
 update ACT_GE_PROPERTY set VALUE_ = '8.0.0.0' where NAME_ = 'cmmn.schema.version';
