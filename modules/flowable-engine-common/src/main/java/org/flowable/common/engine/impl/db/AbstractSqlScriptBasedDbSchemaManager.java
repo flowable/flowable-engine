f68bf45b00ba93f370a38637d7ed8fc7854c566e
pansb <pan33PAN>
2026-07-28 15:08:58 +0800

    fix(flowable-engine-common):Resource Leakage Issue in Flowable-Engine-Common Module AbstractSqlScriptBasedDbSchemaManager.java

--- modules/flowable-engine-common/src/main/java/org/flowable/common/engine/impl/db/AbstractSqlScriptBasedDbSchemaManager.java
+++ modules/flowable-engine-common/src/main/java/org/flowable/common/engine/impl/db/AbstractSqlScriptBasedDbSchemaManager.java
@@ -196,10 +196,10 @@ public abstract class AbstractSqlScriptBasedDbSchemaManager implements SchemaMan
             tableName = prependDatabaseTablePrefix(tableName);
         }
         try (PreparedStatement statement = getDbSqlSession().getSqlSession().getConnection()
-                .prepareStatement("select VALUE_ from " + tableName + " where NAME_ = ?")) {
+                .prepareStatement("select VALUE_ from " + tableName + " where NAME_ = ?");
+             ResultSet resultSet = statement.executeQuery()) {
 
             statement.setString(1, propertyName);
-            ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 return resultSet.getString(1);
             } else {
